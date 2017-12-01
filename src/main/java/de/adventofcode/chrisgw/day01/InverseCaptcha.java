1355fb4bdf7dcd76b182c6c1ad5bb3f4d269c057
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-01 14:57:31 +0100

    2017 Day 1: Inverse Captcha - Part 02

--- src/main/java/de/adventofcode/chrisgw/day01/InverseCaptcha.java
+++ src/main/java/de/adventofcode/chrisgw/day01/InverseCaptcha.java
@@ -1,11 +1,11 @@
 package de.adventofcode.chrisgw.day01;
 
-import java.math.BigInteger;
-
-
 /**
  * <h1><a href="https://adventofcode.com/2017/day/1">https://adventofcode.com/2017/day/1</a></h1>
- * The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
+ * <p>
+ * The captcha requires you to review a sequence of digits (your puzzle input)
+ * and find the <em>sum</em> of all digits that match the <em>next</em> digit in the list. The
+ * list is circular, so the digit after the last digit is the <em>first</em> digit in the list.
  * <p>
  * For example:
  * <ul>
@@ -15,39 +15,67 @@ import java.math.BigInteger;
  * <li>1234 produces 0 because no digit matches the next.</li>
  * <li>91212129 produces 9 because the only digit that matches the next one is the last digit, 9.</li>
  * </ul>
+ * <p>
+ * --- Part Two ---
+ * <p>
+ * You notice a progress bar that jumps to 50% completion.
+ * Apparently, the door isn't yet satisfied, but it did emit a star as encouragement. The instructions change:
+ * <p>
+ * Now, instead of considering the <em>next</em> digit, it wants you to consider the
+ * digit <em>halfway around</em> the circular list. That is, if your list contains <code>10</code>
+ * items, only include a digit in your sum if the digit <code>10/2 = 5</code> steps forward
+ * matches it. Fortunately, your list has an even number of elements.
+ * <p>
+ * For example:
+ * <li>1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.</li>
+ * <li>1221 produces 0, because every comparison is between a 1 and a 2.</li>
+ * <li>123425 produces 4, because both 2s match each other, but no other digit has a match.</li>
+ * <li>123123 produces 12.</li>
+ * <li>12131415 produces 4.</li>
+ * </ul>
  */
 public class InverseCaptcha {
 
-
     /**
      * The captcha requires you to review a sequence of digits (your puzzle input)
-     * and find the sum of all digits that match the next digit in the list.
+     * and find the <em>sum</em> of all digits that match the <em>next</em> digit in the list. The
+     * list is circular, so the digit after the last digit is the <em>first</em> digit in the list.
      *
      * @param captchaInput positive number
      * @return captcha solution sum
-     * @see #solveInverseCaptcha(String)
+     * @see #solveInverseCaptcha(String, int)
      */
-    public static long solveInverseCaptcha(BigInteger captchaInput) {
-        if (captchaInput.compareTo(BigInteger.ZERO) < 0) {
-            throw new IllegalArgumentException("Expect positive number, but was " + captchaInput);
-        }
-        return solveInverseCaptcha(captchaInput.toString());
+    public static long solveInverseCaptchaPart1(String captchaInput) {
+        int nextDigitOffset = 1;
+        return solveInverseCaptcha(captchaInput, nextDigitOffset);
     }
 
     /**
-     * The captcha requires you to review a sequence of digits (your puzzle input)
-     * and find the sum of all digits that match the next digit in the list.
+     * Now, instead of considering the <em>next</em> digit, it wants you to consider the
+     * digit <em>halfway around</em> the circular list. That is, if your list contains <code>10</code>
+     * items, only include a digit in your sum if the digit <code>10/2 = 5</code> steps forward
+     * matches it. Fortunately, your list has an even number of elements.
      *
-     * @param captchaInput positive number
+     * @param captchaInput positive number even number of elements
      * @return captcha solution sum
-     * @see #solveInverseCaptcha(BigInteger)
+     * @see #solveInverseCaptcha(String, int)
      */
-    public static long solveInverseCaptcha(String captchaInput) {
+    public static long solveInverseCaptchaPart2(String captchaInput) {
+        int halfWayDigitOffset = captchaInput.length() / 2;
+        return solveInverseCaptcha(captchaInput, halfWayDigitOffset);
+    }
+
+    /**
+     * @param captchaInput    positive number
+     * @param nextDigitOffset offset of next digit to compare
+     * @return captcha solution sum
+     */
+    private static long solveInverseCaptcha(String captchaInput, int nextDigitOffset) {
         long sum = 0;
 
         for (int i = 0; i < captchaInput.length(); i++) {
             int currentDigit = getDigitOfNumber(captchaInput, i);
-            int nextDigit = getDigitOfNumber(captchaInput, (i + 1));
+            int nextDigit = getDigitOfNumber(captchaInput, (i + nextDigitOffset));
 
             if (currentDigit == nextDigit) {
                 sum += currentDigit;
@@ -63,10 +91,11 @@ public class InverseCaptcha {
      * @return digit at given index n
      */
     private static int getDigitOfNumber(String numberStr, int n) {
-        if (n >= numberStr.length()) {
-            n = 0; // start from number beginning again (circular number)
+        int numberLength = numberStr.length();
+        if (n >= numberLength) {
+            n = n % numberLength; // start from number beginning again (circular number list)
         }
-        int numericValue = Character.getNumericValue(numberStr.charAt(n));
+        int numericValue = numberStr.charAt(n) - '0';
         if (numericValue < 0 || numericValue > 9) {
             throw new NumberFormatException(String.format("'%s' has no digit at index %d", numberStr, n));
         }
@@ -82,8 +111,17 @@ public class InverseCaptcha {
 
         for (String captchaInput : args) {
             try {
-                long captchaSolution = solveInverseCaptcha(captchaInput);
-                System.out.printf("%6d = '%s'\n", captchaSolution, captchaInput);
+                System.out.printf("captcha input: '%s'\n", captchaInput);
+
+                long startTime = System.nanoTime();
+                long captchaSolution1 = solveInverseCaptchaPart1(captchaInput);
+                long duration = System.nanoTime() - startTime;
+                System.out.printf("next digit captcha:    %9d\t(after %9d ns)\n", captchaSolution1, duration);
+
+                startTime = System.nanoTime();
+                long captchaSolution2 = solveInverseCaptchaPart2(captchaInput);
+                duration = System.nanoTime() - startTime;
+                System.out.printf("halfway digit captcha: %9d\t(after %9d ns)\n", captchaSolution2, duration);
             } catch (Exception e) {
                 e.printStackTrace();
             }
