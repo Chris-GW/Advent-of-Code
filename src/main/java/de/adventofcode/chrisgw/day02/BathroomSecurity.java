1baf4b55fdce43d18a8f06345498b43c8e85ddc0
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-02 13:36:31 +0100

    2016 Day 2: Bathroom Security - Part 2

--- src/main/java/de/adventofcode/chrisgw/day02/BathroomSecurity.java
+++ src/main/java/de/adventofcode/chrisgw/day02/BathroomSecurity.java
@@ -1,8 +1,9 @@
 package de.adventofcode.chrisgw.day02;
 
-import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -36,77 +37,225 @@ import java.util.List;
  * - Finally, you move up four times (stopping at "2"), then down once, ending with 5.
  *
  * So, in this example, the bathroom code is 1985.
+ *
+ * --- Part Two ---
+ *
+ * You finally arrive at the bathroom (it's a several minute walk from the
+ * lobby so visitors can behold the many fancy conference rooms and water
+ * coolers on this floor) and go to punch in the code. Much to your bladder's
+ * dismay, the keypad is not at all like you imagined it. Instead, you are
+ * confronted with the result of hundreds of man-hours of bathroom-keypad-design meetings:
+ *     1
+ *   2 3 4
+ * 5 6 7 8 9
+ *   A B C
+ *     D
+ *
+ * You still start at "5" and stop when you're at an edge, but given the same
+ * instructions as above, the outcome is very different:
+ *
+ * You start at "5" and don't move at all (up and left are both edges), ending at 5.
+ * Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
+ * Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
+ * Finally, after five more moves, you end at 3.
+ * So, given the actual keypad layout, the code would be 5DB3.
  * </pre>
  */
 public class BathroomSecurity {
 
-    private static final int[][] KEYPAD = new int[][] { //
-            { 1, 2, 3 }, //
-            { 4, 5, 6 }, //
-            { 7, 8, 9 } };
-
-    private int x = 1;
-    private int y = 1;
+    private Keypad keypad;
 
 
-    public List<Integer> followInstructions(String instructions) {
+    public BathroomSecurity(Keypad keypad) {
+        this.keypad = Objects.requireNonNull(keypad);
+    }
+
+
+    public List<String> followInstructions(String instructions) {
         String[] instructionRows = instructions.split("\n");
-        List<Integer> buttonsToPress = new ArrayList<>(instructionRows.length);
+        List<String> buttonsToPress = new ArrayList<>(instructionRows.length);
 
+        System.out.println(keypad);
         for (int rowIndex = 0; rowIndex < instructionRows.length; rowIndex++) {
             String instruction = instructionRows[rowIndex];
 
+            System.out.printf("'%s'", keypad.getCurrentKey());
             for (int instructionIndex = 0; instructionIndex < instruction.length(); instructionIndex++) {
                 String moveDirectionCode = String.valueOf(instruction.charAt(instructionIndex));
                 MoveDirection moveDirection = MoveDirection.fromDirectionCode(moveDirectionCode);
-                followMoveInstruction(moveDirection);
+                if (keypad.canMove(moveDirection)) {
+                    keypad.move(moveDirection);
+                }
+                System.out.printf("-[%s]->'%s'", moveDirection, keypad.getCurrentKey());
             }
-            buttonsToPress.add(getKeypadButton());
+            System.out.println();
+            System.out.println(keypad);
+            buttonsToPress.add(keypad.getCurrentKey());
         }
         return buttonsToPress;
     }
 
-    private void followMoveInstruction(MoveDirection moveDirection) {
-        if (moveDirection == MoveDirection.UP) {
-            y -= 1;
-        } else if (moveDirection == MoveDirection.DOWN) {
-            y += 1;
-        } else if (moveDirection == MoveDirection.LEFT) {
-            x -= 1;
-        } else if (moveDirection == MoveDirection.RICHT) {
-            x += 1;
-        } else {
-            throw new IllegalArgumentException("Unexpected MoveDirection " + moveDirection);
-        }
-        normalizeKeypadPosition();
-    }
-
-    private void normalizeKeypadPosition() {
-        if (x < 0) {
-            x = 0;
-        } else if (x > 2) {
-            x = 2;
-        }
-
-        if (y < 0) {
-            y = 0;
-        } else if (y > 2) {
-            y = 2;
-        }
-    }
-
-
-    private int getKeypadButton() {
-        return KEYPAD[y][x];
-    }
 
     @Override
     public String toString() {
-        return String.format("(%d;%d)=%d", x, y, getKeypadButton());
+        return keypad.toString();
+    }
+
+
+    public interface Keypad {
+
+        String getCurrentKey();
+
+        void move(MoveDirection moveDirection);
+
+        boolean canMove(MoveDirection moveDirection);
+
+    }
+
+
+    public static abstract class GridKeypad implements Keypad {
+
+        protected int x;
+        protected int y;
+
+
+        public GridKeypad(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public abstract String[][] getGridKeypad();
+
+        protected String getKeypadKey(int x, int y) {
+            return getGridKeypad()[y][x];
+        }
+
+        public boolean isAviableKeypadKey(int x, int y) {
+            String[][] gridKeypad = getGridKeypad();
+            if (y < 0 || y >= gridKeypad.length) {
+                return false;
+            }
+            if (x < 0 || x >= gridKeypad[y].length) {
+                return false;
+            }
+            return gridKeypad[y][x] != null;
+        }
+
+
+        @Override
+        public String getCurrentKey() {
+            return getKeypadKey(x, y);
+        }
+
+        @Override
+        public void move(MoveDirection moveDirection) {
+            if (!canMove(moveDirection)) {
+                throw new IllegalArgumentException("can't move " + moveDirection);
+            }
+            if (MoveDirection.UP.equals(moveDirection)) {
+                y -= 1;
+            } else if (MoveDirection.DOWN.equals(moveDirection)) {
+                y += 1;
+            } else if (MoveDirection.LEFT.equals(moveDirection)) {
+                x -= 1;
+            } else if (MoveDirection.RICHT.equals(moveDirection)) {
+                x += 1;
+            }
+        }
+
+        @Override
+        public boolean canMove(MoveDirection moveDirection) {
+            if (moveDirection == null) {
+                return false;
+            }
+            if (MoveDirection.UP.equals(moveDirection) && !isAviableKeypadKey(x, y - 1)) {
+                return false;
+            } else if (MoveDirection.DOWN.equals(moveDirection) && !isAviableKeypadKey(x, y + 1)) {
+                return false;
+            } else if (MoveDirection.LEFT.equals(moveDirection) && !isAviableKeypadKey(x - 1, y)) {
+                return false;
+            } else if (MoveDirection.RICHT.equals(moveDirection) && !isAviableKeypadKey(x + 1, y)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            String[][] gridKeypad = getGridKeypad();
+            for (int y = 0; y < gridKeypad.length; y++) {
+                for (int x = 0; x < gridKeypad[y].length; x++) {
+                    if (!isAviableKeypadKey(x, y)) {
+                        sb.append("    ");
+                    } else if (this.y == y && this.x == x) {
+                        String keypadKey = getKeypadKey(x, y);
+                        sb.append("[").append(keypadKey).append("] ");
+                    } else {
+                        String keypadKey = getKeypadKey(x, y);
+                        sb.append(" ").append(keypadKey).append("  ");
+                    }
+                }
+                sb.append("\n");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            return sb.toString();
+        }
+
+    }
+
+
+    public static class NumberKeypad extends GridKeypad {
+
+        // @formatter:off
+        private static final String[][] KEYPAD = new String[][] {
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" } };
+        // @formatter:on
+
+
+        public NumberKeypad() {
+            super(1, 1);
+        }
+
+
+        @Override
+        public String[][] getGridKeypad() {
+            return KEYPAD;
+        }
+
+    }
+
+
+    public static class FancyBathroomKeypad extends GridKeypad {
+
+        // @formatter:off
+        private static final String[][] KEYPAD = new String[][] {
+                { null, null, "1", null, null },
+                { null,  "2", "3", "4", null },
+                {   "5", "6", "7", "8", "9" },
+                {  null, "A", "B", "C", null },
+                { null, null, "D", null, null } };
+        // @formatter:on
+
+
+        public FancyBathroomKeypad() {
+            super(0, 2);
+        }
+
+
+        @Override
+        public String[][] getGridKeypad() {
+            return KEYPAD;
+        }
+
     }
 
 
     public enum MoveDirection {
+
         UP("U"), DOWN("D"), LEFT("L"), RICHT("R");
 
         private String moveDirectionCode;
@@ -144,12 +293,12 @@ public class BathroomSecurity {
         }
 
         String instructions = args[0];
-        BathroomSecurity bathroomSecurity = new BathroomSecurity();
-        List<Integer> buttonsToPress = bathroomSecurity.followInstructions(instructions);
-        for (Integer buttonToPress : buttonsToPress) {
-            System.out.print(buttonToPress);
+        List<GridKeypad> aviableKeypads = Arrays.asList(new NumberKeypad(), new FancyBathroomKeypad());
+        for (Keypad keypad : aviableKeypads) {
+            BathroomSecurity bathroomSecurity = new BathroomSecurity(keypad);
+            List<String> buttonsToPress = bathroomSecurity.followInstructions(instructions);
+            System.out.println(String.join("", buttonsToPress));
         }
-        System.out.println();
     }
 
 }
