85457f7781cc78dd8ea605978c90f9fcf49ce27e
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-03 15:34:28 +0100

    2016 Day 3: Squares With Three Sides Part 2

--- 2016/src/main/java/de/adventofcode/chrisgw/day03/SquaresWithThreeSides.java
+++ 2016/src/main/java/de/adventofcode/chrisgw/day03/SquaresWithThreeSides.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class SquaresWithThreeSides {
 
 
-    public static List<Triangle> parseTriangleDocument(String triangleDocument) {
+    public static List<Triangle> parseTriangleDocumentHorizontal(String triangleDocument) {
         String[] triangleRows = triangleDocument.split("\n");
         List<Triangle> triangles = new ArrayList<>(triangleRows.length);
 
@@ -19,6 +19,27 @@ public class SquaresWithThreeSides {
 
             Triangle triangle = new Triangle(a, b, c);
             triangles.add(triangle);
+        }
+        return triangles;
+    }
+
+    public static List<Triangle> parseTriangleDocumentVertical(String triangleDocument) {
+        String[] triangleRows = triangleDocument.split("\n");
+        List<Triangle> triangles = new ArrayList<>(triangleRows.length);
+
+        for (int rowIndex = 0; rowIndex < triangleRows.length; rowIndex += 3) {
+            String[] triangleSidesA = triangleRows[rowIndex].trim().split("\\s+");
+            String[] triangleSidesB = triangleRows[rowIndex + 1].trim().split("\\s+");
+            String[] triangleSidesC = triangleRows[rowIndex + 2].trim().split("\\s+");
+
+            for (int columnIndex = 0; columnIndex < triangleSidesA.length; columnIndex++) {
+                int a = Integer.parseInt(triangleSidesA[columnIndex]);
+                int b = Integer.parseInt(triangleSidesB[columnIndex]);
+                int c = Integer.parseInt(triangleSidesC[columnIndex]);
+
+                Triangle triangle = new Triangle(a, b, c);
+                triangles.add(triangle);
+            }
         }
         return triangles;
     }
