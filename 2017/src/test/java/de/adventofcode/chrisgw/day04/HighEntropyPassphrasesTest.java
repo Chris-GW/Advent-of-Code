dacc2ea0f6839e14c50bda894adaa51e32dce291
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-04 14:27:51 +0100

    2017 Day 4: High-Entropy Passphrases Part 2

--- 2017/src/test/java/de/adventofcode/chrisgw/day04/HighEntropyPassphrasesTest.java
+++ 2017/src/test/java/de/adventofcode/chrisgw/day04/HighEntropyPassphrasesTest.java
@@ -18,6 +18,7 @@ public class HighEntropyPassphrasesTest {
         Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
         Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
         Assert.assertTrue("Expect passphrase isValid true", passphrase.isValid());
+        Assert.assertTrue("Expect passphrase isValid with anagrams true", passphrase.isValid(true));
     }
 
     @Test
@@ -28,6 +29,7 @@ public class HighEntropyPassphrasesTest {
         Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
         Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
         Assert.assertFalse("Expect passphrase isValid false", passphrase.isValid());
+        Assert.assertFalse("Expect passphrase isValid with anagrams false", passphrase.isValid(true));
     }
 
     @Test
@@ -38,6 +40,7 @@ public class HighEntropyPassphrasesTest {
         Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
         Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
         Assert.assertTrue("Expect passphrase isValid true", passphrase.isValid());
+        Assert.assertTrue("Expect passphrase isValid with anagrams true", passphrase.isValid(true));
     }
 
     @Test
@@ -49,6 +52,75 @@ public class HighEntropyPassphrasesTest {
 
         long validPassphraseCount = passphraseList.stream().filter(Passphrase::isValid).count();
         Assert.assertEquals("Expect validPassphraseCount", expectedValidPassphraseCount, validPassphraseCount);
+    }
+
+
+    // --- part 2
+
+    @Test
+    public void passphrase_myTask_countValidPassphrases_withAnagrams() {
+        long expectedValidPassphraseCount = 208;
+        List<String> passphrasesLines = TestUtils.readAllLinesOfClassPathResource("/day04/Passphrases.txt");
+
+        List<Passphrase> passphraseList = HighEntropyPassphrases.parsePassphrases(passphrasesLines);
+
+        long validPassphraseCount = passphraseList.stream().filter(passphrase -> passphrase.isValid(true)).count();
+        Assert.assertEquals("Expect validPassphraseCount", expectedValidPassphraseCount, validPassphraseCount);
+    }
+
+    @Test
+    public void passphrase_abcde_fghij_should_be_valid() {
+        String passphraseStr = "abcde fghij";
+        String[] expectedWords = new String[] { "abcde", "fghij" };
+
+        Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
+        Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
+        Assert.assertTrue("Expect passphrase isValid true", passphrase.isValid());
+        Assert.assertTrue("Expect passphrase isValid with anagrams true", passphrase.isValid(true));
+    }
+
+    @Test
+    public void passphrase_abcde_xyz_ecdab_should_not_be_valid() {
+        String passphraseStr = "abcde xyz ecdab";
+        String[] expectedWords = new String[] { "abcde", "xyz", "ecdab" };
+
+        Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
+        Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
+        Assert.assertTrue("Expect passphrase isValid false", passphrase.isValid());
+        Assert.assertFalse("Expect passphrase isValid with anagrams false", passphrase.isValid(true));
+    }
+
+    @Test
+    public void passphrase_a_ab_abc_abd_abf_abj_should_be_valid() {
+        String passphraseStr = "a ab abc abd abf abj";
+        String[] expectedWords = new String[] { "a", "ab", "abc", "abd", "abf", "abj" };
+
+        Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
+        Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
+        Assert.assertTrue("Expect passphrase isValid true", passphrase.isValid());
+        Assert.assertTrue("Expect passphrase isValid with anagrams true", passphrase.isValid(true));
+    }
+
+    @Test
+    public void passphrase_iiii_oiii_ooii_oooi_oooo_should_be_valid() {
+        String passphraseStr = "iiii oiii ooii oooi oooo";
+        String[] expectedWords = new String[] { "iiii", "oiii", "ooii", "oooi", "oooo" };
+
+        Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
+        Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
+        Assert.assertTrue("Expect passphrase isValid true", passphrase.isValid());
+        Assert.assertTrue("Expect passphrase isValid with anagrams true", passphrase.isValid(true));
+    }
+
+    @Test
+    public void passphrase_oiii_ioii_iioi_iiio_should_be_not_valid() {
+        String passphraseStr = "oiii ioii iioi iiio";
+        String[] expectedWords = new String[] { "oiii", "ioii", "iioi", "iiio" };
+
+        Passphrase passphrase = HighEntropyPassphrases.parsePassphrase(passphraseStr);
+        Assert.assertArrayEquals("Expect passphrase words", expectedWords, passphrase.getWords());
+        Assert.assertTrue("Expect passphrase isValid true", passphrase.isValid());
+        Assert.assertFalse("Expect passphrase isValid with anagrams false", passphrase.isValid(true));
     }
 
 }
