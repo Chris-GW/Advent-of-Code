40c5ac5f39d4a3c3b621743762c748daf1c73bd6
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-07 00:04:59 +0100

    2016 Day 5: How About a Nice Game of Chess? Part 2

--- 2016/src/main/java/de/adventofcode/chrisgw/day05/HowAboutANiceGameOfChess.java
+++ 2016/src/main/java/de/adventofcode/chrisgw/day05/HowAboutANiceGameOfChess.java
@@ -6,13 +6,76 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 
+/**
+ * <pre>
+ * --- Day 5: How About a Nice Game of Chess? ---
+ *
+ * You are faced with a security door designed by Easter Bunny engineers that
+ * seem to have acquired most of their security knowledge by watching hacking movies.
+ *
+ * The eight-character password for the door is generated one character at a
+ * time by finding the MD5 hash of some Door ID (your puzzle input) and an
+ * increasing integer index (starting with 0).
+ *
+ * A hash indicates the next character in the password if its hexadecimal
+ * representation starts with five zeroes. If it does, the sixth character in
+ * the hash is the next character of the password.
+ *
+ * For example, if the Door ID is abc:
+ *
+ * - The first index which produces a hash that starts with five zeroes is
+ *   3231929, which we find by hashing abc3231929; the sixth character of
+ *   the hash, and thus the first character of the password, is 1.
+ * - 5017308 produces the next interesting hash, which starts with
+ *   000008f82..., so the second character of the password is 8.
+ * - The third time a hash starts with five zeroes is for abc5278568,
+ *   discovering the character f.
+ *
+ * In this example, after continuing this search a total of eight times, the
+ * password is 18f47a30.
+ *
+ * Given the actual Door ID, what is the password?
+ *
+ * --- Part Two ---
+ *
+ * As the door slides open, you are presented with asecond door that uses a
+ * slightly more inspired security mechanism. Clearly unimpressed by the last
+ * version (in what movie is the password decrypted in order?!), the Easter
+ * Bunny engineers have worked out a better solution.
+ *
+ * Instead of simply filling in the password from left to right, the hash now
+ * also indicates the position within the password to fill. You still look for
+ * hashes that begin with five zeroes; however, now, the sixth character
+ * represents the position (0-7), and the seventh character is the character
+ * to put in that position.
+ *
+ * A hash result of 000001f means that f is the second character in the
+ * password. Use only the first result for each position, and ignore invalid positions.
+ *
+ * For example, if the Door ID is abc:
+ *
+ * - The first interesting hash is from abc3231929, which produces
+ *   0000015...; so, 5 goes in position 1: _5______.
+ * - In the previous method, 5017308 produced an interesting hash; however,
+ *   it is ignored, because it specifies an invalid position (8).
+ * - The second interesting hash is at index 5357525, which produces
+ *   000004e...; so, e goes in position 4: _5__e___.
+ *
+ * You almost choke on your popcorn as the final character falls into place,
+ * producing the password 05ace8e3.
+ *
+ * Given the actual Door ID and this new method, what is the password? Be
+ * extra proud of your solution if it uses a cinematic "decrypting" animation.
+ * </pre>
+ */
 public class HowAboutANiceGameOfChess {
 
-    public static String generatePassword(String doorId) throws NoSuchAlgorithmException {
-        StringBuilder password = new StringBuilder();
+    public static String generateSecurePassword(String doorId) throws NoSuchAlgorithmException {
+        int passwordLength = 8;
+        StringBuilder password = new StringBuilder(passwordLength);
         MessageDigest md = MessageDigest.getInstance("MD5");
 
-        for (int i = 0; password.length() < 8; i++) {
+        for (int i = 0; password.length() < passwordLength; i++) {
             String nextPassword = doorId + i;
             md.update(nextPassword.getBytes());
             byte[] digest = md.digest();
@@ -26,8 +89,39 @@ public class HowAboutANiceGameOfChess {
         return password.toString().toLowerCase();
     }
 
+    public static String generateBetterSecurePassword(String doorId) throws NoSuchAlgorithmException {
+        int passwordLength = 8;
+        StringBuilder password = new StringBuilder(passwordLength);
+        password.setLength(passwordLength);
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+        for (int i = 0, k = 0; k < passwordLength; i++) {
+            String nextPassword = doorId + i;
+            md.update(nextPassword.getBytes());
+            byte[] digest = md.digest();
+            String passwordHashHexadecimal = DatatypeConverter.printHexBinary(digest);
+
+            if (isValidPasswordHash(passwordHashHexadecimal)) {
+                char positionLetter = passwordHashHexadecimal.charAt(5);
+                char newPasswordLetter = passwordHashHexadecimal.charAt(6);
+                int position = positionLetter - '0';
+
+                if (isValidPasswordPostion(positionLetter) && password.charAt(position) == '\u0000') {
+                    password.setCharAt(position, newPasswordLetter);
+                    k++;
+                }
+            }
+        }
+        return password.toString().toLowerCase();
+    }
+
+
     private static boolean isValidPasswordHash(String passwordHashHexadecimal) {
         return passwordHashHexadecimal.startsWith("00000");
+    }
+
+    private static boolean isValidPasswordPostion(char position) {
+        return '0' <= position && position <= '7';
     }
 
 
