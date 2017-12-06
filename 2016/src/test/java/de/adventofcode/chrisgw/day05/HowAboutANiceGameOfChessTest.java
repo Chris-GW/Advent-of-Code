66f0c7e1d49903402df444c867677bcbb26af721
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-07 00:04:59 +0100

    2016 Day 5: How About a Nice Game of Chess? Part 2

--- 2016/src/test/java/de/adventofcode/chrisgw/day05/HowAboutANiceGameOfChessTest.java
+++ 2016/src/test/java/de/adventofcode/chrisgw/day05/HowAboutANiceGameOfChessTest.java
@@ -3,40 +3,7 @@ package de.adventofcode.chrisgw.day05;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
-
-/**
- * <pre>
- *     --- Day 5: How About a Nice Game of Chess? ---
- *
- * You are faced with a security door designed by Easter Bunny engineers that
- * seem to have acquired most of their security knowledge by watching hacking movies.
- *
- * The eight-character password for the door is generated one character at a
- * time by finding the MD5 hash of some Door ID (your puzzle input) and an
- * increasing integer index (starting with 0).
- *
- * A hash indicates the next character in the password if its hexadecimal
- * representation starts with five zeroes. If it does, the sixth character in
- * the hash is the next character of the password.
- *
- * For example, if the Door ID is abc:
- *
- * - The first index which produces a hash that starts with five zeroes is
- *   3231929, which we find by hashing abc3231929; the sixth character of
- *   the hash, and thus the first character of the password, is 1.
- * - 5017308 produces the next interesting hash, which starts with
- *   000008f82..., so the second character of the password is 8.
- * - The third time a hash starts with five zeroes is for abc5278568,
- *   discovering the character f.
- *
- * In this example, after continuing this search a total of eight times, the
- * password is 18f47a30.
- *
- * Given the actual Door ID, what is the password?
- * </pre>
- */
 public class HowAboutANiceGameOfChessTest {
 
 
@@ -45,7 +12,7 @@ public class HowAboutANiceGameOfChessTest {
         String doorId = "abc";
         String expectedGeneratedPassword = "18f47a30";
 
-        String generatePassword = HowAboutANiceGameOfChess.generatePassword(doorId);
+        String generatePassword = HowAboutANiceGameOfChess.generateSecurePassword(doorId);
         System.out.println(generatePassword);
 
         Assert.assertEquals("Expect generated password", expectedGeneratedPassword, generatePassword);
@@ -56,7 +23,32 @@ public class HowAboutANiceGameOfChessTest {
         String doorId = "wtnhxymk";
         String expectedGeneratedPassword = "2414bc77";
 
-        String generatePassword = HowAboutANiceGameOfChess.generatePassword(doorId);
+        String generatePassword = HowAboutANiceGameOfChess.generateSecurePassword(doorId);
+        System.out.println(generatePassword);
+
+        Assert.assertEquals("Expect generated password", expectedGeneratedPassword, generatePassword);
+    }
+
+
+    // --- part 2
+
+    @Test
+    public void generateBetterSecurePassword_abc() throws Exception {
+        String doorId = "abc";
+        String expectedGeneratedPassword = "05ace8e3";
+
+        String generatePassword = HowAboutANiceGameOfChess.generateBetterSecurePassword(doorId);
+        System.out.println(generatePassword);
+
+        Assert.assertEquals("Expect generated password", expectedGeneratedPassword, generatePassword);
+    }
+
+    @Test
+    public void generateBetterSecurePassword_myTask_wtnhxymk() throws Exception {
+        String doorId = "wtnhxymk";
+        String expectedGeneratedPassword = "437e60fc";
+
+        String generatePassword = HowAboutANiceGameOfChess.generateBetterSecurePassword(doorId);
         System.out.println(generatePassword);
 
         Assert.assertEquals("Expect generated password", expectedGeneratedPassword, generatePassword);
