f92e7a05e160c5099b2926112c2076a2099d9a99
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-13 13:58:23 +0100

    2017 Day 13: Packet Scanners Part 1

---
+++ 2017/src/main/java/de/adventofcode/chrisgw/day13/FirewallScanner.java
@@ -0,0 +1,88 @@
+package de.adventofcode.chrisgw.day13;
+
+public class FirewallScanner {
+
+    private int layer;
+    private int range;
+    private ScanDirection scanDirection = ScanDirection.DOWN;
+    private int position = 0;
+
+
+    public FirewallScanner(int layer, int range) {
+        this.layer = layer;
+        this.range = range;
+    }
+
+
+    public void nextScanStep() {
+        if (range == 1) {
+            // no op
+        } else if (ScanDirection.UP.equals(scanDirection)) {
+            moveUp();
+        } else if (ScanDirection.DOWN.equals(scanDirection)) {
+            moveDown();
+        } else {
+            throw new IllegalStateException("Unknown ScanDirection: " + scanDirection);
+        }
+    }
+
+    private void moveUp() {
+        if (--position < 0) {
+            position = 1;
+            scanDirection = ScanDirection.DOWN;
+        }
+    }
+
+    private void moveDown() {
+        if (++position >= range) {
+            position = range - 2;
+            scanDirection = ScanDirection.UP;
+        }
+    }
+
+
+    public int getSeverity() {
+        return layer * range;
+    }
+
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getLayer() {
+        return layer;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof FirewallScanner))
+            return false;
+
+        FirewallScanner that = (FirewallScanner) o;
+
+        if (getLayer() != that.getLayer())
+            return false;
+        return getRange() == that.getRange();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getLayer();
+        result = 31 * result + getRange();
+        return result;
+    }
+
+
+    private enum ScanDirection {
+        UP, DOWN;
+    }
+
+}
