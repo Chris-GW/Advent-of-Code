617490ec8bdcc5a99266f48f92cd9f005396cb16
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-16 13:00:45 +0100

    2017 Day 15: Dueling Generators part 2

--- 2017/src/main/java/de/adventofcode/chrisgw/day15/DuelingGenerator.java
+++ 2017/src/main/java/de/adventofcode/chrisgw/day15/DuelingGenerator.java
@@ -1,31 +1,39 @@
 package de.adventofcode.chrisgw.day15;
 
+import java.util.function.Predicate;
+
+
 public class DuelingGenerator {
 
     public static final long DIVIDER = 2147483647;
 
-    private long factor;
+    private final long factor;
+    private final Predicate<Integer> generatedValueCriteria;
     private long previousValue;
 
 
-    public DuelingGenerator(long factor, long previousValue) {
+    public DuelingGenerator(long factor, long previousValue, Predicate<Integer> generatedValueCriteria) {
         this.factor = factor;
         this.previousValue = previousValue;
+        this.generatedValueCriteria = generatedValueCriteria;
     }
 
-    public static DuelingGenerator newDuelingGeneratorA(long startValue) {
-        return new DuelingGenerator(16807, startValue);
+    public static DuelingGenerator newDuelingGeneratorA(long startValue, Predicate<Integer> generatedValueCriteria) {
+        return new DuelingGenerator(16807, startValue, generatedValueCriteria);
     }
 
-    public static DuelingGenerator newDuelingGeneratorB(long startValue) {
-        return new DuelingGenerator(48271, startValue);
+    public static DuelingGenerator newDuelingGeneratorB(long startValue, Predicate<Integer> generatedValueCriteria) {
+        return new DuelingGenerator(48271, startValue, generatedValueCriteria);
     }
 
 
     public int generateNextValue() {
-        long product = previousValue * factor;
-        int reminder = (int) (product % DIVIDER);
-        this.previousValue = reminder;
+        int reminder;
+        do {
+            long product = previousValue * factor;
+            reminder = (int) (product % DIVIDER);
+            this.previousValue = reminder;
+        } while (!generatedValueCriteria.test(reminder));
         return reminder;
     }
 
@@ -36,6 +44,10 @@ public class DuelingGenerator {
 
     public long getPreviousValue() {
         return previousValue;
+    }
+
+    public Predicate<Integer> getGeneratedValueCriteria() {
+        return generatedValueCriteria;
     }
 
 }
--- 2017/src/test/java/de/adventofcode/chrisgw/day15/DuelingGeneratorsTest.java
+++ 2017/src/test/java/de/adventofcode/chrisgw/day15/DuelingGeneratorsTest.java
@@ -11,27 +11,24 @@ public class DuelingGeneratorsTest {
 
     @Test
     public void duelingGeneratorA_startValue_65() {
-        int startValue = 65;
-        List<Long> expectedGeneratedValues = Arrays.asList(1092455L, 1181022009L, 245556042L, 1744312007L, 1352636452L);
+        List<Integer> expectedGeneratedValues = Arrays.asList(1092455, 1181022009, 245556042, 1744312007, 1352636452);
 
-        DuelingGenerator duelingGeneratorA = DuelingGenerator.newDuelingGeneratorA(startValue);
+        DuelingGenerator duelingGeneratorA = newDuelingGenerators_part1().getDuelingGeneratorA();
         for (int i = 0; i < expectedGeneratedValues.size(); i++) {
-            long nextGeneratedValue = duelingGeneratorA.generateNextValue();
-            long expectedGeneratedValue = expectedGeneratedValues.get(i);
+            int nextGeneratedValue = duelingGeneratorA.generateNextValue();
+            int expectedGeneratedValue = expectedGeneratedValues.get(i);
             Assert.assertEquals("Expect next generated value " + i, expectedGeneratedValue, nextGeneratedValue);
         }
     }
 
     @Test
     public void duelingGeneratorB_startValue_8921() {
-        int startValue = 8921;
-        List<Long> expectedGeneratedValues = Arrays.asList(430625591L, 1233683848L, 1431495498L, 137874439L,
-                285222916L);
+        List<Integer> expectedGeneratedValues = Arrays.asList(430625591, 1233683848, 1431495498, 137874439, 285222916);
 
-        DuelingGenerator duelingGeneratorB = DuelingGenerator.newDuelingGeneratorB(startValue);
+        DuelingGenerator duelingGeneratorB = newDuelingGenerators_part1().getDuelingGeneratorB();
         for (int i = 0; i < expectedGeneratedValues.size(); i++) {
-            long nextGeneratedValue = duelingGeneratorB.generateNextValue();
-            long expectedGeneratedValue = expectedGeneratedValues.get(i);
+            int nextGeneratedValue = duelingGeneratorB.generateNextValue();
+            int expectedGeneratedValue = expectedGeneratedValues.get(i);
             Assert.assertEquals("Expect next generated value " + i, expectedGeneratedValue, nextGeneratedValue);
         }
     }
@@ -40,11 +37,9 @@ public class DuelingGeneratorsTest {
     @Test
     public void duelingGenerators_expect_totalTairCount_588() {
         long rounds = 40_000_000;
-        long startValueA = 65;
-        long startValueB = 8921;
         int expectedPairCount = 588;
 
-        DuelingGenerators duelingGenerators = DuelingGenerators.createDuelingGenerators(startValueA, startValueB);
+        DuelingGenerators duelingGenerators = newDuelingGenerators_part1();
         for (long round = 1; round <= rounds; round++) {
             duelingGenerators.nextRound();
         }
@@ -60,7 +55,9 @@ public class DuelingGeneratorsTest {
         long startValueB = 190;
         int expectedPairCount = 597;
 
-        DuelingGenerators duelingGenerators = DuelingGenerators.createDuelingGenerators(startValueA, startValueB);
+        DuelingGenerator duelingGeneratorA = DuelingGenerator.newDuelingGeneratorA(startValueA, (value) -> true);
+        DuelingGenerator duelingGeneratorB = DuelingGenerator.newDuelingGeneratorB(startValueB, (value) -> true);
+        DuelingGenerators duelingGenerators = new DuelingGenerators(duelingGeneratorA, duelingGeneratorB);
         for (long round = 1; round <= rounds; round++) {
             duelingGenerators.nextRound();
         }
@@ -68,5 +65,81 @@ public class DuelingGeneratorsTest {
         long matchCount = duelingGenerators.getMatchCount();
         Assert.assertEquals("expect pair count after " + rounds, expectedPairCount, matchCount);
     }
+
+
+    // --- part 2
+
+    @Test
+    public void duelingGeneratorA_startValue_65_part2() {
+        List<Integer> expectedGeneratedValues = Arrays.asList(1352636452, 1992081072, 530830436, 1980017072, 740335192);
+
+        DuelingGenerator duelingGeneratorA = newDuelingGenerators_part2().getDuelingGeneratorA();
+        for (int i = 0; i < expectedGeneratedValues.size(); i++) {
+            int nextGeneratedValue = duelingGeneratorA.generateNextValue();
+            int expectedGeneratedValue = expectedGeneratedValues.get(i);
+            Assert.assertEquals("Expect next generated value " + i, expectedGeneratedValue, nextGeneratedValue);
+        }
+    }
+
+    @Test
+    public void duelingGeneratorB_startValue_8921_part2() {
+        List<Integer> expectedGeneratedValues = Arrays.asList(1233683848, 862516352, 1159784568, 1616057672, 412269392);
+
+        DuelingGenerator duelingGeneratorB = newDuelingGenerators_part2().getDuelingGeneratorB();
+        for (int i = 0; i < expectedGeneratedValues.size(); i++) {
+            int nextGeneratedValue = duelingGeneratorB.generateNextValue();
+            int expectedGeneratedValue = expectedGeneratedValues.get(i);
+            Assert.assertEquals("Expect next generated value " + i, expectedGeneratedValue, nextGeneratedValue);
+        }
+    }
+
+
+    @Test
+    public void duelingGenerators_part2_expect_totalTairCount_309() {
+        long rounds = 5_000_000;
+        int expectedPairCount = 309;
+
+        DuelingGenerators duelingGenerators = newDuelingGenerators_part2();
+        for (long round = 1; round <= rounds; round++) {
+            duelingGenerators.nextRound();
+        }
+
+        long matchCount = duelingGenerators.getMatchCount();
+        Assert.assertEquals("expect pair count after " + rounds, expectedPairCount, matchCount);
+    }
+
+    @Test
+    public void duelingGenerators_myTask_part2() {
+        long rounds = 5_000_000;
+        long startValueA = 516;
+        long startValueB = 190;
+        int expectedPairCount = 303;
+
+        DuelingGenerator duelingGeneratorA = DuelingGenerator.newDuelingGeneratorA(startValueA,
+                (value) -> value % 4 == 0);
+        DuelingGenerator duelingGeneratorB = DuelingGenerator.newDuelingGeneratorB(startValueB,
+                (value) -> value % 8 == 0);
+        DuelingGenerators duelingGenerators = new DuelingGenerators(duelingGeneratorA, duelingGeneratorB);
+        for (long round = 1; round <= rounds; round++) {
+            duelingGenerators.nextRound();
+        }
+
+        long matchCount = duelingGenerators.getMatchCount();
+        Assert.assertEquals("expect pair count after " + rounds, expectedPairCount, matchCount);
+    }
+
+
+    public static DuelingGenerators newDuelingGenerators_part1() {
+        DuelingGenerator duelingGeneratorA = DuelingGenerator.newDuelingGeneratorA(65, (value) -> true);
+        DuelingGenerator duelingGeneratorB = DuelingGenerator.newDuelingGeneratorB(8921, (value) -> true);
+        return new DuelingGenerators(duelingGeneratorA, duelingGeneratorB);
+    }
+
+    public static DuelingGenerators newDuelingGenerators_part2() {
+        DuelingGenerator duelingGeneratorA = DuelingGenerator.newDuelingGeneratorA(65, (value) -> value % 4 == 0);
+        DuelingGenerator duelingGeneratorB = DuelingGenerator.newDuelingGeneratorB(8921, (value) -> value % 8 == 0);
+        return new DuelingGenerators(duelingGeneratorA, duelingGeneratorB);
+    }
+
 
 }
