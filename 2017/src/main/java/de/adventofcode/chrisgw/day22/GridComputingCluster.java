5c98f1508f4ed68d418825eb30fdba18a36c4098
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-23 21:54:45 +0100

    2017 day 22: Sporifica Virus part 2

--- 2017/src/main/java/de/adventofcode/chrisgw/day22/GridComputingCluster.java
+++ 2017/src/main/java/de/adventofcode/chrisgw/day22/GridComputingCluster.java
@@ -1,59 +1,63 @@
 package de.adventofcode.chrisgw.day22;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 
 public class GridComputingCluster {
 
 
-    private Map<Integer, Map<Integer, Boolean>> positionToComputingNode;
+    private Map<Integer, Map<Integer, ComputingNodeStatus>> positionToComputingNode;
 
 
-    private GridComputingCluster(Map<Integer, Map<Integer, Boolean>> positionToComputingNode) {
+    private GridComputingCluster(Map<Integer, Map<Integer, ComputingNodeStatus>> positionToComputingNode) {
         this.positionToComputingNode = positionToComputingNode;
     }
 
     public static GridComputingCluster parseComputingNodeMap(List<String> computingNodeMapLines) {
-        Map<Integer, Map<Integer, Boolean>> positionToComputingNode = new TreeMap<>();
+        Map<Integer, Map<Integer, ComputingNodeStatus>> positionToComputingNode = new HashMap<>();
         int originDistance = computingNodeMapLines.size() / 2;
 
         for (int row = 0; row < computingNodeMapLines.size(); row++) {
             String computingNodeMapLine = computingNodeMapLines.get(row);
             int y = originDistance - row;
-            Map<Integer, Boolean> computingNodeRow = positionToComputingNode.computeIfAbsent(y, (p) -> new TreeMap<>());
+            Map<Integer, ComputingNodeStatus> computingNodeRow = positionToComputingNode.computeIfAbsent(y,
+                    (p) -> new HashMap<>());
 
             for (int column = 0; column < computingNodeMapLine.length(); column++) {
                 char nodeInfectedLetter = computingNodeMapLine.charAt(column);
                 int x = column - originDistance;
-                computingNodeRow.put(x, nodeInfectedLetter == '#');
+                if (nodeInfectedLetter == '#') {
+                    computingNodeRow.put(x, ComputingNodeStatus.INFECTED);
+                }
             }
         }
         return new GridComputingCluster(positionToComputingNode);
     }
 
 
-    public boolean getNodeStatus(int x, int y) {
-        Map<Integer, Boolean> computingNodeRow = getNodeRow(y);
-        return computingNodeRow.getOrDefault(x, false);
+    public ComputingNodeStatus getNodeStatus(int x, int y) {
+        Map<Integer, ComputingNodeStatus> computingNodeRow = getNodeRow(y);
+        return computingNodeRow.getOrDefault(x, ComputingNodeStatus.CLEAN);
     }
 
-    public void setNodeStatus(int x, int y, boolean infected) {
-        Map<Integer, Boolean> computingNodeRow = getNodeRow(y);
-        computingNodeRow.put(x, infected);
+    public void setNodeStatus(int x, int y, ComputingNodeStatus nodeStatus) {
+        Map<Integer, ComputingNodeStatus> computingNodeRow = getNodeRow(y);
+        computingNodeRow.put(x, nodeStatus);
     }
 
-    private Map<Integer, Boolean> getNodeRow(int y) {
-        return positionToComputingNode.computeIfAbsent(y, (p) -> new TreeMap<>());
+    private Map<Integer, ComputingNodeStatus> getNodeRow(int y) {
+        return positionToComputingNode.computeIfAbsent(y, (p) -> new HashMap<>());
     }
+
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int y = 4; y >= -4; y--) {
             for (int x = -4; x <= 4; x++) {
-                if (getNodeStatus(x, y)) {
+                if (getNodeStatus(x, y).equals(ComputingNodeStatus.INFECTED)) {
                     sb.append('#');
                 } else {
                     sb.append('.');
