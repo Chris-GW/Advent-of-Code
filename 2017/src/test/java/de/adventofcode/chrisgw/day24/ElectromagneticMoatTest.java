26b0730d9a05ab6b1f79d190584deb444fabf3bb
Chris-GW <Chris-GW@users.noreply.github.com>
2017-12-28 16:31:26 +0100

    2017 day 24: Electromagnetic Moat part 2

--- 2017/src/test/java/de/adventofcode/chrisgw/day24/ElectromagneticMoatTest.java
+++ 2017/src/test/java/de/adventofcode/chrisgw/day24/ElectromagneticMoatTest.java
@@ -28,7 +28,7 @@ public class ElectromagneticMoatTest {
         expectedBridge.addComponent(new BridgeComponent(1, 10));
         expectedBridge.addComponent(new BridgeComponent(10, 9));
 
-        ElectromagneticMoat bestBridge = ElectromagneticMoat.buildBestBridge(aviableComponents);
+        ElectromagneticMoat bestBridge = ElectromagneticMoat.buildStrongestBridge(aviableComponents);
 
         Assert.assertEquals("expect best bridge", expectedBridge, bestBridge);
     }
@@ -42,10 +42,51 @@ public class ElectromagneticMoatTest {
                 .collect(Collectors.toList());
         int expectedBridgeStrehngth = 2006;
 
-        ElectromagneticMoat bestBridge = ElectromagneticMoat.buildBestBridge(aviableComponents);
+        ElectromagneticMoat bestBridge = ElectromagneticMoat.buildStrongestBridge(aviableComponents);
         int bridgeStrength = bestBridge.getStrength();
 
         Assert.assertEquals("expect best bridge strength", expectedBridgeStrehngth, bridgeStrength);
+    }
+
+
+    @Test
+    public void electromagneticMoat_part2_example() {
+        List<BridgeComponent> aviableComponents = Stream.of( //
+                "0/2", //
+                "2/2", //
+                "2/3", //
+                "3/4", //
+                "3/5", //
+                "0/1", //
+                "10/1", //
+                "9/10").map(BridgeComponent::parseComponent).collect(Collectors.toList());
+        ElectromagneticMoat expectedBridge = new ElectromagneticMoat();
+        expectedBridge.addComponent(new BridgeComponent(0, 2));
+        expectedBridge.addComponent(new BridgeComponent(2, 2));
+        expectedBridge.addComponent(new BridgeComponent(2, 3));
+        expectedBridge.addComponent(new BridgeComponent(3, 5));
+
+        ElectromagneticMoat bestBridge = ElectromagneticMoat.buildLongestAndSrongestBridge(aviableComponents);
+
+        Assert.assertEquals("expect best bridge", expectedBridge, bestBridge);
+    }
+
+    @Test
+    public void electromagneticMoat_part2_myTask() {
+        String classpathResource = "/day24/ElectromagneticMoat_chrisgw.txt";
+        List<BridgeComponent> aviableComponents = TestUtils.readAllLinesOfClassPathResource(classpathResource)
+                .stream()
+                .map(BridgeComponent::parseComponent)
+                .collect(Collectors.toList());
+        int expectedBridgeLength = 35;
+        int expectedBridgeStrength = 1994;
+
+        ElectromagneticMoat bestBridge = ElectromagneticMoat.buildLongestAndSrongestBridge(aviableComponents);
+        int bridgeLength = bestBridge.getLength();
+        int bridgeStrength = bestBridge.getStrength();
+
+        Assert.assertEquals("expect best bridge length", expectedBridgeLength, bridgeLength);
+        Assert.assertEquals("expect best bridge strength", expectedBridgeStrength, bridgeStrength);
     }
 
 }
