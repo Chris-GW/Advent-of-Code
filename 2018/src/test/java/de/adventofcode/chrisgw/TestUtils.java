1aea12ae8de610a79ecc94cf93b39a40b4980cda
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-02 18:10:57 +0100

    2018 Day 1: Chronal Calibration part 1

---
+++ 2018/src/test/java/de/adventofcode/chrisgw/TestUtils.java
@@ -0,0 +1,28 @@
+package de.adventofcode.chrisgw;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+public class TestUtils {
+
+
+    public static List<String> readAllLinesOfClassPathResource(String classpathResource) {
+        try {
+            return Files.readAllLines(getResourcePath(classpathResource));
+        } catch (Exception e) {
+            throw new RuntimeException("Could not read all lines of classpath resource: " + classpathResource, e);
+        }
+    }
+
+    public static Path getResourcePath(String classpathResource) throws URISyntaxException {
+        URL classpathResourceURL = TestUtils.class.getResource(classpathResource);
+        return Paths.get(classpathResourceURL.toURI());
+    }
+
+
+}
