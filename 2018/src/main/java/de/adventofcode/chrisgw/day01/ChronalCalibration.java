b31d22817627ebcea3db3d3cd3018c33840aa852
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-02 18:34:50 +0100

    2018 Day 1: Chronal Calibration part 2

--- 2018/src/main/java/de/adventofcode/chrisgw/day01/ChronalCalibration.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day01/ChronalCalibration.java
@@ -3,11 +3,14 @@ package de.adventofcode.chrisgw.day01;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 
 public class ChronalCalibration {
 
+    // part 01
 
     public static long calculateFrequency(Path frequenciesDataPath) throws IOException {
         IntStream frequenciesStream = Files.lines(frequenciesDataPath).mapToInt(Integer::parseInt);
@@ -18,6 +21,29 @@ public class ChronalCalibration {
         return frequencies.parallel().sum();
     }
 
+
+    // part 02
+
+    public static long calculateFirstRepeatedFrequency(Path frequenciesDataPath) throws IOException {
+        IntStream frequenciesStream = Files.lines(frequenciesDataPath).mapToInt(Integer::parseInt);
+        return calculateFirstRepeatedFrequency(frequenciesStream);
+    }
+
+    public static long calculateFirstRepeatedFrequency(IntStream frequenciesStream) {
+        int currentFrequency = 0;
+        int[] frequencies = frequenciesStream.toArray();
+        Set<Integer> resultingFrequencies = new HashSet<>();
+        resultingFrequencies.add(0);
+
+        while (true) {
+            for (int frequencyChange : frequencies) {
+                currentFrequency += frequencyChange;
+                if (!resultingFrequencies.add(currentFrequency)) {
+                    return currentFrequency;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         if (args.length == 0) {
