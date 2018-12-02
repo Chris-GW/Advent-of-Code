483ee131f10e182fa8cdbba7e12ddcb239fca086
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-02 18:34:50 +0100

    2018 Day 1: Chronal Calibration part 2

--- 2018/src/test/java/de/adventofcode/chrisgw/day01/ChronalCalibrationTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day01/ChronalCalibrationTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.*;
 
 public class ChronalCalibrationTest {
 
+    // part 01
 
     @Test
     public void calculateFrequency_1_1_1_should_be_3() throws Exception {
@@ -46,6 +47,61 @@ public class ChronalCalibrationTest {
 
         long actualFrequency = ChronalCalibration.calculateFrequency(frequenciesDataPath);
         assertEquals("resulting frequency", expectedFrequency, actualFrequency);
+    }
+
+
+    // part 02
+
+
+    @Test
+    public void calculateFirstRepeatedFrequency_example_01() {
+        long expectedFrequency = 2;
+        IntStream frequencies = IntStream.of(1, -2, 3, 1, 1, -2);
+        long repeatedFrequency = ChronalCalibration.calculateFirstRepeatedFrequency(frequencies);
+        assertEquals("first repeated frequency", expectedFrequency, repeatedFrequency);
+    }
+
+    @Test
+    public void calculateFirstRepeatedFrequency_example_02() {
+        long expectedFrequency = 0;
+        IntStream frequencies = IntStream.of(1, -1);
+        long repeatedFrequency = ChronalCalibration.calculateFirstRepeatedFrequency(frequencies);
+        assertEquals("first repeated frequency", expectedFrequency, repeatedFrequency);
+    }
+
+    @Test
+    public void calculateFirstRepeatedFrequency_example_03() {
+        long expectedFrequency = 10;
+        IntStream frequencies = IntStream.of(+3, +3, +4, -2, -4);
+        long repeatedFrequency = ChronalCalibration.calculateFirstRepeatedFrequency(frequencies);
+        assertEquals("first repeated frequency", expectedFrequency, repeatedFrequency);
+    }
+
+    @Test
+    public void calculateFirstRepeatedFrequency_example_04() {
+        long expectedFrequency = 5;
+        IntStream frequencies = IntStream.of(-6, +3, +8, +5, -6);
+        long repeatedFrequency = ChronalCalibration.calculateFirstRepeatedFrequency(frequencies);
+        assertEquals("first repeated frequency", expectedFrequency, repeatedFrequency);
+    }
+
+    @Test
+    public void calculateFirstRepeatedFrequency_example_05() {
+        long expectedFrequency = 14;
+        IntStream frequencies = IntStream.of(+7, +7, -2, -7, -4);
+        long repeatedFrequency = ChronalCalibration.calculateFirstRepeatedFrequency(frequencies);
+        assertEquals("first repeated frequency", expectedFrequency, repeatedFrequency);
+    }
+
+
+    @Test
+    public void calculateFirstRepeatedFrequency_myTask() throws Exception {
+        long expectedFrequency = 71892;
+        Path frequenciesDataPath = TestUtils.getResourcePath("/day01/frequenciesData.txt");
+        System.out.println(frequenciesDataPath.toAbsolutePath());
+
+        long actualFrequency = ChronalCalibration.calculateFirstRepeatedFrequency(frequenciesDataPath);
+        assertEquals("first repeated frequency", expectedFrequency, actualFrequency);
     }
 
 }
