13c7bc9273b7633fa8fbe496ceb5a2441aca89e1
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-03 10:35:43 +0100

    2018 Day 3: No Matter How You Slice It - part 01

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day02/CutRectangle.java
@@ -0,0 +1,106 @@
+package de.adventofcode.chrisgw.day02;
+
+import de.adventofcode.chrisgw.day02.CutRectangle.SquarePositon;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+
+@Value
+@Builder
+public class CutRectangle implements Iterable<SquarePositon> {
+
+    public static final Pattern RECTANGLE_SPECIFICATION_PATTERN = Pattern.compile(
+            "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)");
+
+    final long id;
+    final int distanceLeft;
+    final int distanceTop;
+    final int wide;
+    final int tall;
+
+
+    public static CutRectangle parse(String rectangleSpecificationString) {
+        Matcher matcher = RECTANGLE_SPECIFICATION_PATTERN.matcher(rectangleSpecificationString);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(
+                    "Expect rectangleSpecification to match Pattern: " + RECTANGLE_SPECIFICATION_PATTERN);
+        }
+        long id = Long.parseLong(matcher.group(1));
+        int distanceLeft = Integer.parseInt(matcher.group(2));
+        int distanceTop = Integer.parseInt(matcher.group(3));
+        int wide = Integer.parseInt(matcher.group(4));
+        int tall = Integer.parseInt(matcher.group(5));
+        return CutRectangle.builder()
+                .id(id)
+                .distanceLeft(distanceLeft)
+                .distanceTop(distanceTop)
+                .wide(wide)
+                .tall(tall)
+                .build();
+    }
+
+    public SquarePositon topLeft() {
+        return new SquarePositon(distanceLeft, distanceTop);
+    }
+
+    public SquarePositon bottomRight() {
+        return new SquarePositon(distanceLeft + wide, distanceTop + tall);
+    }
+
+    public int squares() {
+        return wide * tall;
+    }
+
+
+    public boolean containsPosition(SquarePositon squarePositon) {
+        int x = squarePositon.getX();
+        int y = squarePositon.getY();
+        return distanceLeft <= x && x <= distanceLeft + wide && //
+                distanceTop <= y && y <= distanceTop + tall;
+    }
+
+
+    public Stream<SquarePositon> squarePositons() {
+        Stream.Builder<SquarePositon> squarePositionStreamBuilder = Stream.builder();
+        for (int x = distanceLeft; x < distanceLeft + wide; x++) {
+            for (int y = distanceTop; y < distanceTop + tall; y++) {
+                squarePositionStreamBuilder.add(new SquarePositon(x, y));
+            }
+        }
+        return squarePositionStreamBuilder.build();
+    }
+
+    @Override
+    public Iterator<SquarePositon> iterator() {
+        return squarePositons().iterator();
+    }
+
+
+    public char rectangleLetter() {
+        if (id < 10) {
+            return (char) ('0' + id);
+        } else {
+            return (char) ('A' + id - 10);
+        }
+    }
+
+
+    @Value
+    public static class SquarePositon {
+
+        public final int x;
+        public final int y;
+
+
+        public String toString() {
+            return "(" + x + ";" + y + ")";
+        }
+
+    }
+
+}
--- 2018/src/test/java/de/adventofcode/chrisgw/day02/NoMatterHowYouSliceItTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day02/NoMatterHowYouSliceItTest.java
@@ -1,6 +1,13 @@
 package de.adventofcode.chrisgw.day02;
 
+import de.adventofcode.chrisgw.TestUtils;
 import org.junit.Test;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +15,52 @@ import static org.junit.Assert.*;
 public class NoMatterHowYouSliceItTest {
 
     @Test
-    public void name() {
+    public void parseCutRectangle() {
+        CutRectangle expectedCutRectangle = CutRectangle.builder()
+                .id(123)
+                .distanceLeft(3)
+                .distanceTop(2)
+                .wide(5)
+                .tall(4)
+                .build();
+        String rectangleSpecification = "#123 @ 3,2: 5x4";
 
+        CutRectangle cutRectangle = CutRectangle.parse(rectangleSpecification);
+        assertEquals(rectangleSpecification, expectedCutRectangle, cutRectangle);
+
+        System.out.println(new NoMatterHowYouSliceIt(Collections.singletonList(cutRectangle)));
+    }
+
+
+    @Test
+    public void part_01_countOverlappingSquares_example_01() {
+        long expectedOverlappingSquareCount = 4;
+        String[] cutRectangleSpecifications = new String[] { //
+                "#1 @ 1,3: 4x4", //
+                "#2 @ 3,1: 4x4", //
+                "#3 @ 5,5: 2x2" };
+        Collection<CutRectangle> cutRectangles = Arrays.stream(cutRectangleSpecifications)
+                .map(CutRectangle::parse)
+                .collect(Collectors.toList());
+
+        NoMatterHowYouSliceIt noMatterHowYouSliceIt = new NoMatterHowYouSliceIt(cutRectangles);
+        System.out.println(noMatterHowYouSliceIt);
+        long overlappingSquareCount = noMatterHowYouSliceIt.countOverlappingSquares();
+
+        assertEquals(expectedOverlappingSquareCount, overlappingSquareCount);
+    }
+
+
+    @Test
+    public void part_01_countOverlappingSquares_myTask() throws Exception {
+        long expectedOverlappingSquareCount = 110389;
+        Path rectangleSpecificationFile = TestUtils.getResourcePath("/day02/cutRectangleSpecifications.txt");
+
+        NoMatterHowYouSliceIt noMatterHowYouSliceIt = NoMatterHowYouSliceIt.fromRectangeSpecificationFile(
+                rectangleSpecificationFile);
+        long overlappingSquareCount = noMatterHowYouSliceIt.countOverlappingSquares();
+
+        assertEquals(expectedOverlappingSquareCount, overlappingSquareCount);
     }
 
 }
