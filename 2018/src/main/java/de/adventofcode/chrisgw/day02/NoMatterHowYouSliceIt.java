bc68947a8b33bf73f45e999592e38fb75bf2a066
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-03 09:12:18 +0100

    2018 Day 3: No Matter How You Slice It - part 01 start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day02/NoMatterHowYouSliceIt.java
@@ -0,0 +1,14 @@
+package de.adventofcode.chrisgw.day02;
+
+public class NoMatterHowYouSliceIt {
+
+
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No path in args[]");
+            return;
+        }
+    }
+
+}
---
+++ 2018/src/test/java/de/adventofcode/chrisgw/day02/NoMatterHowYouSliceItTest.java
@@ -0,0 +1,15 @@
+package de.adventofcode.chrisgw.day02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class NoMatterHowYouSliceItTest {
+
+    @Test
+    public void name() {
+
+    }
+
+}
