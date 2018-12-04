216fe8f8c5c212222c48e1ff9abe7c9d2688589d
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-04 08:37:52 +0100

    2018 Day 4: Repose Record - part 01 start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day04/ReposeRecord.java
@@ -0,0 +1,25 @@
+package de.adventofcode.chrisgw.day04;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class ReposeRecord {
+
+    // part 01
+
+
+
+    // part 02
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("No path in args[]");
+            return;
+        }
+        Path boxIdsFile = Paths.get(args[0]);
+
+    }
+
+}
