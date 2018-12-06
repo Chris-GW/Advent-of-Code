31d62e0a1c2c34c5795d72be373936410ac45d01
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-06 12:29:44 +0100

    2018 Day 6: Chronal Coordinates - part 01

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day06/PlaceCoordinate.java
@@ -0,0 +1,77 @@
+package de.adventofcode.chrisgw.day06;
+
+import lombok.Value;
+
+import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
+
+
+@Value
+public class PlaceCoordinate {
+
+    private final int id;
+    private Coordinate coordinate;
+
+
+    public PlaceCoordinate(int id, int x, int y) {
+        this.id = id;
+        this.coordinate = new Coordinate(x, y);
+    }
+
+    public boolean isAtCoordinate(PlaceCoordinate otherPlaceCoordinate) {
+        return isAtCoordinate(otherPlaceCoordinate.getCoordinate());
+    }
+
+    public boolean isAtCoordinate(Coordinate otherCoordinate) {
+        return isAtCoordinate(otherCoordinate.getX(), otherCoordinate.getY());
+    }
+
+    public boolean isAtCoordinate(int x, int y) {
+        return getX() == x && getY() == y;
+    }
+
+    public Stream<Coordinate> coordinatesWithinDistance(int distance) {
+        Builder<Coordinate> coordinateStreamBuilder = Stream.builder();
+        for (int dy = -distance; dy <= distance; dy++) {
+            for (int dx = -distance; dx <= distance; dx++) {
+                Coordinate coordinate = new Coordinate(getX() + dx, getY() + dy);
+                if (this.coordinate.distanceTo(coordinate) == distance) {
+                    coordinateStreamBuilder.add(coordinate);
+                }
+            }
+        }
+        return coordinateStreamBuilder.build();
+    }
+
+
+    public int getX() {
+        return getCoordinate().getX();
+    }
+
+    public int getY() {
+        return getCoordinate().getY();
+    }
+
+
+    @Value
+    public static class Coordinate {
+
+        private final int x;
+        private final int y;
+
+
+        public int distanceTo(Coordinate coordinate) {
+            int distanceX = Math.abs(this.x - coordinate.getX());
+            int distanceY = Math.abs(this.y - coordinate.getY());
+            return distanceX + distanceY;
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format("(%2d;%2d)", x, y);
+        }
+
+    }
+
+}
