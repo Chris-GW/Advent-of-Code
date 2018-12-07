a4ca2ad95601a91af734cee3c6cf4c4912af561e
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-07 11:24:47 +0100

    2018 Day 7: The Sum of Its Parts - part 02 finished

--- 2018/src/main/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPart.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPart.java
@@ -1,69 +1,153 @@
 package de.adventofcode.chrisgw.day07;
 
+import lombok.Value;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
 public class TheSumOfOtsPart {
 
-    private final Map<Character, Step> letterToStep;
-    private final Step firstStep;
+    private final Map<Character, TaskStep> letterToStep;
 
-    private TheSumOfOtsPart(Map<Character, Step> letterToStep) {
+    private TheSumOfOtsPart(Map<Character, TaskStep> letterToStep) {
         this.letterToStep = new HashMap<>(letterToStep);
-        this.firstStep = letterToStep.values().stream().filter(Step::isFirstStep).findAny().orElse(null);
     }
 
-    public static TheSumOfOtsPart fromStepInstructions(Path stepInstructionFile) throws IOException {
-        return fromStepInstructions(Files.lines(stepInstructionFile));
+    public static TheSumOfOtsPart fromStepInstructions(Path stepInstructionFile, Duration workDuration)
+            throws IOException {
+        return fromStepInstructions(Files.lines(stepInstructionFile), workDuration);
     }
 
-    public static TheSumOfOtsPart fromStepInstructions(Stream<String> stepInstructions) {
-        Map<Character, Step> letterToStep = new HashMap<>('Z' - 'A');
+    public static TheSumOfOtsPart fromStepInstructions(Stream<String> stepInstructions, Duration workDuration) {
+        Map<Character, TaskStep> letterToStep = new HashMap<>('Z' - 'A');
 
         Iterator<String> stepInstructionIterator = stepInstructions.iterator();
         while (stepInstructionIterator.hasNext()) {
             String stepInstruction = stepInstructionIterator.next();
-            Step step = parseStep(letterToStep, stepInstruction);
-            Step requierdStep = parseRequierdStep(letterToStep, stepInstruction);
-            step.addRequierdStep(requierdStep);
+
+            char stepLetter = parseStepLetter(stepInstruction);
+            TaskStep taskStep = letterToStep.computeIfAbsent(stepLetter,
+                    (letter) -> new TaskStep(letter, workDuration));
+
+            char requierdStepLetter = parseRequierdStepLetter(stepInstruction);
+            TaskStep requierdTaskStep = letterToStep.computeIfAbsent(requierdStepLetter,
+                    (letter) -> new TaskStep(letter, workDuration));
+
+            taskStep.addRequierdStep(requierdTaskStep);
         }
 
         return new TheSumOfOtsPart(letterToStep);
     }
 
-    private static Step parseStep(Map<Character, Step> letterToStep, String stepInstruction) {
-        char stepLetter = stepInstruction.charAt("Step P must be finished before step ".length());
-        return letterToStep.computeIfAbsent(stepLetter, Step::new);
+    private static char parseStepLetter(String stepInstruction) {
+        return stepInstruction.charAt("Step P must be finished before step ".length());
     }
 
-    private static Step parseRequierdStep(Map<Character, Step> letterToStep, String stepInstruction) {
-        char stepLetter = stepInstruction.charAt("Step ".length());
-        return letterToStep.computeIfAbsent(stepLetter, Step::new);
+    private static char parseRequierdStepLetter(String stepInstruction) {
+        return stepInstruction.charAt("Step ".length());
     }
 
 
-    public Stream<Step> getStepCompletionOrder() {
-        Set<Step> stepCompletionOrder = new LinkedHashSet<>(letterToStep.size());
-        stepCompletionOrder.add(firstStep);
-        while (stepCompletionOrder.size() < letterToStep.size()) {
-            Optional<Step> nextStep = findNextStep(stepCompletionOrder);
-            nextStep.ifPresent(stepCompletionOrder::add);
+    public StepComplitionResult calculateStepCompletionOrder() {
+        return calculateStepCompletionOrder(1);
+    }
+
+    public StepComplitionResult calculateStepCompletionOrder(int aviableWorkers) {
+        Set<TaskStep> taskStepCompletionOrder = new LinkedHashSet<>(letterToStep.size());
+        List<ElveWorker> elveWorkers = createElveWorkers(aviableWorkers);
+
+        System.out.println(formatCompletionOrderHeader(elveWorkers));
+        Duration elapsedDuration;
+        for (elapsedDuration = Duration.ZERO; //
+             taskStepCompletionOrder.size() < letterToStep.size(); //
+             elapsedDuration = elapsedDuration.plusSeconds(1)) {
+
+            assignElveWorkerWithTasks(elveWorkers, taskStepCompletionOrder);
+            elveWorkers.forEach(ElveWorker::doWork);
+            System.out.println(formatCompletionOrder(elapsedDuration, elveWorkers, taskStepCompletionOrder));
         }
-        return stepCompletionOrder.stream();
+        elapsedDuration = elapsedDuration.minusSeconds(1);
+
+        List<TaskStep> completionOrder = new ArrayList<>(taskStepCompletionOrder);
+        return new StepComplitionResult(aviableWorkers, elapsedDuration, completionOrder);
     }
 
-    private Optional<Step> findNextStep(Set<Step> stepCompletionOrder) {
+    private CharSequence formatCompletionOrder(Duration elapsedDuration, List<ElveWorker> elveWorkers,
+            Set<TaskStep> taskStepCompletionOrder) {
+        StringBuilder sb = new StringBuilder();
+
+        long elapsedSeconds = TimeUnit.MILLISECONDS.toSeconds(elapsedDuration.toMillis());
+        sb.append(String.format("%6d\t", elapsedSeconds));
+
+        for (ElveWorker elveWorker : elveWorkers) {
+            TaskStep assignedTaskStep = elveWorker.getAssignedTaskStep();
+            char assignedTaskLetter = assignedTaskStep != null ? assignedTaskStep.getLetter() : '-';
+            sb.append(assignedTaskLetter).append("\t");
+        }
+
+        for (TaskStep taskStep : taskStepCompletionOrder) {
+            sb.append(taskStep.getLetter());
+        }
+        return sb;
+    }
+
+    private static CharSequence formatCompletionOrderHeader(List<ElveWorker> elveWorkers) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Second").append("\t");
+        for (ElveWorker elveWorker : elveWorkers) {
+            sb.append(elveWorker.getId()).append("\t");
+        }
+        sb.append("Done");
+        return sb;
+    }
+
+
+    private List<ElveWorker> createElveWorkers(int aviableWorkers) {
+        return IntStream.rangeClosed(1, aviableWorkers).mapToObj(ElveWorker::new).collect(Collectors.toList());
+    }
+
+    private void assignElveWorkerWithTasks(List<ElveWorker> elveWorkers, Set<TaskStep> taskStepCompletionOrder) {
+        currentlyCompletedStepTask(elveWorkers).forEachOrdered(taskStepCompletionOrder::add);
+
+        List<ElveWorker> aviableElveWorkers = aviableElveWorkers(elveWorkers);
+        List<TaskStep> aviableNextTaskSteps = aviableNextSteps(elveWorkers, taskStepCompletionOrder);
+        int newAssignedTaskCount = Math.min(aviableElveWorkers.size(), aviableNextTaskSteps.size());
+        for (int i = 0; i < newAssignedTaskCount; i++) {
+            ElveWorker nextElveWorker = aviableElveWorkers.get(i);
+            TaskStep nextTaskStep = aviableNextTaskSteps.get(i);
+            nextElveWorker.assignTask(nextTaskStep);
+        }
+    }
+
+    private Stream<TaskStep> currentlyCompletedStepTask(List<ElveWorker> elveWorkers) {
+        return elveWorkers.stream()
+                .filter(ElveWorker::canWorkOnNextStep)
+                .map(ElveWorker::getAssignedTaskStep)
+                .filter(Objects::nonNull);
+    }
+
+    private List<ElveWorker> aviableElveWorkers(List<ElveWorker> elveWorkers) {
+        return elveWorkers.stream().filter(ElveWorker::canWorkOnNextStep).collect(Collectors.toList());
+    }
+
+    private List<TaskStep> aviableNextSteps(List<ElveWorker> elveWorkers, Set<TaskStep> taskStepCompletionOrder) {
         return letterToStep.values()
                 .stream()
-                .filter(step -> !stepCompletionOrder.contains(step))
-                .filter(step -> step.canBeFinished(stepCompletionOrder))
-                .findFirst();
+                .filter(taskStep -> !taskStepCompletionOrder.contains(taskStep))
+                .filter(taskStep -> taskStep.canBeFinished(taskStepCompletionOrder))
+                .filter(taskStep -> elveWorkers.stream()
+                        .map(ElveWorker::getAssignedTaskStep)
+                        .noneMatch(taskStep::equals))
+                .collect(Collectors.toList());
     }
 
 
@@ -73,13 +157,30 @@ public class TheSumOfOtsPart {
             return;
         }
         Path puzzleInputFile = Paths.get(args[0]);
-        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(puzzleInputFile);
-        String stepCompletionOrder = toStepLetterString(theSumOfOtsPart.getStepCompletionOrder());
-        System.out.println("stepCompletionOrder: " + stepCompletionOrder);
+        Duration taskDuration = Duration.ofSeconds(60);
+
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(puzzleInputFile, taskDuration);
+        StepComplitionResult stepComplitionResult1 = theSumOfOtsPart.calculateStepCompletionOrder();
+        System.out.println(stepComplitionResult1);
+
+        int aviableWorkers = 5;
+        StepComplitionResult stepComplitionResult2 = theSumOfOtsPart.calculateStepCompletionOrder(aviableWorkers);
+        System.out.println(stepComplitionResult2);
     }
 
-    public static String toStepLetterString(Stream<Step> stepComplitionOrder) {
-        return stepComplitionOrder.map(Step::getLetter).map(String::valueOf).collect(Collectors.joining(""));
+
+    @Value
+    public static class StepComplitionResult {
+
+        private final int workerCound;
+        private final Duration neededDuration;
+        private final List<TaskStep> completionOrder;
+
+
+        public String completionOrderString() {
+            return completionOrder.stream().map(TaskStep::toString).collect(Collectors.joining(""));
+        }
+
     }
 
 }
--- 2018/src/test/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPartTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPartTest.java
@@ -1,16 +1,14 @@
 package de.adventofcode.chrisgw.day07;
 
 import de.adventofcode.chrisgw.TestUtils;
+import de.adventofcode.chrisgw.day07.TheSumOfOtsPart.StepComplitionResult;
 import org.junit.Test;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static de.adventofcode.chrisgw.day07.TheSumOfOtsPart.toStepLetterString;
 import static org.junit.Assert.*;
 
 
@@ -26,26 +24,68 @@ public class TheSumOfOtsPartTest {
             "Step F must be finished before step E can begin.");
 
     @Test
-    public void getStepCompletionOrder_example_01() {
+    public void calculateStepCompletionOrder_example_01() {
         String expectedStepComplitionOrder = "CABDFE";
+        Duration taskDuration = Duration.ZERO;
 
-        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructions_example01.stream());
-        String stepComplitionOrder = toStepLetterString(theSumOfOtsPart.getStepCompletionOrder());
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructions_example01.stream(),
+                taskDuration);
+        StepComplitionResult stepComplitionResult = theSumOfOtsPart.calculateStepCompletionOrder();
+        String stepComplitionOrder = stepComplitionResult.completionOrderString();
+
         assertEquals("stepComplitionOrder", expectedStepComplitionOrder, stepComplitionOrder);
     }
 
 
     @Test
-    public void getStepCompletionOrder_myPuzzleInput() throws Exception{
+    public void calculateStepCompletionOrder_myPuzzleInput() throws Exception {
         String expectedStepComplitionOrder = "OCPUEFIXHRGWDZABTQJYMNKVSL";
         Path stepInstructionFile = TestUtils.getResourcePath("/day07/myPuzzleInput.txt");
+        Duration taskDuration = Duration.ofSeconds(60);
 
-        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructionFile);
-        String stepComplitionOrder = toStepLetterString(theSumOfOtsPart.getStepCompletionOrder());
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructionFile, taskDuration);
+        StepComplitionResult stepComplitionResult = theSumOfOtsPart.calculateStepCompletionOrder();
+        String stepComplitionOrder = stepComplitionResult.completionOrderString();
+
         assertEquals("stepComplitionOrder", expectedStepComplitionOrder, stepComplitionOrder);
     }
 
 
+    // part 02
 
+    @Test
+    public void calculateStepCompletionOrder_withWorkers02_example_01() {
+        String expectedStepComplitionOrder = "CABFDE";
+        int workerCount = 2;
+        Duration taskDuration = Duration.ZERO;
+        Duration expectedNeededDuration = Duration.ofSeconds(15);
+
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructions_example01.stream(),
+                taskDuration);
+        StepComplitionResult stepComplitionResult = theSumOfOtsPart.calculateStepCompletionOrder(workerCount);
+        String stepCompletionOrder = stepComplitionResult.completionOrderString();
+        Duration neededDuration = stepComplitionResult.getNeededDuration();
+
+        assertEquals("stepComplitionOrder", expectedStepComplitionOrder, stepCompletionOrder);
+        assertEquals("neededDuration", expectedNeededDuration, neededDuration);
+    }
+
+
+    @Test
+    public void calculateStepCompletionOrder__withWorkers02_myPuzzleInput() throws Exception {
+        String expectedStepComplitionOrder = "OPCUXEHFIRWZGDABTQYJMNKVSL";
+        Path stepInstructionFile = TestUtils.getResourcePath("/day07/myPuzzleInput.txt");
+        Duration taskDuration = Duration.ofSeconds(60);
+        int workerCount = 15;
+        Duration expectedNeededDuration = Duration.ofSeconds(991);
+
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructionFile, taskDuration);
+        StepComplitionResult stepComplitionResult = theSumOfOtsPart.calculateStepCompletionOrder(workerCount);
+        String stepCompletionOrder = stepComplitionResult.completionOrderString();
+        Duration neededDuration = stepComplitionResult.getNeededDuration();
+
+        assertEquals("stepComplitionOrder", expectedStepComplitionOrder, stepCompletionOrder);
+        assertEquals("neededDuration", expectedNeededDuration, neededDuration);
+    }
 
 }
