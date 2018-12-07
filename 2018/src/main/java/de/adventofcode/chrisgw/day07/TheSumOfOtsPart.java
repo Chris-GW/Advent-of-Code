a611daeacfb9173be5820c0866c0a03b5cf17c1a
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-07 09:27:05 +0100

    2018 Day 7: The Sum of Its Parts - part 01 finished

--- 2018/src/main/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPart.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPart.java
@@ -1,6 +1,70 @@
 package de.adventofcode.chrisgw.day07;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 public class TheSumOfOtsPart {
+
+    private final Map<Character, Step> letterToStep;
+    private final Step firstStep;
+
+    private TheSumOfOtsPart(Map<Character, Step> letterToStep) {
+        this.letterToStep = new HashMap<>(letterToStep);
+        this.firstStep = letterToStep.values().stream().filter(Step::isFirstStep).findAny().orElse(null);
+    }
+
+    public static TheSumOfOtsPart fromStepInstructions(Path stepInstructionFile) throws IOException {
+        return fromStepInstructions(Files.lines(stepInstructionFile));
+    }
+
+    public static TheSumOfOtsPart fromStepInstructions(Stream<String> stepInstructions) {
+        Map<Character, Step> letterToStep = new HashMap<>('Z' - 'A');
+
+        Iterator<String> stepInstructionIterator = stepInstructions.iterator();
+        while (stepInstructionIterator.hasNext()) {
+            String stepInstruction = stepInstructionIterator.next();
+            Step step = parseStep(letterToStep, stepInstruction);
+            Step requierdStep = parseRequierdStep(letterToStep, stepInstruction);
+            step.addRequierdStep(requierdStep);
+        }
+
+        return new TheSumOfOtsPart(letterToStep);
+    }
+
+    private static Step parseStep(Map<Character, Step> letterToStep, String stepInstruction) {
+        char stepLetter = stepInstruction.charAt("Step P must be finished before step ".length());
+        return letterToStep.computeIfAbsent(stepLetter, Step::new);
+    }
+
+    private static Step parseRequierdStep(Map<Character, Step> letterToStep, String stepInstruction) {
+        char stepLetter = stepInstruction.charAt("Step ".length());
+        return letterToStep.computeIfAbsent(stepLetter, Step::new);
+    }
+
+
+    public Stream<Step> getStepCompletionOrder() {
+        Set<Step> stepCompletionOrder = new LinkedHashSet<>(letterToStep.size());
+        stepCompletionOrder.add(firstStep);
+        while (stepCompletionOrder.size() < letterToStep.size()) {
+            Optional<Step> nextStep = findNextStep(stepCompletionOrder);
+            nextStep.ifPresent(stepCompletionOrder::add);
+        }
+        return stepCompletionOrder.stream();
+    }
+
+    private Optional<Step> findNextStep(Set<Step> stepCompletionOrder) {
+        return letterToStep.values()
+                .stream()
+                .filter(step -> !stepCompletionOrder.contains(step))
+                .filter(step -> step.canBeFinished(stepCompletionOrder))
+                .findFirst();
+    }
 
 
     public static void main(String[] args) throws Exception {
@@ -8,8 +72,14 @@ public class TheSumOfOtsPart {
             System.out.println("No path in args[]");
             return;
         }
+        Path puzzleInputFile = Paths.get(args[0]);
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(puzzleInputFile);
+        String stepCompletionOrder = toStepLetterString(theSumOfOtsPart.getStepCompletionOrder());
+        System.out.println("stepCompletionOrder: " + stepCompletionOrder);
+    }
 
-
+    public static String toStepLetterString(Stream<Step> stepComplitionOrder) {
+        return stepComplitionOrder.map(Step::getLetter).map(String::valueOf).collect(Collectors.joining(""));
     }
 
 }
--- 2018/src/test/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPartTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day07/TheSumOfOtsPartTest.java
@@ -1,8 +1,51 @@
 package de.adventofcode.chrisgw.day07;
 
+import de.adventofcode.chrisgw.TestUtils;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static de.adventofcode.chrisgw.day07.TheSumOfOtsPart.toStepLetterString;
 import static org.junit.Assert.*;
 
 
 public class TheSumOfOtsPartTest {
+
+    List<String> stepInstructions_example01 = Arrays.asList( //
+            "Step C must be finished before step A can begin.", //
+            "Step C must be finished before step F can begin.", //
+            "Step A must be finished before step B can begin.", //
+            "Step A must be finished before step D can begin.", //
+            "Step B must be finished before step E can begin.", //
+            "Step D must be finished before step E can begin.", //
+            "Step F must be finished before step E can begin.");
+
+    @Test
+    public void getStepCompletionOrder_example_01() {
+        String expectedStepComplitionOrder = "CABDFE";
+
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructions_example01.stream());
+        String stepComplitionOrder = toStepLetterString(theSumOfOtsPart.getStepCompletionOrder());
+        assertEquals("stepComplitionOrder", expectedStepComplitionOrder, stepComplitionOrder);
+    }
+
+
+    @Test
+    public void getStepCompletionOrder_myPuzzleInput() throws Exception{
+        String expectedStepComplitionOrder = "OCPUEFIXHRGWDZABTQJYMNKVSL";
+        Path stepInstructionFile = TestUtils.getResourcePath("/day07/myPuzzleInput.txt");
+
+        TheSumOfOtsPart theSumOfOtsPart = TheSumOfOtsPart.fromStepInstructions(stepInstructionFile);
+        String stepComplitionOrder = toStepLetterString(theSumOfOtsPart.getStepCompletionOrder());
+        assertEquals("stepComplitionOrder", expectedStepComplitionOrder, stepComplitionOrder);
+    }
+
+
+
 
 }
