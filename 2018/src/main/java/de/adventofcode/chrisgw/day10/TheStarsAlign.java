43c12b976f768f55ebefed99eb037b63968e309c
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-10 11:07:08 +0100

    2018 Day 10: The Stars Align - start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day10/TheStarsAlign.java
@@ -0,0 +1,19 @@
+package de.adventofcode.chrisgw.day10;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class TheStarsAlign {
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("No path in args[]");
+            return;
+        }
+        Path puzzleInputFile = Paths.get(args[0]);
+
+    }
+
+}
