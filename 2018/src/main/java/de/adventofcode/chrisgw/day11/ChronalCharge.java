764bf1fd9808987605e4b10126acd5818b882aff
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-11 08:59:00 +0100

    2018 Day 11: Chronal Charge - start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day11/ChronalCharge.java
@@ -0,0 +1,20 @@
+package de.adventofcode.chrisgw.day11;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class ChronalCharge {
+
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("No path in args[]");
+            return;
+        }
+        Path puzzleInputFile = Paths.get(args[0]);
+
+    }
+
+}
