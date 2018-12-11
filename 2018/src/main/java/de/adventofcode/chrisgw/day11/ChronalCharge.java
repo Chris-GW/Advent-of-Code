775eb52992d5a98a9365c7fc7c640704dd41c125
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-11 10:42:56 +0100

    2018 Day 11: Chronal Charge - part02

--- 2018/src/main/java/de/adventofcode/chrisgw/day11/ChronalCharge.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day11/ChronalCharge.java
@@ -2,7 +2,10 @@ package de.adventofcode.chrisgw.day11;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 
@@ -22,9 +25,33 @@ public class ChronalCharge {
     }
 
 
+    public FuelCellSquare findHighestPowerLevelFuelCellSquare() {
+        AtomicInteger completedSquareSizes = new AtomicInteger();
+        return IntStream.range(1, fuelCellGridSize())
+                .parallel()
+                .mapToObj(this::findHighestPowerLevelFuelCellSquare)
+                .peek(logFindHeighestPowerLevelFuelCellSquareProgression(completedSquareSizes))
+                .max(Comparator.comparingInt(FuelCellSquare::getTotalPowerLevel))
+                .orElse(null);
+    }
+
+    private Consumer<FuelCellSquare> logFindHeighestPowerLevelFuelCellSquareProgression(
+            AtomicInteger completedSquareSizes) {
+        return fuelCellSquare -> {
+            double fuelCellGridSize = fuelCellGridSize();
+            int percent = (int) (completedSquareSizes.incrementAndGet() / fuelCellGridSize * 100.0);
+            int size = fuelCellSquare.size();
+            int x = fuelCellSquare.getTopLeftFuelCell().getX();
+            int y = fuelCellSquare.getTopLeftFuelCell().getY();
+            int totalPowerLevel = fuelCellSquare.getTotalPowerLevel();
+            System.out.printf("%3d%%: Square with size = %3d\tat <%3d;%3d> with totalPowerLevel = %9d%n", //
+                    percent, size, x, y, totalPowerLevel);
+        };
+    }
+
     public FuelCellSquare findHighestPowerLevelFuelCellSquare(int fuelCellSquareSize) {
         return fuelCellSquares(fuelCellSquareSize) //
-                .max(Comparator.comparingInt(FuelCellSquare::totalPowerLevel)) //
+                .max(Comparator.comparingInt(FuelCellSquare::getTotalPowerLevel)) //
                 .orElse(null);
     }
 
@@ -32,15 +59,21 @@ public class ChronalCharge {
         Builder<FuelCellSquare> fuelCellSquares = Stream.builder();
         for (int y = 0; y < fuelCellGridSize() - fuelCellSquareSize; y++) {
             for (int x = 0; x < fuelCellGridSize() - fuelCellSquareSize; x++) {
-                FuelCellSquare fuelCellSquare = createFuelCellSquare(fuelCellSquareSize, y, x);
+                FuelCellSquare fuelCellSquare = createFuelCellSquare(x, y, fuelCellSquareSize);
                 fuelCellSquares.add(fuelCellSquare);
             }
         }
         return fuelCellSquares.build();
     }
 
-    private FuelCellSquare createFuelCellSquare(int fuelCellSquareSize, int y, int x) {
-        return FuelCellSquare.createFuelCellSquare(fuelCellGrid, fuelCellSquareSize, x, y);
+    private FuelCellSquare createFuelCellSquare(int x, int y, int fuelCellSquareSize) {
+        FuelCell topLeftFuelCell = fuelCellAt(x, y);
+        return new FuelCellSquare(fuelCellGrid, topLeftFuelCell, fuelCellSquareSize);
+    }
+
+
+    public FuelCell fuelCellAt(int x, int y) {
+        return fuelCellGrid[y][x];
     }
 
 
