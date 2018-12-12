02f107ed2ac134b68914b321226396c0efa89f36
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-12 13:42:21 +0100

    2018 Day 12: Subterranean Sustainability - why does that not work :(

--- 2018/src/main/java/de/adventofcode/chrisgw/day12/SubterraneanSustainability.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day12/SubterraneanSustainability.java
@@ -1,7 +1,180 @@
 package de.adventofcode.chrisgw.day12;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static de.adventofcode.chrisgw.day12.PlantGrowNote.parsePlantGrowNote;
+
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SubterraneanSustainability {
 
+    private int generation = 0;
+
+    private List<PlantPot> plantPots = new ArrayList<>();
+    private List<PlantPot> negativePlantPots = new ArrayList<>();
+
+    private List<PlantGrowNote> plantGrowNotes = new ArrayList<>();
+
+
+    public static SubterraneanSustainability parseSubterraneanSustainability(List<String> descriptionLines) {
+        SubterraneanSustainability plantPotField = new SubterraneanSustainability();
+        String initalPlantPotLine = descriptionLines.get(0);
+        initalPlantPotLine = initalPlantPotLine.substring("initial state: ".length());
+        plantPotField.plantPots = parseInitialPlantPots(plantPotField, initalPlantPotLine);
+
+        List<String> plantGrowNotes = descriptionLines.subList(2, descriptionLines.size());
+        plantPotField.plantGrowNotes = parsePlantGrowNotes(plantGrowNotes);
+        return plantPotField;
+    }
+
+    private static List<PlantPot> parseInitialPlantPots(SubterraneanSustainability plantPotField,
+            String initalPlantPotLine) {
+        List<PlantPot> initalPlantPots = new ArrayList<>(initalPlantPotLine.length());
+        for (int i = 0; i < initalPlantPotLine.length(); i++) {
+            boolean hasPlant = initalPlantPotLine.charAt(i) == '#';
+            PlantPot plantPot = plantPotField.new PlantPot(i, hasPlant);
+            initalPlantPots.add(plantPot);
+        }
+        return initalPlantPots;
+    }
+
+    private static List<PlantGrowNote> parsePlantGrowNotes(List<String> plantGrowNotesStr) {
+        List<PlantGrowNote> plantGrowNotes = new ArrayList<>(plantGrowNotesStr.size());
+        for (String plantGrowTransitionNoteStr : plantGrowNotesStr) {
+            PlantGrowNote plantGrowNote = parsePlantGrowNote(plantGrowTransitionNoteStr);
+            plantGrowNotes.add(plantGrowNote);
+        }
+        return plantGrowNotes;
+    }
+
+
+    public SubterraneanSustainability nextGeneration() {
+        SubterraneanSustainability newPlantPotField = new SubterraneanSustainability();
+        newPlantPotField.generation = this.generation + 1;
+        newPlantPotField.plantGrowNotes = this.plantGrowNotes;
+        newPlantPotField.plantPots = collectNewGenerationPlantPots(newPlantPotField, this.plantPots, false);
+        newPlantPotField.negativePlantPots = collectNewGenerationPlantPots(newPlantPotField, this.negativePlantPots,
+                true);
+        return newPlantPotField;
+    }
+
+    private List<PlantPot> collectNewGenerationPlantPots(SubterraneanSustainability plantPotField,
+            List<PlantPot> plantPots, boolean negative) {
+        int currentPlantPotsSize = plantPots.size();
+        List<PlantPot> newGenerationPlantPots = new ArrayList<>(currentPlantPotsSize + 5);
+
+        plantPots.stream().parallel().map(plantPot -> {
+            int number = plantPot.number;
+            boolean hasPlant = plantPot.willHavePlantInNextGeneration();
+            return plantPotField.new PlantPot(number, hasPlant);
+        }).forEachOrdered(newGenerationPlantPots::add);
+
+        IntStream.rangeClosed(currentPlantPotsSize, currentPlantPotsSize + 5)
+                .mapToObj(number -> plantPotField.new PlantPot(negative ? -number : number))
+                .forEachOrdered(newGenerationPlantPots::add);
+        return newGenerationPlantPots;
+    }
+
+
+    public long getTotalPotWithPlantesSum() {
+        return allPlantPots().filter(PlantPot::hasPlant).mapToInt(PlantPot::getNumber).sum();
+    }
+
+
+    private PlantPot plantPotAt(int potNumber) {
+        List<PlantPot> plantPots = this.plantPots;
+        if (potNumber < 0) {
+            potNumber = Math.abs(potNumber);
+            plantPots = negativePlantPots;
+        }
+        if (potNumber < plantPots.size()) {
+            return plantPots.get(potNumber);
+        } else {
+            return new PlantPot(potNumber);
+        }
+    }
+
+
+    private Stream<PlantPot> allPlantPots() {
+        return Stream.concat(negativePlantPots.stream(), plantPots.stream());
+    }
+
+    public int plantPotFieldSize() {
+        return negativePlantPots.size() + plantPots.size();
+    }
+
+
+    public int getGeneration() {
+        return generation;
+    }
+
+
+    public class PlantPot {
+
+        private int number;
+        private boolean hasPlant;
+
+
+        public PlantPot(int number) {
+            this(number, false);
+        }
+
+        public PlantPot(int number, boolean hasPlant) {
+            this.number = number;
+            this.hasPlant = hasPlant;
+        }
+
+
+        public int getNumber() {
+            return number;
+        }
+
+        public boolean hasPlant() {
+            return hasPlant;
+        }
+
+
+        public boolean willHavePlantInNextGeneration() {
+            Optional<PlantGrowNote> matchingPlantGrowNote = plantGrowNotes.stream()
+                    .filter(plantGrowNote -> plantGrowNote.matchesPlantPot(this))
+                    .findFirst();
+            return matchingPlantGrowNote.map(PlantGrowNote::isResultIntoPotWithPlant).orElse(false);
+        }
+
+
+        PlantPot getPlantPotTo(int relativePlantPotIndex) {
+            return plantPotAt(number + relativePlantPotIndex);
+        }
+
+        @Override
+        public String toString() {
+            String plantStr = hasPlant ? "#" : ".";
+            return String.format("(%3d) %s", number, plantStr);
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(6 + plantPotFieldSize());
+        sb.append(String.format("%3d: ", generation));
+        for (int number = -3; number <= 35; number++) {
+            PlantPot plantPot = plantPotAt(number);
+            sb.append(plantPot.hasPlant ? "#" : ".");
+        }
+        return sb.toString();
+    }
 
 
     public static void main(String[] args) throws Exception {
@@ -9,7 +182,17 @@ public class SubterraneanSustainability {
             System.out.println("Usage <gridSerialNumber> <fuelCellGridSize>");
             return;
         }
+        Path inputFile = Paths.get(args[0]);
+        SubterraneanSustainability plantPotField = SubterraneanSustainability.parseSubterraneanSustainability(
+                Files.readAllLines(inputFile));
 
+        for (int gen = 0; gen < 20; gen++) {
+            System.out.println(plantPotField);
+            plantPotField = plantPotField.nextGeneration();
+        }
+
+        long totalPotWithPlantesSum = plantPotField.getTotalPotWithPlantesSum();
+        System.out.println("totalPotWithPlantesSum: " + totalPotWithPlantesSum);
     }
 
 }
--- 2018/src/test/java/de/adventofcode/chrisgw/day12/SubterraneanSustainabilityTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day12/SubterraneanSustainabilityTest.java
@@ -1,8 +1,95 @@
 package de.adventofcode.chrisgw.day12;
 
+import de.adventofcode.chrisgw.TestUtils;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 
 public class SubterraneanSustainabilityTest {
+
+
+    @Test
+    public void countPlantPotWithPlants_example01() {
+        List<String> descriptionLines = Arrays.asList( //
+                "initial state: #..#.#..##......###...###", //
+                "", //
+                "...## => #", //
+                "..#.. => #", //
+                ".#... => #", //
+                ".#.#. => #", //
+                ".#.## => #", //
+                ".##.. => #", //
+                ".#### => #", //
+                "#.#.# => #", //
+                "#.### => #", //
+                "##.#. => #", //
+                "##.## => #", //
+                "###.. => #", //
+                "###.# => #", //
+                "####. => #");
+
+        List<String> expectedPlantPotField = Arrays.asList( //
+                "  0: ...#..#.#..##......###...###...........", //
+                "  1: ...#...#....#.....#..#..#..#...........", //
+                "  2: ...##..##...##....#..#..#..##..........", //
+                "  3: ..#.#...#..#.#....#..#..#...#..........", //
+                "  4: ...#.#..#...#.#...#..#..##..##.........", //
+                "  5: ....#...##...#.#..#..#...#...#.........", //
+                "  6: ....##.#.#....#...#..##..##..##........", //
+                "  7: ...#..###.#...##..#...#...#...#........", //
+                "  8: ...#....##.#.#.#..##..##..##..##.......", //
+                "  9: ...##..#..#####....#...#...#...#.......", //
+                " 10: ..#.#..#...#.##....##..##..##..##......", //
+                " 11: ...#...##...#.#...#.#...#...#...#......", //
+                " 12: ...##.#.#....#.#...#.#..##..##..##.....", //
+                " 13: ..#..###.#....#.#...#....#...#...#.....", //
+                " 14: ..#....##.#....#.#..##...##..##..##....", //
+                " 15: ..##..#..#.#....#....#..#.#...#...#....", //
+                " 16: .#.#..#...#.#...##...#...#.#..##..##...", //
+                " 17: ..#...##...#.#.#.#...##...#....#...#...", //
+                " 18: ..##.#.#....#####.#.#.#...##...##..##..", //
+                " 19: .#..###.#..#.#.#######.#.#.#..#.#...#..", //
+                " 20: .#....##....#####...#######....#.#..##.");
+
+        int generation = 20;
+        long expectedTotalPotWithPlantes = 325;
+
+        SubterraneanSustainability plantToField = SubterraneanSustainability.parseSubterraneanSustainability(
+                descriptionLines);
+        for (int gen = 0; gen < generation; gen++) {
+            System.out.println(plantToField);
+            assertEquals("plantPotField " + gen, expectedPlantPotField.get(gen), plantToField.toString());
+            plantToField = plantToField.nextGeneration();
+        }
+        System.out.println(plantToField);
+        long totalPotWithPlantes = plantToField.getTotalPotWithPlantesSum();
+        assertEquals("totalPotWithPlantes", expectedTotalPotWithPlantes, totalPotWithPlantes);
+    }
+
+
+    @Test
+    public void countPlantPotWithPlants_myPuzzleInput() throws Exception {
+        Path myPuzzleInputFile = TestUtils.getResourcePath("/day12/myPuzzleInput.txt");
+        List<String> descriptionLines = Files.readAllLines(myPuzzleInputFile);
+
+        int generation = 20;
+        long expectedTotalPotWithPlantes = 1511;
+
+        SubterraneanSustainability plantToField = SubterraneanSustainability.parseSubterraneanSustainability(
+                descriptionLines);
+        for (int gen = 0; gen < generation; gen++) {
+            System.out.println(plantToField);
+            plantToField = plantToField.nextGeneration();
+        }
+        System.out.println(plantToField);
+        long totalPotWithPlantes = plantToField.getTotalPotWithPlantesSum();
+        assertEquals("totalPotWithPlantes", expectedTotalPotWithPlantes, totalPotWithPlantes);
+    }
 
 }
