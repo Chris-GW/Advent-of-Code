5705d11fdcbf57f7ecec602393b8e6657e761d9b
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-12 08:36:41 +0100

    2018 Day 12: Subterranean Sustainability - start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day12/SubterraneanSustainability.java
@@ -0,0 +1,15 @@
+package de.adventofcode.chrisgw.day12;
+
+public class SubterraneanSustainability {
+
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Usage <gridSerialNumber> <fuelCellGridSize>");
+            return;
+        }
+
+    }
+
+}
---
+++ 2018/src/test/java/de/adventofcode/chrisgw/day12/SubterraneanSustainabilityTest.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day12;
+
+import static org.junit.Assert.*;
+
+
+public class SubterraneanSustainabilityTest {
+
+}
