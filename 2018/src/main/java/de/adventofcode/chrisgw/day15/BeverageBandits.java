37fbdfc1de2b7d2193b9aa639c28b1548eac9f3e
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-15 17:31:51 +0100

    2018 Day 15: Beverage Bandits - start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day15/BeverageBandits.java
@@ -0,0 +1,14 @@
+package de.adventofcode.chrisgw.day15;
+
+public class BeverageBandits {
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Usage <neededRecipes>");
+            return;
+        }
+
+    }
+
+}
