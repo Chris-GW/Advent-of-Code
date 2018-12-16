64245271b6bba7c9def81a0f05378314ead0a031
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-16 02:19:23 +0100

    2018 Day 15: Beverage Bandits - some progression

--- 2018/src/main/java/de/adventofcode/chrisgw/day15/BeverageBandits.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day15/BeverageBandits.java
@@ -1,6 +1,338 @@
 package de.adventofcode.chrisgw.day15;
 
+import lombok.*;
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
+
+
 public class BeverageBandits {
+
+    @Getter
+    private int battleRounds = 0;
+
+    private GameField[][] gameArea;
+
+    private List<GameUnit> gameUnits = new ArrayList<>();
+
+
+    public BeverageBandits(List<String> gameAreaLines) {
+        gameArea = new GameField[gameAreaLines.size()][];
+        for (int y = 0; y < gameArea.length; y++) {
+            String gameAreaRowStr = gameAreaLines.get(y);
+            gameArea[y] = new GameField[gameAreaRowStr.length()];
+            for (int x = 0; x < gameArea[y].length; x++) {
+                char gameFieldChar = gameAreaRowStr.charAt(x);
+                boolean isCavern = gameFieldChar != '#';
+                GameField gameField = new GameField(x, y, isCavern);
+                gameArea[y][x] = gameField;
+                parseGameUnit(gameFieldChar, gameField).ifPresent(gameUnits::add);
+            }
+        }
+    }
+
+    private Optional<GameUnit> parseGameUnit(char gameFieldChar, GameField gameField) {
+        for (GameFaction gameFaction : GameFaction.values()) {
+            if (gameFieldChar == gameFaction.getUnitFactionLetter()) {
+                return Optional.of(new GameUnit(gameFaction, gameField));
+            }
+        }
+        return Optional.empty();
+    }
+
+
+    public void nextBattleRound() {
+        aliveGameUnits().forEachOrdered(GameUnit::takeNextTurn);
+        battleRounds++;
+    }
+
+    void nextTurn() {
+        aliveGameUnits().findFirst().ifPresent(GameUnit::takeNextTurn);
+    }
+
+
+    public boolean isOnlyOneFactionAlive() {
+        return Arrays.stream(GameFaction.values())
+                .map(this::isAnyUnitAliveOfFaction)
+                .reduce(false, (factionIsAlive, otherFactionIsAlive) -> factionIsAlive ^ otherFactionIsAlive);
+    }
+
+    private boolean isAnyUnitAliveOfFaction(GameFaction faction) {
+        return aliveGameUnits().map(GameUnit::getFaction).anyMatch(faction::equals);
+    }
+
+
+    public int calculateFinishingGameScore() {
+        int remainingUnitHitPointSum = aliveGameUnits().mapToInt(GameUnit::getHitPoints).sum();
+        return battleRounds * remainingUnitHitPointSum;
+    }
+
+
+    private Stream<GameUnit> aliveGameUnits() {
+        return gameUnits.stream().sorted(Comparator.comparing(GameUnit::getCurrentField)).filter(GameUnit::isAlive);
+    }
+
+
+    public GameField fieldAt(int x, int y) {
+        if (0 <= y && y < gameArea.length && 0 <= x && x < gameArea[y].length) {
+            return gameArea[y][x];
+        } else {
+            boolean isCavern = false;
+            return new GameField(x, y, isCavern);
+        }
+    }
+
+
+    public Stream<GameField> cavernFields() {
+        return Arrays.stream(gameArea).flatMap(Arrays::stream).filter(GameField::isCavern);
+    }
+
+
+    public int size() {
+        return gameArea.length * gameArea[0].length;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < gameArea.length; y++) {
+            for (int x = 0; x < gameArea[y].length; x++) {
+                GameField field = gameArea[y][x];
+                if (field.isHoldingUnit()) {
+                    GameUnit unit = field.getUnit();
+                    sb.append(unit.getFaction().getUnitFactionLetter());
+                } else if (field.isCavern()) {
+                    sb.append('.');
+                } else {
+                    sb.append('#');
+                }
+            }
+            sb.append("\n");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+
+    @Getter
+    @ToString
+    @EqualsAndHashCode
+    public class GameUnit implements Comparable<GameUnit> {
+
+        private GameFaction faction;
+        private GameField currentField;
+        private int hitPoints = 200;
+        private int attackValue = 3;
+
+
+        public GameUnit(GameFaction faction, GameField currentField) {
+            this.faction = faction;
+            moveTo(currentField);
+        }
+
+
+        public void takeNextTurn() {
+            if (!isInRangeForAttackingEnemyUnit()) {
+                moveIntoRangeOfNearestEnemyUnit();
+            }
+            if (isInRangeForAttackingEnemyUnit()) {
+                attackNearestEnemyUnit();
+            }
+            System.out.println(BeverageBandits.this);
+        }
+
+        private boolean isInRangeForAttackingEnemyUnit() {
+            return adjacentCavernFields().filter(GameField::isHoldingUnit)
+                    .map(GameField::getUnit)
+                    .anyMatch(this::isEnemyTo);
+        }
+
+
+        private void moveIntoRangeOfNearestEnemyUnit() {
+            List<GameUnit> enemyUnits = aliveGameUnits().filter(this::isEnemyTo).sorted().collect(Collectors.toList());
+
+            List<GameField> freeAdjacentFields = enemyUnits.stream()
+                    .flatMap(GameUnit::adjacentCavernFields)
+                    .filter(GameField::isFreeCavern)
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            Optional<GameField> chosenField = freeAdjacentFields.stream()
+                    .min(Comparator.comparingInt(currentField::distanceTo).thenComparing(Comparator.naturalOrder()));
+
+            chosenField.map(currentField::shortestPathTo).map(gameFields -> gameFields.get(1)).ifPresent(this::moveTo);
+        }
+
+
+        private void attackNearestEnemyUnit() {
+            currentField.adjacentFields()
+                    .filter(GameField::isHoldingUnit)
+                    .map(GameField::getUnit)
+                    .filter(this::isEnemyTo)
+                    .findFirst()
+                    .ifPresent(this::attackEnemyUnit);
+        }
+
+        private void attackEnemyUnit(GameUnit enemyUnit) {
+            enemyUnit.hitPoints -= this.attackValue;
+        }
+
+
+        private void moveTo(GameField field) {
+            if (this.currentField != null) {
+                this.currentField.unit = null;
+            }
+            field.unit = this;
+            this.currentField = field;
+        }
+
+
+        public Stream<GameField> adjacentCavernFields() {
+            return currentField.adjacentFields().filter(GameField::isCavern);
+        }
+
+
+        public boolean isAlive() {
+            return hitPoints > 0;
+        }
+
+        public boolean isEnemyTo(GameUnit otherUnit) {
+            return !this.faction.equals(otherUnit.faction);
+        }
+
+
+        @Override
+        public int compareTo(GameUnit otherUnit) {
+            return this.currentField.compareTo(otherUnit.currentField);
+        }
+
+    }
+
+
+    @Data
+    @Setter(AccessLevel.PRIVATE)
+    public class GameField implements Comparable<GameField> {
+
+        private final int x;
+        private final int y;
+        private final boolean isCavern;
+
+        @EqualsAndHashCode.Exclude
+        private GameUnit unit;
+
+
+        public Stream<GameField> adjacentFields() {
+            Builder<GameField> adjacentFields = Stream.builder();
+            adjacentFields.add(fieldAt(x + 1, y + 0));
+            adjacentFields.add(fieldAt(x - 1, y + 0));
+            adjacentFields.add(fieldAt(x + 0, y + 1));
+            adjacentFields.add(fieldAt(x + 0, y - 1));
+            return adjacentFields.build();
+        }
+
+
+        public List<GameField> shortestPathTo(GameField otherField) {
+            Map<GameField, Integer> distance = new HashMap<>(size());
+            Map<GameField, GameField> prev = new HashMap<>(size());
+            Set<GameField> q = cavernFields().collect(Collectors.toSet());
+
+            distance.put(this, 0);
+
+            while (q.size() > 0) {
+                GameField u = q.stream()
+                        .min(Comparator.comparingInt(
+                                (GameField field) -> distance.getOrDefault(field, Integer.MAX_VALUE))
+                                .thenComparing(Comparator.naturalOrder()))
+                        .orElseThrow(IllegalStateException::new);
+                q.remove(u);
+
+                u.adjacentFields().sorted().forEachOrdered(v -> {
+                    int length = u.distanceTo(v);
+                    int distanceU = distance.getOrDefault(u, Integer.MAX_VALUE);
+                    int distanceV = distance.getOrDefault(v, Integer.MAX_VALUE);
+                    int altDistance = distanceU + length;
+                    if (altDistance < distanceV) {
+                        distance.put(v, altDistance);
+                        prev.put(v, u);
+                    } else if (altDistance == distanceV && v.compareTo(prev.get(v)) < 0) {
+                        prev.put(v, u);
+                    }
+                });
+            }
+
+            return readShortestPath(prev, otherField);
+        }
+
+        private List<GameField> readShortestPath(Map<GameField, GameField> prev, GameField target) {
+            List<GameField> s = new ArrayList<>();
+            GameField u = target;
+            if (prev.containsKey(u)) {
+                while (u != null) {
+                    s.add(u);
+                    u = prev.get(u);
+                }
+            }
+            Collections.reverse(s);
+            return s;
+        }
+
+
+        public int distanceTo(GameField otherField) {
+            int dx = Math.abs(this.x - otherField.x);
+            int dy = Math.abs(this.y - otherField.y);
+            return dx + dy;
+        }
+
+
+        public boolean isHoldingUnit() {
+            return unit != null;
+        }
+
+
+        public boolean isFreeCavern() {
+            return isCavern && !isHoldingUnit();
+        }
+
+
+        @Override
+        public int compareTo(GameField otherField) {
+            return new CompareToBuilder().append(this.y, otherField.y).append(this.x, otherField.x).toComparison();
+        }
+
+
+        @Override
+        public String toString() {
+            if (!isCavern()) {
+                return String.format("(%2d,%2d)=#", x, y);
+            } else if (isHoldingUnit()) {
+                GameFaction faction = unit.getFaction();
+                return String.format("(%2d,%2d)=%s", x, y, faction.getUnitFactionLetter());
+            } else {
+                return String.format("(%2d,%2d)=.", x, y);
+            }
+        }
+
+    }
+
+
+    public enum GameFaction {
+        ELVE('E'), GOBLIN('G');
+
+        char unitFactionLetter;
+
+        GameFaction(char unitFactionLetter) {
+            this.unitFactionLetter = unitFactionLetter;
+        }
+
+        public char getUnitFactionLetter() {
+            return unitFactionLetter;
+        }
+
+    }
 
 
     public static void main(String[] args) throws Exception {
--- 2018/src/test/java/de/adventofcode/chrisgw/day15/BeverageBanditsTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day15/BeverageBanditsTest.java
@@ -1,8 +1,130 @@
 package de.adventofcode.chrisgw.day15;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class BeverageBanditsTest {
+
+
+    @Test
+    public void moveIntoRangeOfNearestEnemyUnit_example01() {
+        List<String> gameAreaLines = Arrays.asList( //
+                "#######", //
+                "#E..G.#", //
+                "#...#.#", //
+                "#.G.#G#", //
+                "#######");
+        String expectedGameStr = "" //
+                + "#######\n" //
+                + "#.E.G.#\n" //
+                + "#...#.#\n" //
+                + "#.G.#G#\n" //
+                + "#######";
+
+        BeverageBandits game = new BeverageBandits(gameAreaLines);
+        System.out.println(game);
+        System.out.println();
+        game.nextTurn();
+
+        String gameAreaStr = game.toString();
+        System.out.println(gameAreaStr);
+        Assert.assertEquals("gameAreaStr", expectedGameStr, gameAreaStr);
+    }
+
+
+    @Test
+    public void moveIntoRangeOfNearestEnemyUnit_example02() {
+        List<String> gameAreaLines = Arrays.asList( //
+                "#######", //
+                "#.E...#", //
+                "#.....#", //
+                "#...G.#", //
+                "#######");
+        String expectedGameStr = "" //
+                + "#######\n" //
+                + "#..E..#\n" //
+                + "#.....#\n" //
+                + "#...G.#\n" //
+                + "#######";
+
+        BeverageBandits game = new BeverageBandits(gameAreaLines);
+        System.out.println(game);
+        System.out.println();
+        game.nextTurn();
+
+        String gameAreaStr = game.toString();
+        System.out.println(gameAreaStr);
+        Assert.assertEquals("gameAreaStr", expectedGameStr, gameAreaStr);
+    }
+
+
+    @Test
+    public void nextTurn_example01() {
+        List<String> gameAreaLines = Arrays.asList( //
+                "#########", //
+                "#G..G..G#", //
+                "#.......#", //
+                "#.......#", //
+                "#G..E..G#", //
+                "#.......#", //
+                "#.......#", //
+                "#G..G..G#", //
+                "#########");
+
+        List<String> expectedGameTurnStr = Arrays.asList( //
+                "" // round 00
+                        + "#########\n" //
+                        + "#G..G..G#\n" //
+                        + "#.......#\n" //
+                        + "#.......#\n" //
+                        + "#G..E..G#\n" //
+                        + "#.......#\n" //
+                        + "#.......#\n" //
+                        + "#G..G..G#\n" //
+                        + "#########", //
+                "" // round 01
+                        + "#########\n" //
+                        + "#.G...G.#\n" //
+                        + "#...G...#\n" //
+                        + "#...E..G#\n" //
+                        + "#.G.....#\n" //
+                        + "#.......#\n" //
+                        + "#G..G..G#\n" //
+                        + "#.......#\n" //
+                        + "#########", //
+                "" // round 02
+                        + "#########\n" //
+                        + "#..G.G..#\n" //
+                        + "#...G...#\n" //
+                        + "#.G.E.G.#\n" //
+                        + "#.......#\n" //
+                        + "#G..G..G#\n" //
+                        + "#.......#\n" //
+                        + "#.......#\n" //
+                        + "#########", //
+                "" // round 03
+                        + "#########\n" //
+                        + "#.......#\n" //
+                        + "#..GGG..#\n" //
+                        + "#..GEG..#\n" //
+                        + "#G..G...#\n" //
+                        + "#......G#\n" //
+                        + "#.......#\n" //
+                        + "#.......#\n" //
+                        + "#########");
+
+        BeverageBandits game = new BeverageBandits(gameAreaLines);
+
+        for (int trun = 0; trun < expectedGameTurnStr.size(); trun++) {
+            String gameAreaStr = game.toString();
+            System.out.println(gameAreaStr);
+            Assert.assertEquals("gameAreaStr", expectedGameTurnStr.get(trun), gameAreaStr);
+            game.nextBattleRound();
+        }
+    }
 
 }
