f929104f275c1afe53f089bf4aa7b4c74869b828
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-17 10:49:14 +0100

    2018 Day 16: Chronal Classification - start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day16/ChronalClassification.java
@@ -0,0 +1,22 @@
+package de.adventofcode.chrisgw.day16;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class ChronalClassification {
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Usage <puzzleInputFile>");
+            return;
+        }
+        Path puzzleInputFile = Paths.get(args[0]);
+
+    }
+
+}
---
+++ 2018/src/test/java/de/adventofcode/chrisgw/day16/ChronalClassificationTest.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day16;
+
+import static org.junit.Assert.*;
+
+
+public class ChronalClassificationTest {
+
+}
