803d20467e161d2567f4efa3151e1d59005fa28b
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-18 08:50:59 +0100

    2018 Day 17: Reservoir Research - start

---
+++ 2018/src/main/java/de/adventofcode/chrisgw/day17/ReservoirResearch.java
@@ -0,0 +1,19 @@
+package de.adventofcode.chrisgw.day17;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class ReservoirResearch {
+
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Usage <puzzleInputFile>");
+            return;
+        }
+        Path puzzleInputFile = Paths.get(args[0]);
+
+    }
+
+}
