58acffaa47c58c956c453d5eeec2399207d662f7
Chris-GW <Chris-GW@users.noreply.github.com>
2018-12-20 16:37:14 +0100

    some more non working stuff

--- 2018/src/main/java/de/adventofcode/chrisgw/day20/ARegularMap.java
+++ 2018/src/main/java/de/adventofcode/chrisgw/day20/ARegularMap.java
@@ -1,13 +1,14 @@
 package de.adventofcode.chrisgw.day20;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Value;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class ARegularMap {
@@ -23,12 +24,44 @@ public class ARegularMap {
 
     public ARegularMap(String roomRouteRegex) {
         this.roomRouteRegex = roomRouteRegex;
-        this.startRoom = new FacilityRoom();
+        this.startRoom = new FacilityRoom(0, 0);
         putFacilityRoom(this.startRoom);
+        followRouteRegex();
+    }
+
+    private FacilityRoute followRouteRegex() {
+        Stack<FacilityRoute> branchingRouteStartStack = new Stack<>();
+        Stack<FacilityRoute> routeStack = new Stack<>();
+        routeStack.push(new FacilityRoute());
+
+        for (int i = 1; i < roomRouteRegex.length() - 1; i++) {
+            char directionLetter = roomRouteRegex.charAt(i);
+            if (directionLetter == '(') {
+                FacilityRoute branchingRoute = routeStack.peek();
+                branchingRouteStartStack.push(branchingRoute);
+                routeStack.push(new FacilityRoute(branchingRoute));
+
+            } else if (directionLetter == '|') {
+                FacilityRoute newFacilityRoute = new FacilityRoute(branchingRouteStartStack.peek());
+                routeStack.push(newFacilityRoute);
+
+            } else if (directionLetter == ')') {
+                FacilityRoute branchingRoutePoint = branchingRouteStartStack.pop();
+                while (branchingRoutePoint != routeStack.peek()) {
+                    FacilityRoute branchingRoute = routeStack.pop();
+                    branchingRoutePoint.branchingRoutes.add(branchingRoute);
+                }
+
+            } else {
+                FacilityRouteDirection direction = FacilityRouteDirection.valueOf(directionLetter);
+                routeStack.peek().addDirection(direction);
+            }
+        }
+        return routeStack.pop();
     }
 
 
-    public Optional<FacilityRoom> facilityRoomAt(int x, int y) {
+    public Optional<FacilityRoom> roomAt(int x, int y) {
         Map<Integer, FacilityRoom> row = facilityRoomMap.getOrDefault(y, Collections.emptyMap());
         return Optional.ofNullable(row.get(x));
     }
@@ -36,11 +69,113 @@ public class ARegularMap {
     private FacilityRoom putFacilityRoom(FacilityRoom facilityRoom) {
         int x = facilityRoom.getX();
         int y = facilityRoom.getY();
-        Map<Integer, FacilityRoom> row = facilityRoomMap.computeIfAbsent(y, HashMap::new);
+        Map<Integer, FacilityRoom> row = facilityRoomMap.computeIfAbsent(y, (yVal) -> new HashMap<>());
         return row.put(x, facilityRoom);
     }
 
+    public int longestRouteLength() {
+        return 0;
+    }
 
+
+    @EqualsAndHashCode
+    public class FacilityRoute {
+
+        private List<FacilityRouteDirection> walkedDirections = new ArrayList<>();
+        private List<FacilityRoom> walkedRooms = new ArrayList<>();
+        private List<FacilityRoute> branchingRoutes = new ArrayList<>();
+
+
+        private FacilityRoute() {
+            this.walkedRooms.add(startRoom);
+        }
+
+        private FacilityRoute(FacilityRoute otherRoute) {
+            this.walkedDirections.addAll(otherRoute.walkedDirections);
+            this.walkedRooms.addAll(otherRoute.walkedRooms);
+        }
+
+
+        private void addDirection(FacilityRouteDirection direction) {
+            walkedDirections.add(direction);
+            FacilityRoom destinationRoom = getDestinationRoom();
+            int x = destinationRoom.getX() + direction.getDx();
+            int y = destinationRoom.getY() + direction.getDy();
+            walkedRooms.add(roomAt(x, y).orElseGet(() -> new FacilityRoom(x, y)));
+        }
+
+
+        public Stream<FacilityRoom> facilityRooms() {
+            return walkedRooms.stream();
+        }
+
+        public FacilityRoom getStartRoom() {
+            return walkedRooms.get(0);
+        }
+
+        public FacilityRoom getDestinationRoom() {
+            return walkedRooms.get(walkedRooms.size() - 1);
+        }
+
+
+        public Stream<FacilityRouteDirection> directions() {
+            return walkedDirections.stream();
+        }
+
+
+        public int length() {
+            return walkedDirections.size();
+        }
+
+
+        @Override
+        public String toString() {
+            return walkedDirections.stream().map(FacilityRouteDirection::letter).collect(Collectors.joining(""));
+        }
+
+    }
+
+
+    @Value
+    public class FacilityRoom {
+
+        private final int x;
+        private final int y;
+
+
+        private FacilityRoom(int x, int y) {
+            this.x = x;
+            this.y = y;
+            putFacilityRoom(this);
+        }
+
+
+        public boolean hasRoomTo(FacilityRouteDirection direction) {
+            int x = getX() + direction.getDx();
+            int y = getX() + direction.getDy();
+            return roomAt(x, y).isPresent();
+        }
+
+        public FacilityRoom getRoomTo(FacilityRouteDirection direction) {
+            int x = getX() + direction.getDx();
+            int y = getX() + direction.getDy();
+            return roomAt(x, y).orElse(null);
+        }
+
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("(").append(x).append(",").append(y).append("): ");
+            for (FacilityRouteDirection direction : FacilityRouteDirection.values()) {
+                if (hasRoomTo(direction)) {
+                    sb.append(direction.letter());
+                }
+            }
+            return sb.toString();
+        }
+
+    }
 
 
     public static void main(String[] args) throws Exception {
--- 2018/src/test/java/de/adventofcode/chrisgw/day20/ARegularMapTest.java
+++ 2018/src/test/java/de/adventofcode/chrisgw/day20/ARegularMapTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static de.adventofcode.chrisgw.day20.FacilityRouteDirection.*;
 import static org.junit.Assert.*;
 
 
@@ -18,52 +17,6 @@ public class ARegularMapTest {
     private final String roomRouteRegex_example03 = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
     private final String roomRouteRegex_example04 = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
     private final String roomRouteRegex_example05 = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$";
-
-    @Test
-    public void example_01() {
-        FacilityRoute expectedRootRoute = new FacilityRoute();
-        expectedRootRoute.addDirection(WEST);
-        expectedRootRoute.addDirection(NORTH);
-        expectedRootRoute.addDirection(EAST);
-
-        ARegularMap facilityMap = new ARegularMap(roomRouteRegex_example01);
-        FacilityRoute rootRoute = facilityMap.getRootRoute();
-        assertEquals("rootRoute", expectedRootRoute, rootRoute);
-
-    }
-
-    @Test
-    public void example_02() {
-        FacilityRoute expectedRootRoute = new FacilityRoute();
-        expectedRootRoute.addDirections(EAST, NORTH, WEST, WEST, WEST);
-        FacilityRoute firstBranchRoute = new FacilityRoute().addDirections(NORTH, EAST, EAST, EAST);
-        expectedRootRoute.addBranchingRoutes(firstBranchRoute);
-
-        FacilityRoute secondBranchRoute = new FacilityRoute().addDirections(SOUTH, SOUTH, EAST)
-                .addBranchingRoutes(new FacilityRoute().addDirections(EAST, EAST),
-                        new FacilityRoute().addDirections(NORTH));
-        expectedRootRoute.addBranchingRoutes(secondBranchRoute);
-
-        ARegularMap facilityMap = new ARegularMap(roomRouteRegex_example02);
-        FacilityRoute rootRoute = facilityMap.getRootRoute();
-        assertEquals("rootRoute", expectedRootRoute, rootRoute);
-    }
-
-    @Test
-    public void example_03() {
-        FacilityRoute expectedRootRoute = new FacilityRoute();
-        expectedRootRoute.addDirections(EAST, NORTH, NORTH, WEST, SOUTH, WEST, WEST)
-                .addBranchingRoutes(new FacilityRoute().addDirections(NORTH, EAST, WEST, SOUTH), new FacilityRoute())
-                .addDirections(SOUTH, SOUTH, SOUTH, EAST, EAST, NORTH)
-                .addBranchingRoutes(new FacilityRoute().addDirections(WEST, NORTH, SOUTH, EAST), new FacilityRoute())
-                .addDirections(EAST, EAST)
-                .addBranchingRoutes(new FacilityRoute().addDirections(SOUTH, WEST, EAST, NORTH), new FacilityRoute())
-                .addDirections(NORTH, NORTH, NORTH);
-
-        ARegularMap facilityMap = new ARegularMap(roomRouteRegex_example03);
-        FacilityRoute rootRoute = facilityMap.getRootRoute();
-        assertEquals("rootRoute", expectedRootRoute, rootRoute);
-    }
 
 
     @Test
