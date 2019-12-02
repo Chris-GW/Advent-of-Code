f6db04a6cf488754d6e84fc7407ee1541b9b9e0f
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-02 12:41:20 +0100

    2019 Day 1: The Tyranny of the Rocket Equation - part01

--- 2019/src/main/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01.java
+++ 2019/src/main/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01.java
@@ -5,6 +5,9 @@ package de.adventofcode.chrisgw.day01;
  */
 public class AdventOfCodeDay01 {
 
-
+    public static long calculateModuleFuelRequirement(long moduleMass) {
+        long fuel = moduleMass / 3;
+        return fuel - 2;
+    }
 
 }
