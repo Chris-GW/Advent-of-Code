c5e37ee7f77e909e487ea8d7aa0f0750ad29f224
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-02 12:41:20 +0100

    2019 Day 1: The Tyranny of the Rocket Equation - part01

--- 2019/src/test/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01Test.java
+++ 2019/src/test/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01Test.java
@@ -1,8 +1,64 @@
 package de.adventofcode.chrisgw.day01;
 
+import de.adventofcode.chrisgw.TestUtils;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static de.adventofcode.chrisgw.day01.AdventOfCodeDay01.calculateModuleFuelRequirement;
 import static org.junit.Assert.*;
 
 
 public class AdventOfCodeDay01Test {
+
+    @Test
+    public void calculateModuleFuelRequirement_12() {
+        long moduleMass = 12;
+        long expectedFuel = 2;
+
+        long fuel = calculateModuleFuelRequirement(moduleMass);
+        assertEquals("fuel", expectedFuel, fuel);
+    }
+
+    @Test
+    public void calculateModuleFuelRequirement_14() {
+        long moduleMass = 14;
+        long expectedFuel = 2;
+
+        long fuel = calculateModuleFuelRequirement(moduleMass);
+        assertEquals("fuel", expectedFuel, fuel);
+    }
+
+    @Test
+    public void calculateModuleFuelRequirement_1969() {
+        long moduleMass = 1969;
+        long expectedFuel = 654;
+
+        long fuel = calculateModuleFuelRequirement(moduleMass);
+        assertEquals("fuel", expectedFuel, fuel);
+    }
+
+    @Test
+    public void calculateModuleFuelRequirement_100756() {
+        long moduleMass = 100756;
+        long expectedFuel = 33583;
+
+        long fuel = calculateModuleFuelRequirement(moduleMass);
+        assertEquals("fuel", expectedFuel, fuel);
+    }
+
+
+    @Test
+    public void calculateTotalModuleFuelRequirement_part01() {
+        long expectedTotalFuel = 3442987;
+        List<Long> moduleMassList = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay01.txt")
+                .stream()
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        long totalFuel = moduleMassList.stream().mapToLong(AdventOfCodeDay01::calculateModuleFuelRequirement).sum();
+        assertEquals("totalFuel", expectedTotalFuel, totalFuel);
+    }
 
 }
