ed8af9b00c91921825b8f99758e7d441c8b5421d
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-04 08:33:36 +0100

    2019 Day 4: Secure Container - part01

--- 2019/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
+++ 2019/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
@@ -1,9 +1,32 @@
 package de.adventofcode.chrisgw.day04;
 
+import java.util.stream.IntStream;
+
+
 /**
  * https://adventofcode.com/2019/day/4
  */
 public class AdventOfCodeDay04 {
 
+
+    public static long countPossiblePasswords(int rangeFrom, int rangeTo) {
+        return IntStream.rangeClosed(rangeFrom, rangeTo).filter(AdventOfCodeDay04::isValidPassword).count();
+    }
+
+    private static boolean isValidPassword(int password) {
+        String passwordStr = String.valueOf(password);
+        boolean hasTwoSameAdjacentDigits = false;
+        int lastDigit = -1;
+        for (int i = 0; i < passwordStr.length(); i++) {
+            int currentDigit = passwordStr.charAt(i) - '0';
+            if (lastDigit == currentDigit) {
+                hasTwoSameAdjacentDigits = true;
+            } else if (lastDigit > currentDigit) {
+                return false;
+            }
+            lastDigit = currentDigit;
+        }
+        return hasTwoSameAdjacentDigits;
+    }
 
 }
--- 2019/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
+++ 2019/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
@@ -1,8 +1,19 @@
 package de.adventofcode.chrisgw.day04;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
 public class AdventOfCodeDay04Test {
+
+    @Test
+    public void myPuzzleInput() {
+        long expectedPossiblePasswords = 966;
+        int rangeFrom = 264793;
+        int rangeTo = 803935;
+        long possiblePasswords = AdventOfCodeDay04.countPossiblePasswords(rangeFrom, rangeTo);
+        assertEquals("possiblePasswords", expectedPossiblePasswords, possiblePasswords);
+    }
 
 }
