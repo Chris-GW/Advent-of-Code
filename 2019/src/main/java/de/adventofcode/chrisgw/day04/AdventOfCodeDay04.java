fc9cf8a41d8890f05d1d4418320f72464fb68ba9
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-04 08:17:00 +0100

    2019 Day 4: Secure Container - start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
@@ -0,0 +1,9 @@
+package de.adventofcode.chrisgw.day04;
+
+/**
+ * https://adventofcode.com/2019/day/4
+ */
+public class AdventOfCodeDay04 {
+
+
+}
---
+++ 2019/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day04;
+
+import static org.junit.Assert.*;
+
+
+public class AdventOfCodeDay04Test {
+
+}
