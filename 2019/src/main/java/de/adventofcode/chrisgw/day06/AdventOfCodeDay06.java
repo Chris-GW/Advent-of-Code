1299fd586ffc5e87135c4720851af4f279ae876d
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-06 08:28:32 +0100

    2019 Day 06: Universal Orbit Map - start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
@@ -0,0 +1,9 @@
+package de.adventofcode.chrisgw.day06;
+
+/**
+ * 2019 Day 6: Universal Orbit Map
+ * https://adventofcode.com/2019/day/6
+ */
+public class AdventOfCodeDay06 {
+
+}
---
+++ 2019/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day06;
+
+import static org.junit.Assert.*;
+
+
+public class AdventOfCodeDay06Test {
+
+}
