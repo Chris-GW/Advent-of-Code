a936ab5fcca0ba248ab86a3b3acef331a92a110f
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-06 09:55:47 +0100

    2019 Day 06: Universal Orbit Map - part02

--- 2019/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
+++ 2019/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
@@ -12,7 +12,7 @@ public class AdventOfCodeDay06Test {
 
 
     @Test
-    public void example01() {
+    public void example01_part01_countTotalOrbits() {
         List<String> orbitMapLines = List.of( //
                 "COM)B", //
                 "B)C", //
@@ -34,13 +34,51 @@ public class AdventOfCodeDay06Test {
 
 
     @Test
-    public void myPuzzleInput_part01() {
+    public void myPuzzleInput_part01_countTotalOrbits() {
         List<String> orbitMapLines = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay06.txt");
         long expectedTotalOrbits = 358244;
 
         AdventOfCodeDay06 aocDay06 = AdventOfCodeDay06.parseUniversalOrbitMap(orbitMapLines);
         long totalOrbits = aocDay06.countTotalOrbits();
         assertEquals("totalOrbits", expectedTotalOrbits, totalOrbits);
+    }
+
+
+    // part 02
+
+
+    @Test
+    public void example01_part02_countNeededOrbitalTransfers() {
+        List<String> orbitMapLines = List.of( //
+                "COM)B", //
+                "B)C", //
+                "C)D", //
+                "D)E", //
+                "E)F", //
+                "B)G", //
+                "G)H", //
+                "D)I", //
+                "E)J", //
+                "J)K", //
+                "K)L", //
+                "K)YOU", //
+                "I)SAN");
+        int expectedNeededOrbitalTransfers = 4;
+
+        AdventOfCodeDay06 aocDay06 = AdventOfCodeDay06.parseUniversalOrbitMap(orbitMapLines);
+        int neededOrbitalTransfers = aocDay06.countNeededOrbitalTransfers();
+        assertEquals("neededOrbitalTransfers", expectedNeededOrbitalTransfers, neededOrbitalTransfers);
+    }
+
+
+    @Test
+    public void myPuzzleInput_part02() {
+        List<String> orbitMapLines = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay06.txt");
+        int expectedNeededOrbitalTransfers = 517;
+
+        AdventOfCodeDay06 aocDay06 = AdventOfCodeDay06.parseUniversalOrbitMap(orbitMapLines);
+        int neededOrbitalTransfers = aocDay06.countNeededOrbitalTransfers();
+        assertEquals("neededOrbitalTransfers", expectedNeededOrbitalTransfers, neededOrbitalTransfers);
     }
 
 }
