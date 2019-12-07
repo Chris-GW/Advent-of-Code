4d6d5c9d4275ed5a26aa3f8e8b0f370e4c178e10
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-07 15:25:57 +0100

    2019 Day 7: Amplification Circuit - part02

--- 2019/src/test/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07Test.java
+++ 2019/src/test/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07Test.java
@@ -57,4 +57,43 @@ public class AdventOfCodeDay07Test {
         assertEquals("highestThrusterSignal", expectedHighestThrusterSignal, highestThrusterSignal);
     }
 
+    // part 02
+
+    @Test
+    public void example04_part02() {
+        int[] initialState = new int[] { 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, //
+                27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5 };
+        int expectedHighestThrusterSignal = 139629729;
+
+        IntCodeProgram controllerSoftware = new IntCodeProgram(initialState);
+        AdventOfCodeDay07 aocDay07 = new AdventOfCodeDay07(controllerSoftware);
+        int highestThrusterSignal = aocDay07.findHighestThrusterSignalWithFeedbackLoop();
+        assertEquals("highestThrusterSignal", expectedHighestThrusterSignal, highestThrusterSignal);
+    }
+
+    @Test
+    public void example05_part02() {
+        int[] initialState = new int[] { //
+                3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, //
+                -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, //
+                53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10 };
+        int expectedHighestThrusterSignal = 18216;
+
+        IntCodeProgram controllerSoftware = new IntCodeProgram(initialState);
+        AdventOfCodeDay07 aocDay07 = new AdventOfCodeDay07(controllerSoftware);
+        int highestThrusterSignal = aocDay07.findHighestThrusterSignalWithFeedbackLoop();
+        assertEquals("highestThrusterSignal", expectedHighestThrusterSignal, highestThrusterSignal);
+    }
+
+    @Test
+    public void myPuzzleInput_part02() {
+        String intCodeProgrammStr = TestUtils.readSingleLineOfClassPathResource("/puzzleInputDay07.txt");
+        int expectedHighestThrusterSignal = 5371621;
+
+        IntCodeProgram controllerSoftware = IntCodeProgram.parseIntCodeProgram(intCodeProgrammStr);
+        AdventOfCodeDay07 aocDay07 = new AdventOfCodeDay07(controllerSoftware);
+        int highestThrusterSignal = aocDay07.findHighestThrusterSignalWithFeedbackLoop();
+        assertEquals("highestThrusterSignal", expectedHighestThrusterSignal, highestThrusterSignal);
+    }
+
 }
