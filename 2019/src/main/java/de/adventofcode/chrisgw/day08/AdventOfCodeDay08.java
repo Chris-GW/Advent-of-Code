ae48d844a991e794ccf09be55858cfd2c6171731
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-08 09:45:59 +0100

    2019 Day 8: Space Image Format - part02

--- 2019/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
+++ 2019/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
@@ -16,8 +16,6 @@ public class AdventOfCodeDay08 {
     }
 
 
-
-
     public LayeredPixelImage findFewest0DigitsImageLayer() {
         return layeredPixelImage.imageLayers()
                 .min(Comparator.comparingInt(pixelImage -> pixelImage.pixelColorCount(0)))
@@ -29,6 +27,29 @@ public class AdventOfCodeDay08 {
         int digit1Count = fewest0DigitsImageLayer.pixelColorCount(1);
         int digit2Count = fewest0DigitsImageLayer.pixelColorCount(2);
         return digit1Count * digit2Count;
+    }
+
+
+    public String printVisiblePixelImagePart02() {
+        LayeredPixelImage visiblePixelImage = layeredPixelImage.visiblePixelImage();
+        StringBuilder sb = new StringBuilder(visiblePixelImage.imageLayerSize());
+        for (int row = 0; row < visiblePixelImage.getHeight(); row++) {
+            for (int column = 0; column < visiblePixelImage.getWidth(); column++) {
+                int pixelDataAt = visiblePixelImage.pixelDataAt(row, column);
+                if (pixelDataAt == LayeredPixelImage.PIXEL_DATA_BLACK) {
+                    sb.append("\u2588");
+                } else if (pixelDataAt == LayeredPixelImage.PIXEL_DATA_WHITE) {
+                    sb.append("\u2591");
+                } else {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        String visiblePixelImageStr = sb.toString();
+        System.out.println(visiblePixelImageStr);
+        return visiblePixelImageStr;
     }
 
 }
--- 2019/src/main/java/de/adventofcode/chrisgw/day08/LayeredPixelImage.java
+++ 2019/src/main/java/de/adventofcode/chrisgw/day08/LayeredPixelImage.java
@@ -11,6 +11,10 @@ import java.util.stream.Stream;
 @Data
 public class LayeredPixelImage {
 
+    public static final int PIXEL_DATA_BLACK = 0;
+    public static final int PIXEL_DATA_WHITE = 1;
+    public static final int PIXEL_DATA_TRANSPARENT = 2;
+
     private final int width;
     private final int height;
     private final int[] imageData;
@@ -18,6 +22,9 @@ public class LayeredPixelImage {
 
     public static LayeredPixelImage parseLayeredPixelImageStr(int width, int height, String imageDataStr) {
         int[] imageData = imageDataStr.chars().map(numberChar -> numberChar - '0').toArray();
+        if (!Arrays.stream(imageData).allMatch(value -> 0 <= value && value <= 2)) {
+            throw new IllegalArgumentException("unknown pixelData");
+        }
         return new LayeredPixelImage(width, height, imageData);
     }
 
@@ -39,13 +46,38 @@ public class LayeredPixelImage {
     }
 
 
-    public int imageDataAt(int row, int column) {
+    public int pixelDataAt(int row, int column) {
         int index = (row * width) + column;
         return imageData[index];
     }
 
+    private void setPixelDataAt(int row, int column, int pixelData) {
+        int index = (row * width) + column;
+        imageData[index] = pixelData;
+    }
+
+    public int visiblePixelDataAt(int row, int column) {
+        return imageLayers().mapToInt(layeredPixelImage -> layeredPixelImage.pixelDataAt(row, column))
+                .filter(pixelData -> pixelData != PIXEL_DATA_TRANSPARENT)
+                .findFirst()
+                .orElse(PIXEL_DATA_TRANSPARENT);
+    }
+
     public int pixelColorCount(int pixelColor) {
         return (int) Arrays.stream(imageData).filter(value -> value == pixelColor).count();
+    }
+
+
+    public LayeredPixelImage visiblePixelImage() {
+        int[] visibleImageData = new int[imageLayerSize()];
+        LayeredPixelImage layeredPixelImage = new LayeredPixelImage(width, height, visibleImageData);
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                int visiblePixelData = visiblePixelDataAt(row, column);
+                layeredPixelImage.setPixelDataAt(row, column, visiblePixelData);
+            }
+        }
+        return layeredPixelImage;
     }
 
 
@@ -72,7 +104,7 @@ public class LayeredPixelImage {
             LayeredPixelImage layeredPixelImage = pixelImageIterator.next();
             for (int row = 0; row < layeredPixelImage.getHeight(); row++) {
                 for (int column = 0; column < layeredPixelImage.getWidth(); column++) {
-                    int pixelData = layeredPixelImage.imageDataAt(row, column);
+                    int pixelData = layeredPixelImage.pixelDataAt(row, column);
                     sb.append(pixelData);
                 }
                 sb.append("\n").append(leftPadding);
@@ -80,7 +112,7 @@ public class LayeredPixelImage {
             sb.setLength(sb.length() - leftPadding.length());
             sb.append("\n");
         }
-        sb.deleteCharAt(sb.length() - 1);
+        sb.setLength(sb.length() - 2);
         return sb.toString();
     }
 
