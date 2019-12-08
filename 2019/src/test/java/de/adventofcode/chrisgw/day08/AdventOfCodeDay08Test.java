138f24141bcbec94aece3dc768da2428fda85b73
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-08 09:45:59 +0100

    2019 Day 8: Space Image Format - part02

--- 2019/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
+++ 2019/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
@@ -31,9 +31,45 @@ public class AdventOfCodeDay08Test {
         int expectedSolution = 1474;
 
         LayeredPixelImage layeredPixelImage = LayeredPixelImage.parseLayeredPixelImageStr(wide, height, imageDataStr);
-        System.out.println(layeredPixelImage);
         AdventOfCodeDay08 aocDay08 = new AdventOfCodeDay08(layeredPixelImage);
         int solution = aocDay08.calculatePart01();
+        assertEquals("solution part01", expectedSolution, solution);
+    }
+
+
+    // part 02
+
+    @Test
+    public void example02_part02() {
+        int wide = 2;
+        int height = 2;
+        int[] imageData = new int[] { 0, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 2, 0, 0, 0, 0 };
+        String expectedSolution = "" //
+                + "█░\n"  //
+                + "░█";
+
+        LayeredPixelImage layeredPixelImage = new LayeredPixelImage(wide, height, imageData);
+        AdventOfCodeDay08 aocDay08 = new AdventOfCodeDay08(layeredPixelImage);
+        String solution = aocDay08.printVisiblePixelImagePart02();
+        assertEquals("solution part02", expectedSolution, solution);
+    }
+
+    @Test
+    public void myPuzzleInput_part02() {
+        String imageDataStr = TestUtils.readSingleLineOfClassPathResource("/puzzleInputDay08.txt");
+        int wide = 25;
+        int height = 6;
+        String expectedSolution = "" //
+                + "██░░██░░██░░░███░░██░░░██\n" //
+                + "███░█░██░█░██░█░██░█░██░█\n" //
+                + "███░█░████░██░█░████░░░██\n" //
+                + "███░█░████░░░██░████░██░█\n" //
+                + "░██░█░██░█░█░██░██░█░██░█\n" //
+                + "█░░███░░██░██░██░░██░░░██";
+
+        LayeredPixelImage layeredPixelImage = LayeredPixelImage.parseLayeredPixelImageStr(wide, height, imageDataStr);
+        AdventOfCodeDay08 aocDay08 = new AdventOfCodeDay08(layeredPixelImage);
+        String solution = aocDay08.printVisiblePixelImagePart02();
         assertEquals("solution part01", expectedSolution, solution);
     }
 
