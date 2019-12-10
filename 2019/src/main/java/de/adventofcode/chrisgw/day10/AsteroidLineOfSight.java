0d1cb78b13d9abcde873a6e24d4faed035e0afe1
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-10 17:28:44 +0100

    2019 Day 10: Monitoring Station - part02

--- 2019/src/main/java/de/adventofcode/chrisgw/day10/AsteroidLineOfSight.java
+++ 2019/src/main/java/de/adventofcode/chrisgw/day10/AsteroidLineOfSight.java
@@ -10,36 +10,40 @@ import java.util.stream.Stream;
 public class AsteroidLineOfSight {
 
     private final AsteroidMapLocation fromLocation;
+    private final int dx;
+    private final int dy;
     private final AsteroidMapLocation toLocation;
 
 
-    public Stream<AsteroidMapLocation> blockedLocations() {
-        return Stream.iterate(fromLocation, Objects::nonNull, this::nextLocationAlong)
-                .dropWhile(location -> fromLocation.distanceTo(location) <= distance());
+    public static AsteroidLineOfSight between(AsteroidMapLocation fromLocation, AsteroidMapLocation toLocation) {
+        int dx = toLocation.getX() - fromLocation.getX();
+        int dy = toLocation.getY() - fromLocation.getY();
+        int gcd = Math.abs(greatestCommonDivisor(dx, dy));
+        dx /= gcd;
+        dy /= gcd;
+        return new AsteroidLineOfSight(fromLocation, dx, dy, toLocation);
     }
 
 
-    public int distance() {
-        return fromLocation.distanceTo(toLocation);
+    public Stream<AsteroidMapLocation> blockedLocations() {
+        return Stream.iterate(toLocation, Objects::nonNull, this::nextLocationAlong).skip(1);
     }
 
 
     private AsteroidMapLocation nextLocationAlong(AsteroidMapLocation location) {
-        int dx = toLocation.getX() - fromLocation.getX();
-        int dy = toLocation.getY() - fromLocation.getY();
-        if (dx == 0) {
-            dy /= Math.abs(dy);
-        } else if (dy == 0) {
-            dx /= Math.abs(dx);
-        } else {
-            int gcd = Math.abs(greatestCommonDivisor(dx, dy));
-            dx /= gcd;
-            dy /= gcd;
-        }
-
         int x = location.getX() + dx;
         int y = location.getY() + dy;
         return location.getAsteriodMap().asteroidAt(x, y);
+    }
+
+
+    public double winkel() {
+        double degrees = Math.toDegrees(winkel(dx, dy));
+        degrees -= 180;
+        if (degrees < 0) {
+            degrees += 360;
+        }
+        return degrees;
     }
 
 
@@ -48,6 +52,15 @@ public class AsteroidLineOfSight {
             return n1;
         }
         return greatestCommonDivisor(n2, n1 % n2);
+    }
+
+
+    public static double winkel(int x2, int y2) {
+        int x1 = 0;
+        int y1 = 1;
+        int dot = x1 * x2 + y1 * y2;
+        int det = x1 * y2 - y1 * x2;
+        return Math.atan2(det, dot);
     }
 
 }
