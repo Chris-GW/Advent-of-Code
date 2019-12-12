5a19414ddb0b077bd781e0d49ba78dc0aea03e46
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-12 08:24:56 +0100

    2019 Day 12: The N-Body Problem - start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12.java
@@ -0,0 +1,10 @@
+package de.adventofcode.chrisgw.day12;
+
+/**
+ * 2019 Day 12: The N-Body Problem
+ * https://adventofcode.com/2019/day/12
+ */
+public class AdventOfCodeDay12 {
+
+
+}
---
+++ 2019/src/test/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12Test.java
@@ -0,0 +1,5 @@
+package de.adventofcode.chrisgw.day12;
+
+public class AdventOfCodeDay12Test {
+
+}
