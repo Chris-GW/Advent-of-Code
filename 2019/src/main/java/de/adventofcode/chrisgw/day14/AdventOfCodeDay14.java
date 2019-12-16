6710ef3a0efffa5f228c32aaf36d4c0b56c0ce1d
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-16 19:44:59 +0100

    2019 Day 14: Space Stoichiometry -start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day14/AdventOfCodeDay14.java
@@ -0,0 +1,23 @@
+package de.adventofcode.chrisgw.day14;
+
+import de.adventofcode.chrisgw.day13.ArcadeCabinet;
+import de.adventofcode.chrisgw.day13.JoystickInput;
+import de.adventofcode.chrisgw.day13.ScreenTileType;
+import de.adventofcode.chrisgw.intcode.IntCodeProgram;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static de.adventofcode.chrisgw.day13.JoystickInput.*;
+
+
+/**
+ * 2019 Day 14: Space Stoichiometry
+ * https://adventofcode.com/2019/day/14
+ */
+public class AdventOfCodeDay14 {
+
+}
