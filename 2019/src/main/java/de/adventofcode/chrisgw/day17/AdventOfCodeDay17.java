3c7efbfd8e83d24292c502779e9df6aad6da8ae9
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-17 09:26:45 +0100

    2019 Day 17: Set and Forget - start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day17/AdventOfCodeDay17.java
@@ -0,0 +1,10 @@
+package de.adventofcode.chrisgw.day17;
+
+/**
+ * 2019 Day 17: Set and Forget
+ * https://adventofcode.com/2019/day/17
+ */
+public class AdventOfCodeDay17 {
+
+
+}
---
+++ 2019/src/test/java/de/adventofcode/chrisgw/day17/AdventOfCodeDay17Test.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day17;
+
+import static org.junit.Assert.*;
+
+
+public class AdventOfCodeDay17Test {
+
+}
