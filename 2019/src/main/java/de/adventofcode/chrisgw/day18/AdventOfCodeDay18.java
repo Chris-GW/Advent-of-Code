1029ff4964f87a9d50d2755a4850ddf1e940a9b8
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-18 08:43:40 +0100

    2019 Day 18: Many-Worlds Interpretation - start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day18/AdventOfCodeDay18.java
@@ -0,0 +1,9 @@
+package de.adventofcode.chrisgw.day18;
+
+/**
+ * 2019 Day 18: Many-Worlds Interpretation
+ * https://adventofcode.com/2019/day/18
+ */
+public class AdventOfCodeDay18 {
+
+}
---
+++ 2019/src/test/java/de/adventofcode/chrisgw/day18/AdventOfCodeDay18Test.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day18;
+
+import static org.junit.Assert.*;
+
+
+public class AdventOfCodeDay18Test {
+
+}
