019a1599c570dd438d9f5cc1ca19fe3be68149c5
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-19 15:51:12 +0100

    2019 Day 15: Oxygen System - start

---
+++ 2019/src/main/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15.java
@@ -0,0 +1,9 @@
+package de.adventofcode.chrisgw.day15;
+
+/**
+ * 2019 Day 15: Oxygen System
+ * https://adventofcode.com/2019/day/15
+ */
+public class AdventOfCodeDay15 {
+
+}
---
+++ 2019/src/test/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15Test.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day15;
+
+import static org.junit.Assert.*;
+
+
+public class AdventOfCodeDay15Test {
+
+}
