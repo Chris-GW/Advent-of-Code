b5b94783306a692e4f18b3e8cd48adb768a304eb
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-19 08:39:42 +0100

    2019 Day 19: Tractor Beam - part01

--- 2019/src/test/java/de/adventofcode/chrisgw/day19/AdventOfCodeDay19Test.java
+++ 2019/src/test/java/de/adventofcode/chrisgw/day19/AdventOfCodeDay19Test.java
@@ -1,8 +1,24 @@
 package de.adventofcode.chrisgw.day19;
 
+import de.adventofcode.chrisgw.TestUtils;
+import de.adventofcode.chrisgw.intcode.IntCodeProgram;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
 public class AdventOfCodeDay19Test {
+
+    @Test
+    public void myPuzzleInput_part01_countAffectedCointsForGrid() {
+        String intCodeProgramStr = TestUtils.readSingleLineOfClassPathResource("/puzzleInputDay19.txt");
+        IntCodeProgram droneProgram = IntCodeProgram.parseIntCodeProgram(intCodeProgramStr);
+        int expectedAffectedPoints = 234;
+
+        AdventOfCodeDay19 aocDay19 = new AdventOfCodeDay19(droneProgram);
+        long affectedPoints = aocDay19.countAffectedCointsForGrid(50);
+        System.out.println(aocDay19);
+        assertEquals("affectedPoints", expectedAffectedPoints, affectedPoints);
+    }
 
 }
