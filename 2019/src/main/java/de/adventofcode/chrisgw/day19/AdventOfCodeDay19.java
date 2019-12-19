395abf2336665a0f52f100e61bad96a089dca36e
Chris-GW <Chris-GW@users.noreply.github.com>
2019-12-19 14:42:13 +0100

    2019 Day 19: Tractor Beam - part02 some more

--- 2019/src/main/java/de/adventofcode/chrisgw/day19/AdventOfCodeDay19.java
+++ 2019/src/main/java/de/adventofcode/chrisgw/day19/AdventOfCodeDay19.java
@@ -1,10 +1,11 @@
 package de.adventofcode.chrisgw.day19;
 
 import de.adventofcode.chrisgw.intcode.IntCodeProgram;
+import org.apache.commons.math3.geometry.euclidean.twod.Line;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 
 /**
@@ -14,7 +15,14 @@ import java.util.Map;
 public class AdventOfCodeDay19 {
 
     private final IntCodeProgram droneProgram;
+    private int gridSize;
     private Map<Vector2D, Boolean> affectedPoints;
+
+    private Line leftTractorBeam;
+    private Line rightTractorBeam;
+    private Map<Long, AffectedBeamRow> affectedRows = new HashMap<>();
+    private AffectedBeamRow uperShipRow;
+    private AffectedBeamRow lowerShipRow;
 
 
     public AdventOfCodeDay19(IntCodeProgram droneProgram) {
@@ -22,8 +30,9 @@ public class AdventOfCodeDay19 {
     }
 
 
-    public long countAffectedCointsForGrid(int size) {
+    public long countAffectedPointsForGrid(int size) {
         this.affectedPoints = new HashMap<>(size * size);
+        this.gridSize = size;
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
                 Vector2D point = new Vector2D(x, y);
@@ -32,6 +41,108 @@ public class AdventOfCodeDay19 {
             }
         }
         return affectedPoints.values().stream().filter(Boolean.TRUE::equals).count();
+    }
+
+    private boolean isAffected(Vector2D point) {
+        long row = Math.round(point.getY());
+        long x = Math.round(point.getX());
+        if (row < gridSize && x < gridSize) {
+            return affectedPoints.get(point);
+        } else {
+            return false;
+        }
+    }
+
+
+    public long findShip(int shipSize) {
+        int gridSize = 20;
+        countAffectedPointsForGrid(gridSize);
+        lowerShipRow = findLastAffectedBeamRow(gridSize);
+        putAffectedRow(lowerShipRow);
+
+        for (long row = lowerShipRow.getRow() + 1; true; row++) {
+            lowerShipRow = scanAffectedBeamRow(row);
+            uperShipRow = affectedRows.get(lowerShipRow.getRow() - shipSize);
+            if (fitsShip(shipSize, lowerShipRow, uperShipRow)) {
+                break;
+            }
+        }
+        long x = lowerShipRow.getLeft();
+        long y = uperShipRow.getRow();
+        return x * 10000 + y;
+    }
+
+    private AffectedBeamRow findLastAffectedBeamRow(int gridSize) {
+        Vector2D startTopRight = new Vector2D(gridSize - 2, 0);
+        Vector2D stepDown = new Vector2D(0, 1);
+        Vector2D lastRight = Stream.iterate(startTopRight, vector2D -> vector2D.add(stepDown))
+                .filter(this::isAffected)
+                .findFirst()
+                .orElseThrow();
+
+        Vector2D stepToLeft = new Vector2D(-1, 0);
+        Vector2D lastLeft = Stream.iterate(lastRight, this::isAffected, vector2D -> vector2D.add(stepToLeft))
+                .min(Comparator.comparingDouble(Vector2D::getX))
+                .orElseThrow();
+        return new AffectedBeamRow(lastLeft, lastRight);
+    }
+
+
+    private boolean fitsShip(int shipSize, AffectedBeamRow lowerRow, AffectedBeamRow upperRow) {
+        if (upperRow == null) {
+            return false;
+        }
+        long upperRight = upperRow.getRight();
+        long lowerLeft = lowerRow.getLeft();
+        return upperRight - lowerLeft >= shipSize - 1;
+    }
+
+
+    private AffectedBeamRow scanAffectedBeamRow(long row) {
+        Line horizontalLine = new Line(new Vector2D(0, row), new Vector2D(1, row), 1.0e-6);
+        Vector2D leftIntersection = findOuterCorner(leftTractorBeam.intersection(horizontalLine));
+        Vector2D rightIntersection = findOuterCorner(rightTractorBeam.intersection(horizontalLine));
+        AffectedBeamRow affectedBeamRow = new AffectedBeamRow(leftIntersection, rightIntersection);
+        putAffectedRow(affectedBeamRow);
+        return affectedBeamRow;
+    }
+
+    private Vector2D findOuterCorner(Vector2D point) {
+        Vector2D leftStep = new Vector2D(-1, 0);
+        Vector2D rightStep = new Vector2D(1, 0);
+
+        Iterator<Vector2D> leftStepIterator = Stream.iterate(point, leftStep::add).skip(1).iterator();
+        Iterator<Vector2D> rightStepIterator = Stream.iterate(point, rightStep::add).skip(1).iterator();
+
+        Deque<Boolean> affectedResults = new ArrayDeque<>();
+        affectedResults.add(testAffactionFor(point));
+        while (true) {
+            Vector2D left = leftStepIterator.next();
+            boolean isLeftAffacted = testAffactionFor(left);
+            Vector2D right = rightStepIterator.next();
+            boolean isRightAffacted = testAffactionFor(right);
+
+            if (isLeftAffacted && !affectedResults.peekFirst()) {
+                return left;
+            } else if (!isLeftAffacted && affectedResults.peekFirst()) {
+                return left.subtract(leftStep);
+
+            } else if (isRightAffacted && !affectedResults.peekLast()) {
+                return right;
+            } else if (!isRightAffacted && affectedResults.peekLast()) {
+                return right.subtract(rightStep);
+            }
+            affectedResults.addFirst(isLeftAffacted);
+            affectedResults.addLast(isRightAffacted);
+        }
+    }
+
+
+    private void putAffectedRow(AffectedBeamRow lastAffectedBeamRow) {
+        long row = lastAffectedBeamRow.getRow();
+        affectedRows.put(row, lastAffectedBeamRow);
+        leftTractorBeam = lastAffectedBeamRow.leftTractorBeam();
+        rightTractorBeam = lastAffectedBeamRow.rightTractorBeam();
     }
 
 
@@ -46,18 +157,22 @@ public class AdventOfCodeDay19 {
     }
 
 
-    private boolean isAffected(Vector2D point) {
-        return affectedPoints.getOrDefault(point, false);
-    }
-
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int y = 0; y < 50; y++) {
-            for (int x = 0; x < 50; x++) {
+        long rows = affectedRows.values().stream().mapToLong(AffectedBeamRow::getRow).max().orElse(20) + 1;
+        long columns = affectedRows.values().stream().mapToLong(AffectedBeamRow::getRight).max().orElse(20) + 1;
+        for (int y = 0; y <= rows; y++) {
+            AffectedBeamRow affectedBeamRow = affectedRows.get(Long.valueOf(y));
+            for (int x = 0; x <= columns; x++) {
                 Vector2D point = new Vector2D(x, y);
-                if (isAffected(point)) {
+                if (isShipPoint(point)) {
+                    sb.append("O");
+                } else if (affectedBeamRow != null && affectedBeamRow.isAffected(point)) {
+                    sb.append("X");
+                } else if (affectedBeamRow != null && affectedBeamRow.isAffected(point) && !isAffected(point)) {
+                    sb.append("x");
+                } else if (isAffected(point)) {
                     sb.append("#");
                 } else {
                     sb.append(".");
@@ -66,6 +181,13 @@ public class AdventOfCodeDay19 {
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    private boolean isShipPoint(Vector2D point) {
+        long x = Math.round(point.getX());
+        long y = Math.round(point.getY());
+        boolean isValidX = lowerShipRow.getLeft() <= x && x <= uperShipRow.getRight();
+        return isValidX && uperShipRow.getRow() <= y && y <= lowerShipRow.getRow();
     }
 
 }
