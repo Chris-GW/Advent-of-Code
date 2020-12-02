123c5c2f3ec4eb2d318714d36093dcd2004a5a80
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-02 11:39:31 +0100

    2020 Day 01: Report Repair - part01

--- 2020/src/main/java/chrisgw/day01/AdventOfCodeDay01.java
+++ 2020/src/main/java/chrisgw/day01/AdventOfCodeDay01.java
@@ -1,8 +1,31 @@
 package chrisgw.day01;
 
+
+import org.apache.commons.math3.util.Pair;
+
+import java.util.List;
+
+
 /**
  * https://adventofcode.com/2020/day/1
  */
 public class AdventOfCodeDay01 {
+
+    
+    public static Pair<Integer, Integer> findSum2020Entries(List<Integer> expenseReport) {
+        int requestedSum = 2020;
+
+        for (int i = 0; i < expenseReport.size(); i++) {
+            int firstExpenseReportEntry = expenseReport.get(i);
+
+            for (int j = i + 1; j < expenseReport.size(); j++) {
+                int secondExpenseReportEntry = expenseReport.get(j);
+                if (firstExpenseReportEntry + secondExpenseReportEntry == requestedSum) {
+                    return Pair.create(firstExpenseReportEntry, secondExpenseReportEntry);
+                }
+            }
+        }
+        throw new IllegalArgumentException("Expense Report doesn't contain any sum for " + requestedSum);
+    }
 
 }
--- 2020/src/test/java/chrisgw/day01/AdventOfCodeDay01Test.java
+++ 2020/src/test/java/chrisgw/day01/AdventOfCodeDay01Test.java
@@ -1,20 +1,51 @@
 package chrisgw.day01;
 
+import chrisgw.TestUtils;
+import org.apache.commons.math3.util.Pair;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static chrisgw.day01.AdventOfCodeDay01.findSum2020Entries;
+import static org.junit.Assert.*;
 
 
 public class AdventOfCodeDay01Test {
 
+    private final List<Integer> exampleExpenseReportLines = List.of( //
+            1721, //
+            979, //
+            366, //
+            299, //
+            675, //
+            1456);
+
 
     @Test
-    public void calculateModuleFuelRequirement_part01() {
-
+    public void findSum2020Entries_example() {
+        int expectedPairProduct = 1721 * 299;
+        Pair<Integer, Integer> sum2020Entries = findSum2020Entries(exampleExpenseReportLines);
+        assertEquals("entriesProduct", expectedPairProduct, calculatePairProduct(sum2020Entries));
     }
 
 
     @Test
-    public void calculateCorrectFuelRequirement_part02() {
+    public void findSum2020Entries_myPuzzleInput() {
+        int expectedPairProduct = 1165 * 855; // = 996075
+        List<Integer> expenseReportLines = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay01.txt")
+                .stream()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        Pair<Integer, Integer> sum2020Entries = findSum2020Entries(expenseReportLines);
+        int pairProduct = calculatePairProduct(sum2020Entries);
+        assertEquals("entriesProduct", expectedPairProduct, pairProduct);
+    }
 
+
+    private int calculatePairProduct(Pair<Integer, Integer> sum2020Entries) {
+        return sum2020Entries.getFirst() * sum2020Entries.getSecond();
     }
 
 }
