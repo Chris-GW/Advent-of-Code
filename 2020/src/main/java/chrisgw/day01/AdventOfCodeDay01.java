5db1b481f9bd80f0b3000ed1020640fa500de16f
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-02 11:50:02 +0100

    2020 Day 01: Report Repair - part02

--- 2020/src/main/java/chrisgw/day01/AdventOfCodeDay01.java
+++ 2020/src/main/java/chrisgw/day01/AdventOfCodeDay01.java
@@ -1,7 +1,10 @@
 package chrisgw.day01;
 
 
-import org.apache.commons.math3.util.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.List;
 
@@ -11,8 +14,11 @@ import java.util.List;
  */
 public class AdventOfCodeDay01 {
 
-    
-    public static Pair<Integer, Integer> findSum2020Entries(List<Integer> expenseReport) {
+    private AdventOfCodeDay01() {
+    }
+
+
+    public static Pair<Integer, Integer> findSum2020PairEntries(List<Integer> expenseReport) {
         int requestedSum = 2020;
 
         for (int i = 0; i < expenseReport.size(); i++) {
@@ -21,7 +27,30 @@ public class AdventOfCodeDay01 {
             for (int j = i + 1; j < expenseReport.size(); j++) {
                 int secondExpenseReportEntry = expenseReport.get(j);
                 if (firstExpenseReportEntry + secondExpenseReportEntry == requestedSum) {
-                    return Pair.create(firstExpenseReportEntry, secondExpenseReportEntry);
+                    return new ImmutablePair<>(firstExpenseReportEntry, secondExpenseReportEntry);
+                }
+            }
+        }
+        throw new IllegalArgumentException("Expense Report doesn't contain any sum for " + requestedSum);
+    }
+
+
+    public static Triple<Integer, Integer, Integer> findSum2020TripleEntries(List<Integer> expenseReport) {
+        int requestedSum = 2020;
+
+        for (int i = 0; i < expenseReport.size(); i++) {
+            int firstEntry = expenseReport.get(i);
+
+            for (int j = i + 1; j < expenseReport.size(); j++) {
+                int secondEntry = expenseReport.get(j);
+
+                for (int k = j + 1; k < expenseReport.size(); k++) {
+                    int thirdEntry = expenseReport.get(k);
+
+                    int tripletEntrySum = firstEntry + secondEntry + thirdEntry;
+                    if (tripletEntrySum == requestedSum) {
+                        return new ImmutableTriple<>(firstEntry, secondEntry, thirdEntry);
+                    }
                 }
             }
         }
--- 2020/src/test/java/chrisgw/day01/AdventOfCodeDay01Test.java
+++ 2020/src/test/java/chrisgw/day01/AdventOfCodeDay01Test.java
@@ -1,13 +1,16 @@
 package chrisgw.day01;
 
 import chrisgw.TestUtils;
-import org.apache.commons.math3.util.Pair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static chrisgw.day01.AdventOfCodeDay01.findSum2020Entries;
+import static chrisgw.TestUtils.readAllLinesOfClassPathResource;
+import static chrisgw.day01.AdventOfCodeDay01.findSum2020PairEntries;
+import static chrisgw.day01.AdventOfCodeDay01.findSum2020TripleEntries;
 import static org.junit.Assert.*;
 
 
@@ -23,29 +26,53 @@ public class AdventOfCodeDay01Test {
 
 
     @Test
-    public void findSum2020Entries_example() {
-        int expectedPairProduct = 1721 * 299;
-        Pair<Integer, Integer> sum2020Entries = findSum2020Entries(exampleExpenseReportLines);
-        assertEquals("entriesProduct", expectedPairProduct, calculatePairProduct(sum2020Entries));
+    public void findSum2020PairEntries_example() {
+        Pair<Integer, Integer> sum2020Pair = findSum2020PairEntries(exampleExpenseReportLines);
+        int pairProduct = calculatePairProduct(sum2020Pair);
+        assertEquals("entriesProduct", 1721 * 299, pairProduct);
     }
 
 
     @Test
-    public void findSum2020Entries_myPuzzleInput() {
-        int expectedPairProduct = 1165 * 855; // = 996075
-        List<Integer> expenseReportLines = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay01.txt")
-                .stream()
+    public void findSum2020PairEntries_myPuzzleInput() {
+        List<Integer> expenseReportLines = readAllLinesOfClassPathResource("/puzzleInputDay01.txt").stream()
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
-        Pair<Integer, Integer> sum2020Entries = findSum2020Entries(expenseReportLines);
-        int pairProduct = calculatePairProduct(sum2020Entries);
-        assertEquals("entriesProduct", expectedPairProduct, pairProduct);
+        Pair<Integer, Integer> sum2020Pair = findSum2020PairEntries(expenseReportLines);
+        int pairProduct = calculatePairProduct(sum2020Pair);
+        assertEquals("entriesProduct", 1165 * 855, pairProduct);
     }
 
 
-    private int calculatePairProduct(Pair<Integer, Integer> sum2020Entries) {
-        return sum2020Entries.getFirst() * sum2020Entries.getSecond();
+    private int calculatePairProduct(Pair<Integer, Integer> pair) {
+        return pair.getLeft() * pair.getRight();
+    }
+
+
+    // part 02
+
+
+    @Test
+    public void findSum2020TripleEntries_example() {
+        Triple<Integer, Integer, Integer> sum2020Entries = findSum2020TripleEntries(exampleExpenseReportLines);
+        assertEquals("entriesProduct", 979 * 366 * 675, calculateTripleProduct(sum2020Entries));
+    }
+
+
+    @Test
+    public void findSum2020TripleEntries_myPuzzleInput() {
+        List<Integer> expenseReportLines = readAllLinesOfClassPathResource("/puzzleInputDay01.txt").stream()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        Triple<Integer, Integer, Integer> sum2020Triple = findSum2020TripleEntries(expenseReportLines);
+        assertEquals("entriesProduct", 168 * 185 * 1667, calculateTripleProduct(sum2020Triple));
+    }
+
+
+    private int calculateTripleProduct(Triple<Integer, Integer, Integer> triple) {
+        return triple.getLeft() * triple.getMiddle() * triple.getRight();
     }
 
 }
