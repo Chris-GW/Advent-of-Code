22380b8d7ff0da0039fbb074dd1e06911c652e3b
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-02 11:12:52 +0100

    2020 Day 02: Password Philosophy - part02

--- 2020/src/main/java/chrisgw/day02/AdventOfCodeDay02.java
+++ 2020/src/main/java/chrisgw/day02/AdventOfCodeDay02.java
@@ -1,10 +1,7 @@
 package chrisgw.day02;
 
-import lombok.Data;
-
 import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 /**
@@ -13,68 +10,35 @@ import java.util.regex.Pattern;
 public class AdventOfCodeDay02 {
 
 
-    public static long countValidPasswords(List<String> passwords) {
+    private AdventOfCodeDay02() {
+    }
+
+
+    public static long countValidPasswordsWithSledRentalPlacePasswordPolicy(List<String> passwords) {
         return passwords.stream()
-                .map(PasswordWithPolicy::parsePasswordWithPolicy)
-                .filter(PasswordWithPolicy::matchesPolicy)
+                .map(PasswordPolicy.PASSWORD_WITH_POLICY_PATTERN::matcher)
+                .filter(Matcher::matches)
+                .map(Matcher::toMatchResult)
+                .filter(matchResult -> {
+                    PasswordPolicy passwordPolicy = SledRentalPlacePasswordPolicy.fromMatchResult(matchResult);
+                    String password = matchResult.group(4);
+                    return passwordPolicy.matchesPolicy(password);
+                })
                 .count();
     }
 
-
-    @Data
-    public static class PasswordWithPolicy {
-
-        public static final Pattern PASSWORD_WITH_POLICY_PATTERN = Pattern.compile("(\\d+)-(\\d+) (\\w): (\\w+)");
-
-        private final PasswordPolicy passwordPolicy;
-        private final String password;
-
-
-        public static PasswordWithPolicy parsePasswordWithPolicy(String passwordWithPolicy) {
-            Matcher matcher = PASSWORD_WITH_POLICY_PATTERN.matcher(passwordWithPolicy);
-            if (!matcher.matches()) {
-                throw new IllegalArgumentException(
-                        "Expect passwordWithPolicy matching: " + PASSWORD_WITH_POLICY_PATTERN);
-            }
-            int lowerLimit = Integer.parseInt(matcher.group(1));
-            int upperLimit = Integer.parseInt(matcher.group(2));
-            char letter = matcher.group(3).charAt(0);
-            String password = matcher.group(4);
-            PasswordPolicy passwordPolicy = new PasswordPolicy(lowerLimit, upperLimit, letter);
-            return new PasswordWithPolicy(passwordPolicy, password);
-        }
-
-
-        public boolean matchesPolicy() {
-            return passwordPolicy.matchesPolicy(password);
-        }
-
+    public static long countValidPasswordsWithTobogganCorporatePasswordPolicy(List<String> passwords) {
+        return passwords.stream()
+                .map(PasswordPolicy.PASSWORD_WITH_POLICY_PATTERN::matcher)
+                .filter(Matcher::matches)
+                .map(Matcher::toMatchResult)
+                .filter(matchResult -> {
+                    PasswordPolicy passwordPolicy = TobogganCorporatePasswordPolicy.fromMatchResult(matchResult);
+                    String password = matchResult.group(4);
+                    return passwordPolicy.matchesPolicy(password);
+                })
+                .count();
     }
 
-
-    @Data
-    public static class PasswordPolicy {
-
-        private final int lowerLimit;
-        private final int upperLimit;
-        private final char letter;
-
-
-        public boolean matchesPolicy(String password) {
-            int letterOccurrence = countLetterOccurrence(password);
-            return lowerLimit <= letterOccurrence && letterOccurrence <= upperLimit;
-        }
-
-        private int countLetterOccurrence(String password) {
-            int count = 0;
-            for (int i = 0; i < password.length(); i++) {
-                if (password.charAt(i) == letter) {
-                    count++;
-                }
-            }
-            return count;
-        }
-
-    }
 
 }
