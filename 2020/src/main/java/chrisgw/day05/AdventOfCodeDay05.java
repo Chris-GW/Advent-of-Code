7c999d43e09de357e5ec155d151deb3b010a5ee9
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-05 16:34:27 +0100

    2020 Day 05: Binary Boarding - start

---
+++ 2020/src/main/java/chrisgw/day05/AdventOfCodeDay05.java
@@ -0,0 +1,8 @@
+package chrisgw.day05;
+
+/**
+ * https://adventofcode.com/2020/day/5
+ */
+public class AdventOfCodeDay05 {
+
+}
---
+++ 2020/src/test/java/chrisgw/day05/AdventOfCodeDay05Test.java
@@ -0,0 +1,15 @@
+package chrisgw.day05;
+
+import chrisgw.TestUtils;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+
+public class AdventOfCodeDay05Test {
+
+
+}
