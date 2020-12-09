8c49d5e2214c96c289ef217868d28af76178a1a4
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-09 21:17:22 +0100

    2020 Day 09: Encoding Error - part 01 and 02

--- 2020/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
+++ 2020/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
@@ -1,8 +1,67 @@
 package de.adventofcode.chrisgw.day09;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
 /**
  * https://adventofcode.com/2020/day/9
  */
 public class AdventOfCodeDay09 {
+
+    private final int preambleLength;
+    private final List<Integer> numbers;
+
+
+    public AdventOfCodeDay09(int preambleLength) {
+        this.preambleLength = preambleLength;
+        this.numbers = new ArrayList<>(preambleLength);
+    }
+
+
+    public boolean readNext(int number) {
+        if (!isValidNumber(number)) {
+            return false;
+        }
+        numbers.add(number);
+        return true;
+    }
+
+
+    private boolean isValidNumber(int number) {
+        return numbers.size() < preambleLength //
+                || preambleNumbers().anyMatch(firstNumber -> containsPreambleSumPair(number, firstNumber));
+    }
+
+    private Stream<Integer> preambleNumbers() {
+        int fromIndex = numbers.size() - preambleLength;
+        return numbers.subList(fromIndex, numbers.size()).stream();
+    }
+
+    private boolean containsPreambleSumPair(int sum, int firstNumber) {
+        int secondNumber = sum - firstNumber;
+        return firstNumber != secondNumber && preambleNumbers().anyMatch(n -> n == secondNumber);
+    }
+
+
+    // part 02
+
+    public int findContigousListForInvalidNumber(int firstInvalidNumber) {
+        for (int fromIndex = 0; fromIndex < numbers.size() - 1; fromIndex++) {
+            for (int toIndex = fromIndex + 2; toIndex < numbers.size() - 1; toIndex++) {
+                List<Integer> listForInvalidNumber = numbers.subList(fromIndex, toIndex);
+                int sum = listForInvalidNumber.stream().reduce(0, Integer::sum);
+                if (sum == firstInvalidNumber) {
+                    int min = listForInvalidNumber.stream().min(Integer::compareTo).orElseThrow();
+                    int max = listForInvalidNumber.stream().max(Integer::compareTo).orElseThrow();
+                    return min + max;
+                } else if (sum > firstInvalidNumber) {
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
 
 }
--- 2020/src/test/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09Test.java
+++ 2020/src/test/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09Test.java
@@ -1,5 +1,119 @@
 package de.adventofcode.chrisgw.day09;
 
+import de.adventofcode.chrisgw.TestUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+
 public class AdventOfCodeDay09Test {
+
+    private List<Integer> exampleNumbers = List.of( //
+            35, //
+            20, //
+            15, //
+            25, //
+            47, //
+            40, //
+            62, //
+            55, //
+            65, //
+            95, //
+            102, //
+            117, //
+            150, //
+            182, //
+            127, //
+            219, //
+            299, //
+            277, //
+            309, //
+            576);
+
+    @Test
+    public void findFirstNumberWhichDoesntMatchPrembleSumPair_example() {
+        int expectedInvalidNumber = 127;
+        int preambleLength = 5;
+        AdventOfCodeDay09 aoc09 = new AdventOfCodeDay09(preambleLength);
+        for (int number : exampleNumbers) {
+            boolean accepted = aoc09.readNext(number);
+            if (!accepted) {
+                assertEquals("first invalid number", expectedInvalidNumber, number);
+                return;
+            }
+        }
+        fail("no invalid number found");
+    }
+
+    @Test
+    public void findFirstNumberWhichDoesntMatchPrembleSumPair_myPuzzleInput() {
+        int expectedInvalidNumber = 542529149;
+        int preambleLength = 25;
+        Iterator<Integer> inputNumbers = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay09.txt")
+                .stream()
+                .map(Integer::parseInt)
+                .iterator();
+        AdventOfCodeDay09 aoc09 = new AdventOfCodeDay09(preambleLength);
+        while (inputNumbers.hasNext()) {
+            int number = inputNumbers.next();
+            boolean accepted = aoc09.readNext(number);
+            if (!accepted) {
+                assertEquals("first invalid number", expectedInvalidNumber, number);
+                return;
+            }
+        }
+        fail("no invalid number found");
+    }
+
+
+    // part 02
+
+    @Test
+    public void findContigousListForInvalidNumber_example() {
+        int expectedInvalidNumber = 127;
+        int expectedMinMaxSum = 15 + 47;
+        assertEquals("expectedMinMaxSum", 62, expectedMinMaxSum);
+
+        int preambleLength = 5;
+        AdventOfCodeDay09 aoc09 = new AdventOfCodeDay09(preambleLength);
+        for (int number : exampleNumbers) {
+            boolean accepted = aoc09.readNext(number);
+            if (!accepted) {
+                assertEquals("first invalid number", expectedInvalidNumber, number);
+                int minMaxSum = aoc09.findContigousListForInvalidNumber(number);
+                assertEquals("findContigousListForInvalidNumber", expectedMinMaxSum, minMaxSum);
+                return;
+            }
+        }
+        fail("no invalid number found");
+    }
+
+    @Test
+    public void findContigousListForInvalidNumber_myPuzzleInput() {
+        int expectedInvalidNumber = 542529149;
+        int expectedMinMaxSum = 75678618;
+        int preambleLength = 25;
+        Iterator<Integer> inputNumbers = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay09.txt")
+                .stream()
+                .map(Integer::parseInt)
+                .iterator();
+        AdventOfCodeDay09 aoc09 = new AdventOfCodeDay09(preambleLength);
+        while (inputNumbers.hasNext()) {
+            int number = inputNumbers.next();
+            boolean accepted = aoc09.readNext(number);
+            if (!accepted) {
+                assertEquals("first invalid number", expectedInvalidNumber, number);
+                int minMaxSum = aoc09.findContigousListForInvalidNumber(number);
+                assertEquals("findContigousListForInvalidNumber", expectedMinMaxSum, minMaxSum);
+                return;
+            }
+        }
+        fail("no invalid number found");
+    }
 
 }
