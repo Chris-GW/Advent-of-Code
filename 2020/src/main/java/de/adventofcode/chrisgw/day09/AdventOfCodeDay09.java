d6c214faca9792664277b6dd37a6c69430494fbc
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-09 20:17:20 +0100

    2020 Day 09: Encoding Error - start

---
+++ 2020/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
@@ -0,0 +1,8 @@
+package de.adventofcode.chrisgw.day09;
+
+/**
+ * https://adventofcode.com/2020/day/9
+ */
+public class AdventOfCodeDay09 {
+
+}
---
+++ 2020/src/test/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09Test.java
@@ -0,0 +1,5 @@
+package de.adventofcode.chrisgw.day09;
+
+public class AdventOfCodeDay09Test {
+
+}
