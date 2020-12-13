2fb58b393a2c3fcb7262221d7fe0a1f0a5c1cf55
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-13 12:43:02 +0100

    2020 Day 13: Shuttle Search - start

---
+++ 2020/src/main/java/de/adventofcode/chrisgw/day13/AdventOfCodeDay13.java
@@ -0,0 +1,31 @@
+package de.adventofcode.chrisgw.day13;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzle;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * https://adventofcode.com/2020/day/13
+ */
+public class AdventOfCodeDay13 extends AdventOfCodePuzzle {
+
+
+    public AdventOfCodeDay13(List<String> inputLines) {
+        super(Year.of(2020), 13, inputLines);
+    }
+
+
+    @Override
+    public Number solveFirstPart() {
+        return 0;
+    }
+
+
+    @Override
+    public Number solveSecondPart() {
+        return 0;
+    }
+
+}
