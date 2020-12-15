207fb53b890eafcde7236616125daf562ae4211f
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-15 16:25:28 +0100

    2020 Day 14: Docking Data - start

---
+++ 2020/src/main/java/de/adventofcode/chrisgw/day14/AdventOfCodeDay14.java
@@ -0,0 +1,34 @@
+package de.adventofcode.chrisgw.day14;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzle;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * https://adventofcode.com/2020/day/14
+ */
+public class AdventOfCodeDay14 extends AdventOfCodePuzzle {
+
+    public AdventOfCodeDay14(List<String> inputLines) {
+        super(Year.of(2020), 14, inputLines);
+    }
+
+
+    @Override
+    public Number solveFirstPart() {
+        // TODO solveFirstPart()
+        return 0;
+    }
+
+
+    // part 02
+
+    @Override
+    public Number solveSecondPart() {
+        // TODO solveSecondPart()
+        return 0;
+    }
+
+}
---
+++ 2020/src/test/java/de/adventofcode/chrisgw/day14/AdventOfCodeDay14Test.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day14;
+
+import org.junit.Test;
+
+
+public class AdventOfCodeDay14Test {
+
+
+    @Test
+    public void solveFirstPart_example() {
+        // TODO solveFirstPart_example
+    }
+
+    @Test
+    public void solveFirstPart_myPuzzleInput() {
+        // TODO solveFirstPart_myPuzzleInput
+    }
+
+
+    // part 02
+
+    @Test
+    public void solveSecondPart_example() {
+        // TODO solveSecondPart_example
+    }
+
+    @Test
+    public void solveSecondPart_myPuzzleInput() {
+        // TODO solveSecondPart_myPuzzleInput
+    }
+
+}
