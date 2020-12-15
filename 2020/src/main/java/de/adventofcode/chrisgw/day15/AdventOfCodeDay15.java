23005df28beb3df45c6d7dd1e946b6b0d7ac4fe8
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-15 18:29:38 +0100

    2020 Day 15: Rambunctious Recitation - part01

--- 2020/src/main/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15.java
+++ 2020/src/main/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15.java
@@ -3,7 +3,9 @@ package de.adventofcode.chrisgw.day15;
 import de.adventofcode.chrisgw.AdventOfCodePuzzle;
 
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -11,14 +13,36 @@ import java.util.List;
  */
 public class AdventOfCodeDay15 extends AdventOfCodePuzzle {
 
+    private List<Integer> spokenNumbers;
 
-    public AdventOfCodeDay15(List<String> inputLines) {
-        super(Year.of(2020), 15, inputLines);
+    public AdventOfCodeDay15(List<Integer> startingNumbers) {
+        super(Year.of(2020), 15,
+                List.of(startingNumbers.stream().map(String::valueOf).collect(Collectors.joining(","))));
+        spokenNumbers = new ArrayList<>(startingNumbers);
     }
 
 
     @Override
-    public Number solveFirstPart() {
+    public Integer solveFirstPart() {
+        while (!spokenNumbers.isEmpty() && spokenNumbers.size() < 2020) {
+            int lastNumber = getLastNumber();
+            int lastSpokenNumber = findSpokenNumber(lastNumber);
+            spokenNumbers.add(lastSpokenNumber);
+        }
+        return getLastNumber();
+    }
+
+    private int getLastNumber() {
+        return spokenNumbers.get(spokenNumbers.size() - 1);
+    }
+
+    private int findSpokenNumber(int lastNumber) {
+        for (int i = spokenNumbers.size() - 2; i >= 0; i--) {
+            int number = spokenNumbers.get(i);
+            if (number == lastNumber) {
+                return spokenNumbers.size() - i - 1;
+            }
+        }
         return 0;
     }
 
--- 2020/src/test/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15Test.java
+++ 2020/src/test/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15Test.java
@@ -1,17 +1,85 @@
 package de.adventofcode.chrisgw.day15;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 
 public class AdventOfCodeDay15Test {
 
 
     @Test
-    public void solveFirstPart_example() {
+    public void solveFirstPart_example01() {
+        List<Integer> startingNumbers = List.of(0, 3, 6);
+        int expectedSolution = 436;
+
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
     }
 
     @Test
+    public void solveFirstPart_example02() {
+        List<Integer> startingNumbers = List.of(1, 3, 2);
+        int expectedSolution = 1;
+
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
+    }
+
+    @Test
+    public void solveFirstPart_example03() {
+        List<Integer> startingNumbers = List.of(2, 1, 3);
+        int expectedSolution = 10;
+
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
+    }
+
+    @Test
+    public void solveFirstPart_example04() {
+        List<Integer> startingNumbers = List.of(2, 3, 1);
+        int expectedSolution = 78;
+
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
+    }
+
+    @Test
+    public void solveFirstPart_example05() {
+        List<Integer> startingNumbers = List.of(3, 2, 1);
+        int expectedSolution = 438;
+
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
+    }
+
+    @Test
+    public void solveFirstPart_example06() {
+        List<Integer> startingNumbers = List.of(3, 1, 2);
+        int expectedSolution = 1836;
+
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
+    }
+
+
+    @Test
     public void solveFirstPart_myPuzzleInput() {
+        int expectedSolution = 1194;
+
+        List<Integer> startingNumbers = List.of(6, 13, 1, 15, 2, 0);
+        AdventOfCodeDay15 aoc15 = new AdventOfCodeDay15(startingNumbers);
+        int solution = aoc15.solveFirstPart();
+        assertEquals("solveFirstPart", expectedSolution, solution);
     }
 
 
