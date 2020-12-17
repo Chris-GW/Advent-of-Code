997724d80d17dfde81cf9ce7de1df7fe62f6be8a
Chris-GW <Chris-GW@users.noreply.github.com>
2020-12-17 22:09:29 +0100

    2020 Day 16: Ticket Translation - start

---
+++ 2020/src/main/java/de/adventofcode/chrisgw/day16/AdventOfCodeDay16.java
@@ -0,0 +1,30 @@
+package de.adventofcode.chrisgw.day16;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzle;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * https://adventofcode.com/2020/day/16
+ */
+public class AdventOfCodeDay16 extends AdventOfCodePuzzle {
+
+
+    public AdventOfCodeDay16(Year year, int day, List<String> inputLines) {
+        super(year, day, inputLines);
+    }
+
+
+    @Override
+    public Object solveFirstPart() {
+        return null;
+    }
+
+    @Override
+    public Object solveSecondPart() {
+        return null;
+    }
+
+}
