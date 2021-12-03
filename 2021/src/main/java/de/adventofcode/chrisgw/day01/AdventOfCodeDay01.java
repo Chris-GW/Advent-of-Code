1ca87998363d457ee25fb2f293c27d7025cf7872
Chris-GW <Chris-GW@users.noreply.github.com>
2021-12-03 12:55:04 +0100

    2021 Day 1: Sonar Sweep - part 2

--- 2021/src/main/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01.java
+++ 2021/src/main/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01.java
@@ -23,4 +23,18 @@ public class AdventOfCodeDay01 {
         return increaseCounter;
     }
 
+
+    public static int calculateMeasurementIncreasesWithWindow(int[] measurements) {
+        int increaseCounter = 0;
+        int previousWindowValue = Integer.MAX_VALUE;
+        for (int i = 2; i < measurements.length; i++) {
+            int currentWindowValue = measurements[i - 2] + measurements[i - 1] + measurements[i];
+            if (previousWindowValue < currentWindowValue) {
+                increaseCounter++;
+            }
+            previousWindowValue = currentWindowValue;
+        }
+        return increaseCounter;
+    }
+
 }
--- 2021/src/test/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01Test.java
+++ 2021/src/test/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01Test.java
@@ -12,7 +12,7 @@ import static org.hamcrest.Matchers.*;
 
 public class AdventOfCodeDay01Test {
 
-    private static int[] exampleMeasurements = new int[] { //
+    private static final int[] exampleMeasurements = new int[] { //
             199, //
             200, //
             208, //
@@ -24,20 +24,36 @@ public class AdventOfCodeDay01Test {
             260, //
             263 };
 
+
     @Test
     public void part1_example() {
         int[] measurements = Arrays.copyOf(exampleMeasurements, exampleMeasurements.length);
         int result = AdventOfCodeDay01.calculateMeasurementIncreases(measurements);
-        assertThat(result, is(7));
+        assertThat("calculateMeasurementIncreases", result, is(7));
     }
-
 
     @Test
     public void part1_myPuzzle() {
         List<String> measurementLines = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay01.txt");
         int[] measurements = measurementLines.stream().mapToInt(Integer::parseInt).toArray();
         int result = AdventOfCodeDay01.calculateMeasurementIncreases(measurements);
-        assertThat(result, is(1292));
+        assertThat("calculateMeasurementIncreases", result, is(1292));
+    }
+
+
+    @Test
+    public void part2_example() {
+        int[] measurements = Arrays.copyOf(exampleMeasurements, exampleMeasurements.length);
+        int result = AdventOfCodeDay01.calculateMeasurementIncreasesWithWindow(measurements);
+        assertThat("calculateMeasurementIncreasesWithWindow", result, is(5));
+    }
+
+    @Test
+    public void part2_myPuzzle() {
+        List<String> measurementLines = TestUtils.readAllLinesOfClassPathResource("/puzzleInputDay01.txt");
+        int[] measurements = measurementLines.stream().mapToInt(Integer::parseInt).toArray();
+        int result = AdventOfCodeDay01.calculateMeasurementIncreasesWithWindow(measurements);
+        assertThat("calculateMeasurementIncreasesWithWindow", result, is(1262));
     }
 
 }
