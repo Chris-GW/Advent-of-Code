3836abad0695662725cee0265448db1c5cba7744
Chris-GW <Chris-GW@users.noreply.github.com>
2021-12-17 12:36:01 +0100

    2021 Day 6: Lanternfish - part 1

--- 2021/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
+++ 2021/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
@@ -3,7 +3,11 @@ package de.adventofcode.chrisgw.day06;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 
 /**
  * https://adventofcode.com/2021/day/6
@@ -14,14 +18,67 @@ public class AdventOfCodeDay06 extends AdventOfCodePuzzleSolver<Integer> {
         super(Year.of(2021), 6, inputLines);
     }
 
+
     public Integer solveFirstPart() {
-        //TODO solveFirstPart
-        return 0;
+        List<Lanternfish> lanternfishs = new ArrayList<>(parseLanternfishs());
+        for (int day = 1; day <= 80; day++) {
+            List<Lanternfish> newLanternfishs = lanternfishs.stream()
+                    .map(Lanternfish::nextDayWithBirth)
+                    .flatMap(Optional::stream)
+                    .toList();
+            lanternfishs.addAll(newLanternfishs);
+        }
+        return lanternfishs.size();
     }
+
 
     public Integer solveSecondPart() {
         //TODO solveSecondPart
         return 0;
+    }
+
+
+    private List<Lanternfish> parseLanternfishs() {
+        return inputLines().flatMap(line -> Arrays.stream(line.split(",")))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(Lanternfish::new)
+                .toList();
+    }
+
+
+    public static class Lanternfish {
+
+        private int internalTimer;
+
+
+        public Lanternfish() {
+            this(8);
+        }
+
+        public Lanternfish(int internalTimer) {
+            if (0 < internalTimer && internalTimer <= 8) {
+                this.internalTimer = internalTimer;
+            } else {
+                throw new IllegalArgumentException(
+                        "internal birth timer must be between [0:9], but was: " + internalTimer);
+            }
+        }
+
+
+        public Optional<Lanternfish> nextDayWithBirth() {
+            if (internalTimer-- == 0) {
+                internalTimer = 6;
+                return Optional.of(new Lanternfish());
+            }
+            return Optional.empty();
+        }
+
+
+        @Override
+        public String toString() {
+            return String.valueOf(internalTimer);
+        }
+
     }
 
 }
--- 2021/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
+++ 2021/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
@@ -1,25 +1,28 @@
 package de.adventofcode.chrisgw.day06;
 
 import org.junit.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.Assert.*;
 
+
 public class AdventOfCodeDay06Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of("3,4,3,1,2");
 
     @Test
     public void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay06(inputLinesExample).solveFirstPart();
-        assertEquals("firstPart example", 1, result);
+        assertEquals("firstPart example", 5934, result);
     }
 
     @Test
     public void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay06.txt");
         int result = new AdventOfCodeDay06(inputLines).solveFirstPart();
-        assertEquals("firstPart myPuzzleInput", 1, result);
+        assertEquals("firstPart myPuzzleInput", 354564, result);
     }
 
     @Test
