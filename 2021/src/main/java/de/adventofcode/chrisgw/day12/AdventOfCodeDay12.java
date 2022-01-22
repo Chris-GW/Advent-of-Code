77a9ace81c28059bbd2b4b55bf966ba6c45997a7
Chris-GW <Chris-GW@users.noreply.github.com>
2022-01-22 23:42:35 +0100

    2021 Day 12: Passage Pathing - part 1

--- 2021/src/main/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12.java
+++ 2021/src/main/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12.java
@@ -1,9 +1,16 @@
 package de.adventofcode.chrisgw.day12;
 
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.Year;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * Day 12: Passage Pathing
@@ -11,19 +18,143 @@ import java.util.List;
  */
 public class AdventOfCodeDay12 extends AdventOfCodePuzzleSolver<Integer> {
 
+    public static final String START_CAVE_NAME = "start";
+    public static final String END_CAVE_NAME = "end";
+    public final Map<String, Cave> caveNameMap;
+
+
     public AdventOfCodeDay12(List<String> inputLines) {
         super(Year.of(2021), 12, inputLines);
+        caveNameMap = parseCaveNetwork(inputLines);
+    }
+
+    private Map<String, Cave> parseCaveNetwork(List<String> inputLines) {
+        Map<String, Cave> caveNameMap = new HashMap<>();
+
+        for (String caveEntry : inputLines) {
+            String[] splitCaveEntry = caveEntry.split("-");
+            Cave cave = caveNameMap.computeIfAbsent(splitCaveEntry[0], Cave::new);
+            Cave otherCave = caveNameMap.computeIfAbsent(splitCaveEntry[1], Cave::new);
+            cave.connect(otherCave);
+        }
+        return caveNameMap;
+    }
+
+
+    public Cave findCave(String name) {
+        return caveNameMap.get(name);
+    }
+
+    public Cave startCave() {
+        return findCave(START_CAVE_NAME);
+    }
+
+    public Cave endCave() {
+        return findCave(END_CAVE_NAME);
     }
 
 
     public Integer solveFirstPart() {
-        //TODO solveFirstPart
-        return 0;
+        CavePath cavePath = new CavePath().append(startCave());
+        Set<CavePath> possibleCavePaths = buildPossibleCavePaths(cavePath).collect(Collectors.toSet());
+        return possibleCavePaths.size();
+    }
+
+    private Stream<CavePath> buildPossibleCavePaths(CavePath cavePath) {
+        if (cavePath.getLast().equals(endCave())) {
+            return Stream.of(cavePath);
+        }
+        return cavePath.getLast()
+                .connectedCaves()
+                .filter(cavePath::canAppend)
+                .map(cavePath::append)
+                .flatMap(this::buildPossibleCavePaths);
     }
 
     public Integer solveSecondPart() {
         //TODO solveSecondPart
         return 0;
+    }
+
+
+    @Data
+    public static class Cave {
+
+        private final String name;
+
+        @EqualsAndHashCode.Exclude
+        private final Set<Cave> connectedCaves = new HashSet<>();
+
+
+        public boolean connect(Cave otherCave) {
+            if (connectedCaves.add(otherCave)) {
+                otherCave.connect(this);
+                return true;
+            }
+            return false;
+        }
+
+        public boolean isBigCave() {
+            return StringUtils.isAllUpperCase(name);
+        }
+
+        public Stream<Cave> connectedCaves() {
+            return connectedCaves.stream();
+        }
+
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+    }
+
+
+    @Data
+    public static class CavePath implements Iterable<Cave> {
+
+        private final Deque<Cave> cavePath = new ArrayDeque<>();
+
+
+        public boolean canAppend(Cave cave) {
+            return cave != null && cave.isBigCave() || !containsCave(cave);
+        }
+
+        public boolean containsCave(Cave cave) {
+            return cavePath.contains(cave);
+        }
+
+
+        public Cave getFirst() {
+            return cavePath.getFirst();
+        }
+
+        public Cave getLast() {
+            return cavePath.getLast();
+        }
+
+        public CavePath append(Cave cave) {
+            if (!canAppend(cave)) {
+                throw new IllegalArgumentException("can't append cave: " + cave);
+            }
+            CavePath cavePath = new CavePath();
+            cavePath.cavePath.addAll(this.cavePath);
+            cavePath.cavePath.add(cave);
+            return cavePath;
+        }
+
+
+        @Override
+        public Iterator<Cave> iterator() {
+            return cavePath.iterator();
+        }
+
+        @Override
+        public String toString() {
+            return cavePath.stream().map(Cave::getName).collect(joining(","));
+        }
+
     }
 
 }
--- 2021/src/test/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12Test.java
+++ 2021/src/test/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12Test.java
@@ -1,30 +1,61 @@
 package de.adventofcode.chrisgw.day12;
 
 import org.junit.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDay12Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample1 = List.of( //
+            "start-A", //
+            "start-b", //
+            "A-c", //
+            "A-b", //
+            "b-d", //
+            "A-end", //
+            "b-end");
+
 
     @Test
-    public void solveAocPuzzle_firstPart_example() {
-        int result = new AdventOfCodeDay12(inputLinesExample).solveFirstPart();
-        assertEquals("firstPart example", 1, result);
+    public void solveAocPuzzle_firstPart_example1() {
+        int result = new AdventOfCodeDay12(inputLinesExample1).solveFirstPart();
+        assertEquals("firstPart example", 10, result);
     }
+
+
+    private static final List<String> inputLinesExample2 = List.of( //
+            "dc-end", //
+            "HN-start", //
+            "start-kj", //
+            "dc-start", //
+            "dc-HN", //
+            "LN-dc", //
+            "HN-end", //
+            "kj-sa", //
+            "kj-HN", //
+            "kj-dc");
+
+    @Test
+    public void solveAocPuzzle_firstPart_example2() {
+        int result = new AdventOfCodeDay12(inputLinesExample2).solveFirstPart();
+        assertEquals("firstPart example", 19, result);
+    }
+
 
     @Test
     public void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay12.txt");
         int result = new AdventOfCodeDay12(inputLines).solveFirstPart();
-        assertEquals("firstPart myPuzzleInput", 1, result);
+        assertEquals("firstPart myPuzzleInput", 3679, result);
     }
+
 
     @Test
     public void solveAocPuzzle_secondPart_example() {
-        int result = new AdventOfCodeDay12(inputLinesExample).solveSecondPart();
+        int result = new AdventOfCodeDay12(inputLinesExample1).solveSecondPart();
         assertEquals("secondPart example", 1, result);
     }
 
