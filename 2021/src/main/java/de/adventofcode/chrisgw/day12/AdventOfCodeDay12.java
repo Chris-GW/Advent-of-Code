7727e2b0cd2ef0fec7c152e47187f538135e5faa
Chris-GW <Chris-GW@users.noreply.github.com>
2022-01-22 15:40:08 +0100

    2021 Day 12: Passage Pathing - start

--- 2021/src/main/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12.java
+++ 2021/src/main/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12.java
@@ -6,6 +6,7 @@ import java.time.Year;
 import java.util.List;
 
 /**
+ * Day 12: Passage Pathing
  * https://adventofcode.com/2021/day/12
  */
 public class AdventOfCodeDay12 extends AdventOfCodePuzzleSolver<Integer> {
@@ -13,6 +14,7 @@ public class AdventOfCodeDay12 extends AdventOfCodePuzzleSolver<Integer> {
     public AdventOfCodeDay12(List<String> inputLines) {
         super(Year.of(2021), 12, inputLines);
     }
+
 
     public Integer solveFirstPart() {
         //TODO solveFirstPart
