da232da7422ffee54095bd59b00b88d9edfec10c
Chris-GW <Chris-GW@users.noreply.github.com>
2022-01-27 15:38:08 +0100

    2021 Day 15: Chiton - start

--- 2021/src/test/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15Test.java
+++ 2021/src/test/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15Test.java
@@ -1,13 +1,27 @@
 package de.adventofcode.chrisgw.day15;
 
 import org.junit.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.Assert.*;
 
+
 public class AdventOfCodeDay15Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of( //
+            "1163751742", //
+            "1381373672", //
+            "2136511328", //
+            "3694931569", //
+            "7463417111", //
+            "1319128137", //
+            "1359912421", //
+            "3125421639", //
+            "1293138521", //
+            "2311944581");
+
 
     @Test
     public void solveAocPuzzle_firstPart_example() {
@@ -21,6 +35,7 @@ public class AdventOfCodeDay15Test {
         int result = new AdventOfCodeDay15(inputLines).solveFirstPart();
         assertEquals("firstPart myPuzzleInput", 1, result);
     }
+
 
     @Test
     public void solveAocPuzzle_secondPart_example() {
