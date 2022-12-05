938cbbf8543d992ea47ba637d399673a4d77f2b4
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-05 16:32:13 +0100

    2022 Day 5: Supply Stacks - start

---
+++ 2022/src/test/java/de/adventofcode/chrisgw/day05/AdventOfCodeDay05Test.java
@@ -0,0 +1,47 @@
+package de.adventofcode.chrisgw.day05;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdventOfCodeDay05Test {
+
+    private static final List<String> inputLinesExample = List.of( //
+            "    [D]    ", //
+            "[N] [C]    ", //
+            "[Z] [M] [P]", //
+            " 1   2   3 ", //
+            "", //
+            "move 1 from 2 to 1", //
+            "move 3 from 1 to 3", //
+            "move 2 from 2 to 1", //
+            "move 1 from 1 to 2"); //
+
+    @Test
+    public void solveAocPuzzle_firstPart_example() {
+        int result = new AdventOfCodeDay05(inputLinesExample).solveFirstPart();
+        assertEquals(1, result, "firstPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_firstPart_myPuzzleInput() {
+        List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay05.txt");
+        int result = new AdventOfCodeDay05(inputLines).solveFirstPart();
+        assertEquals(1, result, "firstPart myPuzzleInput");
+    }
+
+    @Test
+    void solveAocPuzzle_secondPart_example() {
+        int result = new AdventOfCodeDay05(inputLinesExample).solveSecondPart();
+        assertEquals(1, result, "secondPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_secondPart_myPuzzleInput() {
+        List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay05.txt");
+        int result = new AdventOfCodeDay05(inputLines).solveSecondPart();
+        assertEquals(1, result, "secondPart myPuzzleInput");
+    }
+
+}
