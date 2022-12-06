7c37474cdb84983c3f82117e7a41a25593b99c58
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-06 13:04:56 +0100

    2022 Day 3: Rucksack Reorganization - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day03;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/3">Advent of Code - day 3</a>
+ */
+public class AdventOfCodeDay03 extends AdventOfCodePuzzleSolver<Integer> {
+
+    public AdventOfCodeDay03(List<String> inputLines) {
+        super(Year.of(2022), 3, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 1;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 1;
+    }
+
+}
