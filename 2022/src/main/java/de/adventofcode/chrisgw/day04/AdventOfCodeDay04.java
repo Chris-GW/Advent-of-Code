aa3c779cd5125dd5b9e799da31ffb960fbf55535
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-07 10:08:57 +0100

    2022 Day 4: Camp Cleanup - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day04;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/4">Advent of Code - day 4</a>
+ */
+public class AdventOfCodeDay04 extends AdventOfCodePuzzleSolver<Integer> {
+
+    public AdventOfCodeDay04(List<String> inputLines) {
+        super(Year.of(2022), 4, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 1;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 1;
+    }
+
+}
