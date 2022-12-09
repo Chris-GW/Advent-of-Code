49a912934ecb08ac8822f126e8a962960b9f7d63
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-09 14:58:06 +0100

    2022 Day 9: Rope Bridge - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day09;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/9">Advent of Code - day 9</a>
+ */
+public class AdventOfCodeDay09 extends AdventOfCodePuzzleSolver<Integer> {
+
+    public AdventOfCodeDay09(List<String> inputLines) {
+        super(Year.of(2022), 9, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 1;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 1;
+    }
+
+}
---
+++ 2022/src/test/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09Test.java
@@ -0,0 +1,41 @@
+package de.adventofcode.chrisgw.day09;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class AdventOfCodeDay09Test {
+
+    private static final List<String> inputLinesExample = List.of("");
+
+    @Test
+    void solveAocPuzzle_firstPart_example() {
+        int result = new AdventOfCodeDay09(inputLinesExample).solveFirstPart();
+        assertEquals(1, result, "firstPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_firstPart_myPuzzleInput() {
+        List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay09.txt");
+        int result = new AdventOfCodeDay09(inputLines).solveFirstPart();
+        assertEquals(1, result, "firstPart myPuzzleInput");
+    }
+
+    @Test
+    void solveAocPuzzle_secondPart_example() {
+        int result = new AdventOfCodeDay09(inputLinesExample).solveSecondPart();
+        assertEquals(1, result, "secondPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_secondPart_myPuzzleInput() {
+        List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay09.txt");
+        int result = new AdventOfCodeDay09(inputLines).solveSecondPart();
+        assertEquals(1, result, "secondPart myPuzzleInput");
+    }
+
+}
