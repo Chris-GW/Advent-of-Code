59d35eb53fd6ac787d293b3d75b64404bdb693ee
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-15 22:33:38 +0100

    2022 Day 13: Distress Signal - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day13/AdventOfCodeDay13.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day13;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/13">Advent of Code - day 13</a>
+ */
+public class AdventOfCodeDay13 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay13(List<String> inputLines) {
+        super(Year.of(2022), 13, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
---
+++ 2022/src/test/java/de/adventofcode/chrisgw/day13/AdventOfCodeDay13Test.java
@@ -0,0 +1,64 @@
+package de.adventofcode.chrisgw.day13;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdventOfCodeDay13Test {
+
+    private static final List<String> inputLinesExample = List.of( //s
+            "[1,1,3,1,1]", //
+            "[1,1,5,1,1]", //
+            "", //
+            "[[1],[2,3,4]]", //
+            "[[1],4]", //
+            "", //
+            "[9]", //
+            "[[8,7,6]]", //
+            "", //
+            "[[4,4],4,4]", //
+            "[[4,4],4,4,4]", //
+            "", //
+            "[7,7,7,7]", //
+            "[7,7,7]", //
+            "", //
+            "[]", //
+            "[3]", //
+            "", //
+            "[[[]]]", //
+            "[[]]", //
+            "", //
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]", //
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]");
+
+    @Test
+    void solveAocPuzzle_firstPart_example() {
+        int result = new AdventOfCodeDay13(inputLinesExample).solveFirstPart();
+        assertEquals(1, result, "firstPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_firstPart_myPuzzleInput() {
+        List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay13.txt");
+        int result = new AdventOfCodeDay13(inputLines).solveFirstPart();
+        assertEquals(1, result, "firstPart myPuzzleInput");
+    }
+
+
+    @Test
+    void solveAocPuzzle_secondPart_example() {
+        int result = new AdventOfCodeDay13(inputLinesExample).solveSecondPart();
+        assertEquals(1, result, "secondPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_secondPart_myPuzzleInput() {
+        List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay13.txt");
+        int result = new AdventOfCodeDay13(inputLines).solveSecondPart();
+        assertEquals(1, result, "secondPart myPuzzleInput");
+    }
+
+}
