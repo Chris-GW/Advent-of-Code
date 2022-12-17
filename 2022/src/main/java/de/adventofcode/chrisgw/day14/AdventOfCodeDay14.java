138b467fad995f4b04b807ca441184762ebc004e
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-17 13:14:31 +0100

    2022 Day 14: Regolith Reservoir - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day14/AdventOfCodeDay14.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day14;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/14">Advent of Code - day 14</a>
+ */
+public class AdventOfCodeDay14 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay14(List<String> inputLines) {
+        super(Year.of(2022), 14, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
