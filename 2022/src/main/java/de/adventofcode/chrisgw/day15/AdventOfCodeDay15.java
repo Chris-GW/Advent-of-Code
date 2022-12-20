c15fe494cff7b6f5833c7be7e6cf50f80c043fe6
Chris-GW <Chris-GW@users.noreply.github.com>
2022-12-20 12:11:13 +0100

    2022 Day 15: Beacon Exclusion Zone - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day15/AdventOfCodeDay15.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day15;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/15">Advent of Code - day 15</a>
+ */
+public class AdventOfCodeDay15 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay15(List<String> inputLines) {
+        super(Year.of(2022), 15, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
