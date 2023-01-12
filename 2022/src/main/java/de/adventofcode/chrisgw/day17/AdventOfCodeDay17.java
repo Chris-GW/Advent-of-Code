0bc015875258798d4e1cb73c469a5bf0f83fcea2
Chris-GW <Chris-GW@users.noreply.github.com>
2023-01-12 09:45:19 +0100

    2022 Day 17: Pyroclastic Flow - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day17/AdventOfCodeDay17.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day17;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/17">Advent of Code - day 17</a>
+ */
+public class AdventOfCodeDay17 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay17(List<String> inputLines) {
+        super(Year.of(2022), 17, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
