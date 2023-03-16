de222ee0d0d4c5ed678d54b07e5eec4c44889648
Chris-GW <Chris-GW@users.noreply.github.com>
2023-03-16 08:44:52 +0100

    2022 Day 19: Not Enough Minerals - start

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day19/AdventOfCodeDay19.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day19;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/19">Advent of Code - day 19</a>
+ */
+public class AdventOfCodeDay19 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay19(List<String> inputLines) {
+        super(Year.of(2022), 19, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
