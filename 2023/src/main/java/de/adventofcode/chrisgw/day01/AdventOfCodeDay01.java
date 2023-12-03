19cb3a0003ef076fbdd5726660c7271fcfc244fd
ckramer <ckramer@ukaachen.de>
2023-12-03 13:08:03 +0100

    2023 Day 1: Trebuchet?! - start

---
+++ 2023/src/main/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01.java
@@ -0,0 +1,29 @@
+package de.adventofcode.chrisgw.day01;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2023/day/1">Advent of Code - day 1</a>
+ */
+public class AdventOfCodeDay01 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay01(List<String> inputLines) {
+        super(Year.of(2023), 1, inputLines);
+    }
+
+
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
