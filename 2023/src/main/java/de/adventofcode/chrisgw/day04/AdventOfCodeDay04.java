1b95c9c0f6825915712e3c02b479615ba3496f5e
ckramer <ckramer@ukaachen.de>
2023-12-04 15:33:14 +0100

    2023 Day 4: Scratchcards - part 1

--- 2023/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
+++ 2023/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
@@ -1,9 +1,15 @@
 package de.adventofcode.chrisgw.day04;
 
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.time.Year;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 
 /**
@@ -18,8 +24,7 @@ public class AdventOfCodeDay04 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+        return inputLines().map(ScratchCard::parseScratchCard).mapToInt(ScratchCard::points).sum();
     }
 
 
@@ -27,6 +32,81 @@ public class AdventOfCodeDay04 extends AdventOfCodePuzzleSolver {
     public Integer solveSecondPart() {
         // TODO solveSecondPart
         return 0;
+    }
+
+
+    public record ScratchCard(int cardNumber, int[] winningNumbers, int[] ownedNumbers) {
+
+        public static final Pattern SCRATCH_CARD_PATTERN = Pattern.compile(
+                "Card\\s+(?<cardNumber>\\d+):(?<winningNumbers>(?:\\s+\\d+)+)\\s+\\|(?<ownedNumbers>(?:\\s+\\d+)+)");
+
+
+        public static ScratchCard parseScratchCard(String cardLine) {
+            Matcher matcher = SCRATCH_CARD_PATTERN.matcher(cardLine);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException(
+                        "expect scratch card matching " + SCRATCH_CARD_PATTERN + ", but was: " + cardLine);
+            }
+            int cardNumber = Integer.parseInt(matcher.group("cardNumber"));
+            String winningNumbersStr = matcher.group("winningNumbers").trim();
+            String ownedNumbersStr = matcher.group("ownedNumbers").trim();
+            int[] winningNumbers = Arrays.stream(winningNumbersStr.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            int[] ownedNumbers = Arrays.stream(ownedNumbersStr.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            return new ScratchCard(cardNumber, winningNumbers, ownedNumbers);
+        }
+
+
+        public int points() {
+            return IntStream.rangeClosed(1, countMachtingNumber())
+                    .reduce(((accumulator, right) -> accumulator * 2))
+                    .orElse(0);
+        }
+
+        private int countMachtingNumber() {
+            return Math.toIntExact(Arrays.stream(winningNumbers()).filter(this::ownsNumber).count());
+        }
+
+        private boolean ownsNumber(int number) {
+            return Arrays.stream(ownedNumbers()).anyMatch(value -> value == number);
+        }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof ScratchCard that)) {
+                return false;
+            }
+            return new EqualsBuilder().append(cardNumber, that.cardNumber)
+                    .append(winningNumbers, that.winningNumbers)
+                    .append(ownedNumbers, that.ownedNumbers)
+                    .isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder(17, 37).append(cardNumber)
+                    .append(winningNumbers)
+                    .append(ownedNumbers)
+                    .toHashCode();
+        }
+
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Card ").append(String.format("%3d", cardNumber())).append(":");
+            for (int winningNumber : winningNumbers()) {
+                sb.append(String.format(" %2d", winningNumber));
+            }
+            sb.append(" |");
+            for (int ownedNumber : ownedNumbers()) {
+                sb.append(String.format(" %2d", ownedNumber));
+            }
+            return sb.toString();
+        }
     }
 
 }
--- 2023/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
+++ 2023/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
@@ -1,25 +1,34 @@
 package de.adventofcode.chrisgw.day04;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class AdventOfCodeDay04Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of( //
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", //
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", //
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", //
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", //
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", //
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay04(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        assertEquals(13, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay04.txt");
         int result = new AdventOfCodeDay04(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(18519, result, "firstPart myPuzzleInput");
     }
 
 
