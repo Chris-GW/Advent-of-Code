c545ec78d3121d6ebf444e7f99a55479f7335043
ckramer <ckramer@ukaachen.de>
2023-12-05 08:31:34 +0100

    2023 Day 5: If You Give A Seed A Fertilizer - start
    Took 46 seconds

---
+++ 2023/src/main/java/de/adventofcode/chrisgw/day05/AdventOfCodeDay05.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day05;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2023/day/5">Advent of Code - day 5</a>
+ */
+public class AdventOfCodeDay05 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay05(List<String> inputLines) {
+        super(Year.of(2023), 5, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
