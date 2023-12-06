bba2d2d3208ebb7a94fd1d4ee0472b5501cb8111
ckramer <ckramer@ukaachen.de>
2023-12-06 14:29:41 +0100

    2022 Day 20: Grove Positioning System - start
    Took 5 minutes

---
+++ 2022/src/main/java/de/adventofcode/chrisgw/day20/AdventOfCodeDay20.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day20;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2022/day/20">Advent of Code - day 20</a>
+ */
+public class AdventOfCodeDay20 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay20(List<String> inputLines) {
+        super(Year.of(2022), 20, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
