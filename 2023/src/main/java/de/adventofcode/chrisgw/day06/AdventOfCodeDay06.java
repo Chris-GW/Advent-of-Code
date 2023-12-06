44a187c02b9afcc49a9718d792225a3f72f9666c
ckramer <ckramer@ukaachen.de>
2023-12-06 10:49:55 +0100

    2023 Day 6: Wait For It - start
    Took 1 minute

---
+++ 2023/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day06;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2023/day/6">Advent of Code - day 6</a>
+ */
+public class AdventOfCodeDay06 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay06(List<String> inputLines) {
+        super(Year.of(2023), 6, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
