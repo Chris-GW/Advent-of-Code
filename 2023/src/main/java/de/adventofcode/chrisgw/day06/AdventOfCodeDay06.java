d5e8b30037269122d8a541f8f90d1db274f6f6d1
ckramer <ckramer@ukaachen.de>
2023-12-06 11:09:13 +0100

    2023 Day 6: Wait For It - part 1
    Took 19 minutes

--- 2023/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
+++ 2023/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
@@ -3,7 +3,10 @@ package de.adventofcode.chrisgw.day06;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 
 /**
@@ -18,8 +21,11 @@ public class AdventOfCodeDay06 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+        List<ToyBoatRace> races = parseToyBoatRaces(getInputLines());
+        return races.stream()
+                .mapToInt(ToyBoatRace::countPossibleWaysOfWinning)
+                .reduce((left, right) -> left * right)
+                .orElse(0);
     }
 
 
@@ -27,6 +33,53 @@ public class AdventOfCodeDay06 extends AdventOfCodePuzzleSolver {
     public Integer solveSecondPart() {
         // TODO solveSecondPart
         return 0;
+    }
+
+
+    private List<ToyBoatRace> parseToyBoatRaces(List<String> inputLines) {
+        int[] raceTimes = new int[0];
+        int[] recordDistances = new int[0];
+        for (String line : inputLines) {
+            if (line.startsWith("Time:")) {
+                raceTimes = Arrays.stream(line.substring("Time:".length()).trim().split("\\s+"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+            } else if (line.startsWith("Distance:")) {
+                recordDistances = Arrays.stream(line.substring("Distance:".length()).trim().split("\\s+"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+            } else {
+                throw new IllegalArgumentException("could not parse: " + line);
+            }
+        }
+        if (raceTimes.length != recordDistances.length) {
+            throw new IllegalArgumentException(
+                    "expect same size for raceTimes and recordDistances, but was: %d and %d".formatted( //
+                            raceTimes.length, recordDistances.length));
+        }
+
+        List<ToyBoatRace> races = new ArrayList<>(raceTimes.length);
+        for (int i = 0; i < raceTimes.length; i++) {
+            int raceDuration = raceTimes[i];
+            int recordDistance = recordDistances[i];
+            races.add(new ToyBoatRace(raceDuration, recordDistance));
+        }
+        return races;
+    }
+
+
+    public record ToyBoatRace(int raceDuration, int recordDistance) {
+
+        public int countPossibleWaysOfWinning() {
+            return Math.toIntExact(IntStream.range(1, raceDuration).filter(this::isWinningCharge).count());
+        }
+
+        private boolean isWinningCharge(int chargeDuration) {
+            int movingDuration = raceDuration - chargeDuration;
+            int distance = movingDuration * chargeDuration;
+            return distance > recordDistance;
+        }
+
     }
 
 }
--- 2023/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
+++ 2023/src/test/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06Test.java
@@ -1,25 +1,32 @@
 package de.adventofcode.chrisgw.day06;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class AdventOfCodeDay06Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of( //
+            "Time:      7  15   30", //
+            "Distance:  9  40  200");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay06(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        int expected = 4 * 8 * 9;
+        assertEquals(288, expected, "expected result");
+        assertEquals(expected, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay06.txt");
         int result = new AdventOfCodeDay06(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(3317888, result, "firstPart myPuzzleInput");
     }
 
 
