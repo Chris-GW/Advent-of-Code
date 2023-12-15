4d47378d514b7c2267d7e461e7f621f898c9086f
Chris-GW <Chris-GW@users.noreply.github.com>
2023-12-15 09:00:47 +0100

    2023 Day 7: Camel Cards - start

---
+++ 2023/src/main/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day07;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2023/day/7">Advent of Code - day 7</a>
+ */
+public class AdventOfCodeDay07 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay07(List<String> inputLines) {
+        super(Year.of(2023), 7, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
