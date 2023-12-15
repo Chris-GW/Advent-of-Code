a1d041e04821a8fdc94cd47bb461298a8fd4bb47
Chris-GW <Chris-GW@users.noreply.github.com>
2023-12-15 11:22:36 +0100

    2023 Day 8: Haunted Wasteland - part 1

--- 2023/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
+++ 2023/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
@@ -4,6 +4,12 @@ import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 /**
@@ -18,8 +24,29 @@ public class AdventOfCodeDay08 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+        List<String> inputLines = getInputLines();
+        String directionLine = inputLines.get(0);
+        List<Direction> directions = directionLine.chars()
+                .mapToObj(Character::toString)
+                .map(Direction::valueOf)
+                .toList();
+
+        Map<String, NetworkRecord> networkMap = inputLines.stream()
+                .skip(2)
+                .map(NetworkRecord::parseNetworkRecord)
+                .collect(Collectors.toMap(NetworkRecord::startNode, Function.identity()));
+
+        String startNode = "AAA";
+        String targetNode = "ZZZ";
+        String currentNode = startNode;
+
+        for (int step = 0; true; step++) {
+            if (currentNode.equals(targetNode)) {
+                return step;
+            }
+            Direction direction = directions.get(step % directions.size());
+            currentNode = networkMap.get(currentNode).nodeInDirection(direction);
+        }
     }
 
 
@@ -27,6 +54,44 @@ public class AdventOfCodeDay08 extends AdventOfCodePuzzleSolver {
     public Integer solveSecondPart() {
         // TODO solveSecondPart
         return 0;
+    }
+
+
+    public record NetworkRecord(String startNode, String leftNode, String rightNode) {
+
+        public static final Pattern NETWORK_RECORD_PATTERN = Pattern.compile("([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)");
+
+        public static NetworkRecord parseNetworkRecord(String networkRecordLine) {
+            Matcher matcher = NETWORK_RECORD_PATTERN.matcher(networkRecordLine);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("expect network record line matching pattern "
+                        + NETWORK_RECORD_PATTERN + ", but was: " + networkRecordLine);
+            }
+            String startingNodeLabel = matcher.group(1);
+            String leftNodeLabel = matcher.group(2);
+            String rightNodeLabel = matcher.group(3);
+            return new NetworkRecord(startingNodeLabel, leftNodeLabel, rightNodeLabel);
+        }
+
+
+        public String nodeInDirection(Direction direction) {
+            return switch (direction) {
+                case L -> leftNode();
+                case R -> rightNode();
+            };
+        }
+
+
+        @Override
+        public String toString() {
+            return "%s = (%s, %s)".formatted(startNode(), leftNode(), rightNode());
+        }
+
+    }
+
+    public enum Direction {
+        L, R
+
     }
 
 }
--- 2023/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
+++ 2023/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
@@ -1,25 +1,48 @@
 package de.adventofcode.chrisgw.day08;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdventOfCodeDay08Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of( //
+            "RL", //
+            "", //
+            "AAA = (BBB, CCC)", //
+            "BBB = (DDD, EEE)", //
+            "CCC = (ZZZ, GGG)", //
+            "DDD = (DDD, DDD)", //
+            "EEE = (EEE, EEE)", //
+            "GGG = (GGG, GGG)", //
+            "ZZZ = (ZZZ, ZZZ)");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay08(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        assertEquals(2, result, "firstPart example");
+    }
+
+    @Test
+    void solveAocPuzzle_firstPart_example2() {
+        List<String> inputLinesExample2 = List.of( //
+                "LLR", //
+                "", //
+                "AAA = (BBB, BBB)", //
+                "BBB = (AAA, ZZZ)", //
+                "ZZZ = (ZZZ, ZZZ)");
+        int result = new AdventOfCodeDay08(inputLinesExample2).solveFirstPart();
+        assertEquals(6, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay08.txt");
         int result = new AdventOfCodeDay08(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(21251, result, "firstPart myPuzzleInput");
     }
 
 
