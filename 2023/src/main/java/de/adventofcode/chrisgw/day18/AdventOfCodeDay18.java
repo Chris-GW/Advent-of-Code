ae979b8a04bb0b50774651af9b8e84eced64f84f
Chris-GW <Chris-GW@users.noreply.github.com>
2023-12-18 14:55:34 +0100

    2023 Day 18: Lavaduct Lagoon - start

---
+++ 2023/src/main/java/de/adventofcode/chrisgw/day18/AdventOfCodeDay18.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day18;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2023/day/18">Advent of Code - day 18</a>
+ */
+public class AdventOfCodeDay18 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay18(List<String> inputLines) {
+        super(Year.of(2023), 18, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
