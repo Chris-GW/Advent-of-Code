e38053d77bb3353419d66ec81350b4c0033618f8
Chris-GW <Chris-GW@users.noreply.github.com>
2024-11-21 16:52:49 +0100

    2023 Day 09: Day 9: Mirage Maintenance - part 2
    Took 46 minutes

--- 2023/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
+++ 2023/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
@@ -1,9 +1,14 @@
 package de.adventofcode.chrisgw.day09;
 
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.time.Year;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 
 /**
@@ -18,15 +23,96 @@ public class AdventOfCodeDay09 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+        return inputLines().map(HistoryRecord::parseHistoryRecord).mapToInt(HistoryRecord::predictNextValue).sum();
     }
 
 
     @Override
     public Integer solveSecondPart() {
-        // TODO solveSecondPart
-        return 0;
+        return inputLines().map(HistoryRecord::parseHistoryRecord).mapToInt(HistoryRecord::predictPreviousValue).sum();
+    }
+
+
+    public record HistoryRecord(int[] values) {
+
+        public static HistoryRecord parseHistoryRecord(String line) {
+            Pattern numberPattern = Pattern.compile("\\s+");
+            int[] values = numberPattern.splitAsStream(line).mapToInt(Integer::parseInt).toArray();
+            return new HistoryRecord(values);
+        }
+
+
+        public int predictNextValue() {
+            List<HistoryRecord> steps = Stream.iterate(this,
+                    HistoryRecord::isNotAllZero, HistoryRecord::newDifferenceSequence).toList();
+
+            int previousDifference = 0;
+            for (int step = steps.size() - 1; step >= 0; step--) {
+                int valueToLeft = steps.get(step).lastValue();
+                previousDifference = valueToLeft + previousDifference;
+            }
+            return previousDifference;
+        }
+
+        public int predictPreviousValue() {
+            List<HistoryRecord> steps = Stream.iterate(this,
+                    HistoryRecord::isNotAllZero, HistoryRecord::newDifferenceSequence).toList();
+
+            int previousDifference = 0;
+            for (int step = steps.size() - 1; step >= 0; step--) {
+                int valueToRight = steps.get(step).firstValue();
+                previousDifference = valueToRight - previousDifference;
+            }
+            return previousDifference;
+        }
+
+
+        private HistoryRecord newDifferenceSequence() {
+            int[] differenceSequence = new int[values.length - 1];
+            for (int i = 0; i < differenceSequence.length; i++) {
+                int leftValue = values[i];
+                int rightValue = values[i + 1];
+                differenceSequence[i] = rightValue - leftValue;
+            }
+            return new HistoryRecord(differenceSequence);
+        }
+
+
+        private boolean isNotAllZero() {
+            return !Arrays.stream(values).allMatch(value -> value == 0);
+        }
+
+
+        private int firstValue() {
+            return values[0];
+        }
+
+        private int lastValue() {
+            return values[values.length - 1];
+        }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof HistoryRecord that)) {
+                return false;
+            }
+            return new EqualsBuilder().append(values(), that.values()).isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder(17, 37).append(values()).toHashCode();
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(values);
+        }
+
     }
 
 }
--- 2023/src/test/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09Test.java
+++ 2023/src/test/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09Test.java
@@ -7,33 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdventOfCodeDay09Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of(
+            "0 3 6 9 12 15",
+            "1 3 6 10 15 21",
+            "10 13 16 21 30 45");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay09(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        assertEquals(114, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay09.txt");
         int result = new AdventOfCodeDay09(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(1861775706, result, "firstPart myPuzzleInput");
     }
 
 
     @Test
     void solveAocPuzzle_secondPart_example() {
         int result = new AdventOfCodeDay09(inputLinesExample).solveSecondPart();
-        assertEquals(1, result, "secondPart example");
+        assertEquals(2, result, "secondPart example");
     }
 
     @Test
     void solveAocPuzzle_secondPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay09.txt");
         int result = new AdventOfCodeDay09(inputLines).solveSecondPart();
-        assertEquals(1, result, "secondPart myPuzzleInput");
+        assertEquals(1082, result, "secondPart myPuzzleInput");
     }
 
 }
