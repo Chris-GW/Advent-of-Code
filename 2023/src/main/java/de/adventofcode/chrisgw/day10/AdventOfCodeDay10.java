bb6770bf5322e78c7f88dd9c218e6b9705a18b4d
Chris Kramer <ckramer@ukaachen.de>
2024-11-29 09:37:24 +0100

    2023 Day 10: Pipe Maze - start
    Took 9 minutes

---
+++ 2023/src/main/java/de/adventofcode/chrisgw/day10/AdventOfCodeDay10.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day10;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2023/day/10">Advent of Code - day 10</a>
+ */
+public class AdventOfCodeDay10 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay10(List<String> inputLines) {
+        super(Year.of(2023), 10, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
