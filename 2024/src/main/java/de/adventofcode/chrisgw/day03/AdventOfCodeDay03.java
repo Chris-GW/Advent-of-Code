5dfc3ae38343b8e990ffa4a5cac87b5daf25f798
Chris-GW <Chris-GW@users.noreply.github.com>
2024-12-06 16:53:40 +0100

    2024 Day 3: Mull It Over - part 2
    Took 26 minutes

--- 2024/src/main/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03.java
+++ 2024/src/main/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03.java
@@ -3,8 +3,10 @@ package de.adventofcode.chrisgw.day03;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -21,15 +23,13 @@ public class AdventOfCodeDay03 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        Pattern mulPattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-        Matcher matcher = mulPattern.matcher(inputLines().collect(Collectors.joining("\n")));
+        List<MatchResult> foundInstructions = collectInstructions();
         int totalResult = 0;
 
-        while (matcher.find()) {
-            int x = Integer.parseInt(matcher.group(1));
-            int y = Integer.parseInt(matcher.group(2));
-            int result = x * y;
-            totalResult += result;
+        for (MatchResult foundInstruction : foundInstructions) {
+            if (foundInstruction.group().startsWith("mul(")) {
+                totalResult += executeMult(foundInstruction);
+            }
         }
         return totalResult;
     }
@@ -37,8 +37,42 @@ public class AdventOfCodeDay03 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveSecondPart() {
-        // TODO solveSecondPart
-        return 0;
+        List<MatchResult> foundInstructions = collectInstructions();
+        int totalResult = 0;
+        boolean mulEnabled = true;
+
+        for (MatchResult foundInstruction : foundInstructions) {
+            if (mulEnabled && foundInstruction.group().startsWith("mul(")) {
+                totalResult += executeMult(foundInstruction);
+            } else if (foundInstruction.group().startsWith("don't(")) {
+                mulEnabled = false;
+            } else if (foundInstruction.group().startsWith("do(")) {
+                mulEnabled = true;
+            }
+        }
+        return totalResult;
+    }
+
+
+    private List<MatchResult> collectInstructions() {
+        Pattern doPattern = Pattern.compile("do\\(\\)");
+        Pattern dontPattern = Pattern.compile("don't\\(\\)");
+        Pattern mulPattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+
+        List<MatchResult> foundInstructions = new ArrayList<>();
+        String input = inputLines().collect(Collectors.joining());
+        doPattern.matcher(input).results().forEach(foundInstructions::add);
+        dontPattern.matcher(input).results().forEach(foundInstructions::add);
+        mulPattern.matcher(input).results().forEach(foundInstructions::add);
+        foundInstructions.sort(Comparator.comparingInt(MatchResult::start));
+        return foundInstructions;
+    }
+
+
+    private static int executeMult(MatchResult foundInstruction) {
+        int x = Integer.parseInt(foundInstruction.group(1));
+        int y = Integer.parseInt(foundInstruction.group(2));
+        return x * y;
     }
 
 }
--- 2024/src/test/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03Test.java
+++ 2024/src/test/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03Test.java
@@ -9,11 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdventOfCodeDay03Test {
 
-    private static final List<String> inputLinesExample = List.of(
-            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
+        List<String> inputLinesExample = List.of(
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
         int result = new AdventOfCodeDay03(inputLinesExample).solveFirstPart();
         int expected = 2 * 4 + 5 * 5 + 11 * 8 + 8 * 5;
         assertEquals(161, expected, "expected result");
@@ -24,21 +24,25 @@ class AdventOfCodeDay03Test {
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay03.txt");
         int result = new AdventOfCodeDay03(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(157621318, result, "firstPart myPuzzleInput");
     }
 
 
     @Test
     void solveAocPuzzle_secondPart_example() {
-        int result = new AdventOfCodeDay03(inputLinesExample).solveSecondPart();
-        assertEquals(1, result, "secondPart example");
+        List<String> inputLinesExample2 = List.of(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+        int result = new AdventOfCodeDay03(inputLinesExample2).solveSecondPart();
+        int expected = 2 * 4 + 8 * 5;
+        assertEquals(48, expected, "expected result");
+        assertEquals(expected, result, "secondPart example");
     }
 
     @Test
     void solveAocPuzzle_secondPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay03.txt");
         int result = new AdventOfCodeDay03(inputLines).solveSecondPart();
-        assertEquals(1, result, "secondPart myPuzzleInput");
+        assertEquals(79845780, result, "secondPart myPuzzleInput");
     }
 
 }
