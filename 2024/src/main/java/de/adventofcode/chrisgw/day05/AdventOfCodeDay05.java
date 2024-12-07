597d8acc402b03272058cdb0c061ae27c30d3ca3
Chris-GW <Chris-GW@users.noreply.github.com>
2024-12-07 19:28:25 +0100

    2024 Day 5: Print Queue - part 1
    Took 34 minutes

--- 2024/src/main/java/de/adventofcode/chrisgw/day05/AdventOfCodeDay05.java
+++ 2024/src/main/java/de/adventofcode/chrisgw/day05/AdventOfCodeDay05.java
@@ -4,6 +4,9 @@ import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -18,8 +21,26 @@ public class AdventOfCodeDay05 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+        List<PagePrintOrderRule> pagePrintOrderRules = parseAllPagePrintOrderRules();
+        List<PageUpdate> pageUpdates = parseAllPageUpdates(pagePrintOrderRules.size() + 1);
+        return pageUpdates.stream()
+                .filter(pageUpdate -> pageUpdate.isValid(pagePrintOrderRules))
+                .mapToInt(PageUpdate::getMiddlePageNumber)
+                .sum();
+    }
+
+    private List<PagePrintOrderRule> parseAllPagePrintOrderRules() {
+        return inputLines()
+                .takeWhile(Predicate.not(String::isBlank))
+                .map(PagePrintOrderRule::parse)
+                .toList();
+    }
+
+    private List<PageUpdate> parseAllPageUpdates(int skippedLines) {
+        return inputLines()
+                .skip(skippedLines)
+                .map(PageUpdate::parse)
+                .toList();
     }
 
 
@@ -27,6 +48,75 @@ public class AdventOfCodeDay05 extends AdventOfCodePuzzleSolver {
     public Integer solveSecondPart() {
         // TODO solveSecondPart
         return 0;
+    }
+
+
+    record PagePrintOrderRule(int page, int beforePage) {
+
+        public static PagePrintOrderRule parse(String line) {
+            Pattern pattern = Pattern.compile("(\\d+)\\|(\\d+)");
+            Matcher matcher = pattern.matcher(line);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("Expect line matching pattern " + pattern + " but was: " + line);
+            }
+            int page = Integer.parseInt(matcher.group(1));
+            int pageBefore = Integer.parseInt(matcher.group(2));
+            return new PagePrintOrderRule(page, pageBefore);
+        }
+
+
+        @Override
+        public String toString() {
+            return page + "|" + beforePage;
+        }
+
+    }
+
+
+    record PageUpdate(List<Integer> pages) {
+
+        public static PageUpdate parse(String line) {
+            List<Integer> pages = Pattern.compile(",")
+                    .splitAsStream(line)
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .toList();
+            return new PageUpdate(pages);
+        }
+
+
+        public boolean isValid(List<PagePrintOrderRule> pagePrintOrderRules) {
+            for (int i = 0; i < pages.size(); i++) {
+                int currentPage = pages.get(i);
+                List<Integer> updatedPages = subUpdate(i);
+
+                boolean validatesAllRules = pagePrintOrderRules.stream()
+                        .filter(pagePrintOrderRule -> pagePrintOrderRule.beforePage() == currentPage)
+                        .map(PagePrintOrderRule::page)
+                        .filter(pages::contains)
+                        .allMatch(updatedPages::contains);
+                if (!validatesAllRules) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public List<Integer> subUpdate(int toIndex) {
+            return pages().subList(0, toIndex);
+        }
+
+        public int getMiddlePageNumber() {
+            int middleIndex = pages().size() / 2;
+            return pages().get(middleIndex);
+        }
+
+        @Override
+        public String toString() {
+            return pages.toString();
+        }
+
     }
 
 }
--- 2024/src/test/java/de/adventofcode/chrisgw/day05/AdventOfCodeDay05Test.java
+++ 2024/src/test/java/de/adventofcode/chrisgw/day05/AdventOfCodeDay05Test.java
@@ -1,25 +1,57 @@
 package de.adventofcode.chrisgw.day05;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdventOfCodeDay05Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of(
+            "47|53",
+            "97|13",
+            "97|61",
+            "97|47",
+            "75|29",
+            "61|13",
+            "75|53",
+            "29|13",
+            "97|29",
+            "53|29",
+            "61|53",
+            "97|53",
+            "61|29",
+            "47|13",
+            "75|47",
+            "97|75",
+            "47|61",
+            "75|61",
+            "47|29",
+            "75|13",
+            "53|13",
+            "",
+            "75,47,61,53,29",
+            "97,61,53,29,13",
+            "75,29,13",
+            "75,97,47,61,53",
+            "61,13,29",
+            "97,13,75,29,47");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay05(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        int expected = 61 + 53 + 29;
+        assertEquals(143, expected, "expected sum");
+        assertEquals(expected, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay05.txt");
         int result = new AdventOfCodeDay05(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(5129, result, "firstPart myPuzzleInput");
     }
 
 
