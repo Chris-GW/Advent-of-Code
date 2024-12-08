b3860572fae81b463cc7be4bc042e8910455bb4b
Chris-GW <Chris-GW@users.noreply.github.com>
2024-12-08 19:25:51 +0100

    2024 Day 7: Bridge Repair - part 1
    Took 31 minutes
    
    
    Took 57 seconds

--- 2024/src/main/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07.java
+++ 2024/src/main/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07.java
@@ -3,7 +3,12 @@ package de.adventofcode.chrisgw.day07;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.LongBinaryOperator;
+import java.util.stream.Collectors;
 
 
 /**
@@ -17,9 +22,12 @@ public class AdventOfCodeDay07 extends AdventOfCodePuzzleSolver {
 
 
     @Override
-    public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+    public Long solveFirstPart() {
+        return inputLines()
+                .map(Equation::parseEquation)
+                .filter(Equation::isPossible)
+                .mapToLong(Equation::testValue)
+                .sum();
     }
 
 
@@ -27,6 +35,84 @@ public class AdventOfCodeDay07 extends AdventOfCodePuzzleSolver {
     public Integer solveSecondPart() {
         // TODO solveSecondPart
         return 0;
+    }
+
+
+    record Equation(long testValue, long[] numbers) {
+
+        public static Equation parseEquation(String line) {
+            String[] splitTestValue = line.split(": ");
+            long testValue = Long.parseLong(splitTestValue[0]);
+            String[] splitNumbers = splitTestValue[1].split(" ");
+            long[] numbers = Arrays.stream(splitNumbers).mapToLong(Long::parseLong).toArray();
+            return new Equation(testValue, numbers);
+        }
+
+
+        public boolean isPossible() {
+            List<Operator[]> operatorCombinations = buildOperators(new Operator[neededOperatorCount()], 0);
+            return operatorCombinations.stream().anyMatch(this::test);
+        }
+
+        private List<Operator[]> buildOperators(Operator[] operators, int i) {
+            if (i == neededOperatorCount()) {
+                return Collections.singletonList(Arrays.copyOf(operators, operators.length));
+            }
+            List<Operator[]> foundOperators = new ArrayList<>();
+            for (Operator operator : Operator.values()) {
+                operators[i] = operator;
+                foundOperators.addAll(buildOperators(operators, i + 1));
+                operators[i] = operator;
+            }
+            return foundOperators;
+        }
+
+
+        public boolean test(Operator[] operators) {
+            long value = numbers[0];
+            for (int i = 0; i < operators.length; i++) {
+                value = operators[i].applyAsLong(value, numbers[i + 1]);
+            }
+            return value == testValue;
+        }
+
+
+        public int neededOperatorCount() {
+            return numbers.length - 1;
+        }
+
+        @Override
+        public String toString() {
+            String numberString = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.joining(" "));
+            return testValue + ": " + numberString;
+        }
+
+    }
+
+
+    enum Operator implements LongBinaryOperator {
+        PLUS('+', (a, b) -> a + b),
+        MULT('*', (a, b) -> a * b);
+
+        final char sign;
+        final LongBinaryOperator operator;
+
+        Operator(char sign, LongBinaryOperator operator) {
+            this.sign = sign;
+            this.operator = operator;
+
+        }
+
+
+        @Override
+        public long applyAsLong(long left, long right) {
+            return operator.applyAsLong(left, right);
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(sign);
+        }
     }
 
 }
--- 2024/src/test/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07Test.java
+++ 2024/src/test/java/de/adventofcode/chrisgw/day07/AdventOfCodeDay07Test.java
@@ -1,25 +1,36 @@
 package de.adventofcode.chrisgw.day07;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdventOfCodeDay07Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of(
+            "190: 10 19",
+            "3267: 81 40 27",
+            "83: 17 5",
+            "156: 15 6",
+            "7290: 6 8 6 15",
+            "161011: 16 10 13",
+            "192: 17 8 14",
+            "21037: 9 7 18 13",
+            "292: 11 6 16 20");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
-        int result = new AdventOfCodeDay07(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        long result = new AdventOfCodeDay07(inputLinesExample).solveFirstPart();
+        assertEquals(3749L, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay07.txt");
-        int result = new AdventOfCodeDay07(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        long result = new AdventOfCodeDay07(inputLines).solveFirstPart();
+        assertEquals(4998764814652L, result, "firstPart myPuzzleInput");
     }
 
 
