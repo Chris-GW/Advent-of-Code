21da02dbe8842e218b67c72dd74a7ff4a7298d3d
Chris-GW <Chris-GW@users.noreply.github.com>
2024-12-09 00:44:19 +0100

    2024 Day 8: Resonant Collinearity - part 2
    Took 59 minutes
    
    Took 20 seconds

--- 2024/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
+++ 2024/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
@@ -4,9 +4,11 @@ import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 /**
@@ -22,13 +24,29 @@ public class AdventOfCodeDay08 extends AdventOfCodePuzzleSolver {
     private int mapWidth;
     private int mapHeight;
     private final Map<Character, List<Antenna>> antennaByFrequency = new HashMap<>();
-    private final Map<Integer, Map<Integer, Antinode>> antinodeMap = new HashMap<>();
+    private final Map<Integer, Map<Integer, Antenna>> antinodeMap = new HashMap<>();
 
 
     @Override
     public Integer solveFirstPart() {
         parseAntennaMap();
-        antennaByFrequency.values().forEach(this::markPairedAntinodes);
+        for (List<Antenna> antennas : antennaByFrequency.values()) {
+            markPairedAntinodes(antennas, false);
+        }
+        System.out.println(this);
+        return antinodeMap.values()
+                .stream()
+                .mapToInt(Map::size)
+                .sum();
+    }
+
+    @Override
+    public Integer solveSecondPart() {
+        parseAntennaMap();
+        for (List<Antenna> antennas : antennaByFrequency.values()) {
+            markPairedAntinodes(antennas, true);
+        }
+        System.out.println(this);
         return antinodeMap.values()
                 .stream()
                 .mapToInt(Map::size)
@@ -54,55 +72,96 @@ public class AdventOfCodeDay08 extends AdventOfCodePuzzleSolver {
         }
     }
 
-    private boolean addAntenna(Antenna antenna) {
+    private void addAntenna(Antenna antenna) {
         char frequency = antenna.frequency;
-        return antennaByFrequency.computeIfAbsent(frequency, c -> new ArrayList<>()).add(antenna);
+        antennaByFrequency.computeIfAbsent(frequency, c -> new ArrayList<>()).add(antenna);
     }
 
 
-    private void markPairedAntinodes(List<Antenna> antennas) {
+    private void markPairedAntinodes(List<Antenna> antennas, boolean anyGridPosition) {
         for (int i = 0; i < antennas.size(); i++) {
             Antenna firstAntenna = antennas.get(i);
 
             for (int k = i + 1; k < antennas.size(); k++) {
                 Antenna secondAntenna = antennas.get(k);
-                markAntinodeFor(firstAntenna, secondAntenna);
-                markAntinodeFor(secondAntenna, firstAntenna);
+                markAntinodeFor(firstAntenna, secondAntenna, anyGridPosition);
+                markAntinodeFor(secondAntenna, firstAntenna, anyGridPosition);
             }
         }
     }
 
-    private void markAntinodeFor(Antenna firstAntenna, Antenna secondAntenna) {
+    private void markAntinodeFor(Antenna firstAntenna, Antenna secondAntenna, boolean anyGridPosition) {
         int dx = secondAntenna.x - firstAntenna.x;
         int dy = secondAntenna.y - firstAntenna.y;
-        int x = firstAntenna.x + dx * 2;
-        int y = firstAntenna.y + dy * 2;
-        var antinode = new Antinode(x, y, firstAntenna.frequency);
-        if (isInsideMap(antinode)) {
-            antinodeMap.computeIfAbsent(x, i -> new HashMap<>()).put(y, antinode);
+
+        for (int i = anyGridPosition ? 0 : 2; true; i++) {
+            int x = firstAntenna.x + (dx * i);
+            int y = firstAntenna.y + (dy * i);
+            Antenna antinode = new Antenna(x, y, firstAntenna.frequency);
+            if (!isInsideMap(antinode)) {
+                break;
+            }
+            antinodeMap.computeIfAbsent(x, k -> new HashMap<>()).put(y, antinode);
+            if (!anyGridPosition) {
+                break;
+            }
         }
     }
 
-    private boolean isInsideMap(Antinode antinode) {
+    private boolean isInsideMap(Antenna antinode) {
         return 0 <= antinode.x && antinode.x < mapWidth
                 && 0 <= antinode.y && antinode.y < mapHeight;
     }
 
 
     @Override
-    public Integer solveSecondPart() {
-        // TODO solveSecondPart
-        return 0;
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                Optional<Antenna> antenna = findAntennaAt(x, y);
+                if (antenna.isPresent()) {
+                    sb.append(antenna.get().frequency);
+                    continue;
+                }
+
+                Optional<Antenna> antinode = findAntinodeAt(x, y);
+                if (antinode.isPresent()) {
+                    sb.append('#');
+                } else {
+                    sb.append('.');
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+    private Optional<Antenna> findAntennaAt(int x, int y) {
+        return antennaByFrequency.values()
+                .stream()
+                .flatMap(Collection::stream)
+                .filter(antenna1 -> antenna1.isAt(x, y))
+                .findAny();
+    }
+
+    private Optional<Antenna> findAntinodeAt(int x, int y) {
+        return antinodeMap.values()
+                .stream()
+                .map(Map::values)
+                .flatMap(Collection::stream)
+                .filter(antenna1 -> antenna1.isAt(x, y))
+                .findAny();
     }
 
 
     record Antenna(int x, int y, char frequency) {
 
+        public boolean isAt(int x, int y) {
+            return this.x == x && this.y == y;
+        }
+
     }
-
-    record Antinode(int x, int y, char frequency) {
-
-    }
-
 
 }
--- 2024/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
+++ 2024/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdventOfCodeDay08Test {
 
     private static final List<String> inputLinesExample = List.of(
-            "...........",
+            "............",
             "........0...",
             ".....0......",
             ".......0....",
             "....0.......",
             "......A.....",
-            "...........",
-            "...........",
-            ".......A...",
-            "........A..",
-            "...........",
-            "...........");
+            "............",
+            "............",
+            "........A...",
+            ".........A..",
+            "............",
+            "............");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
@@ -40,14 +40,14 @@ class AdventOfCodeDay08Test {
     @Test
     void solveAocPuzzle_secondPart_example() {
         int result = new AdventOfCodeDay08(inputLinesExample).solveSecondPart();
-        assertEquals(1, result, "secondPart example");
+        assertEquals(34, result, "secondPart example");
     }
 
     @Test
     void solveAocPuzzle_secondPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay08.txt");
         int result = new AdventOfCodeDay08(inputLines).solveSecondPart();
-        assertEquals(1, result, "secondPart myPuzzleInput");
+        assertEquals(1077, result, "secondPart myPuzzleInput");
     }
 
 }
