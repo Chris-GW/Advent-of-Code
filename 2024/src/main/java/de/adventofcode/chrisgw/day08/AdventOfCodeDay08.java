86b62ca4db95265d90ad997be71d66b9d69297d6
Chris-GW <Chris-GW@users.noreply.github.com>
2024-12-08 23:44:49 +0100

    2024 Day 8: Resonant Collinearity - part 1
    Took 1 hour 4 minutes

--- 2024/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
+++ 2024/src/main/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08.java
@@ -3,7 +3,10 @@ package de.adventofcode.chrisgw.day08;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -16,10 +19,73 @@ public class AdventOfCodeDay08 extends AdventOfCodePuzzleSolver {
     }
 
 
+    private int mapWidth;
+    private int mapHeight;
+    private final Map<Character, List<Antenna>> antennaByFrequency = new HashMap<>();
+    private final Map<Integer, Map<Integer, Antinode>> antinodeMap = new HashMap<>();
+
+
     @Override
     public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+        parseAntennaMap();
+        antennaByFrequency.values().forEach(this::markPairedAntinodes);
+        return antinodeMap.values()
+                .stream()
+                .mapToInt(Map::size)
+                .sum();
+    }
+
+
+    private void parseAntennaMap() {
+        mapHeight = getInputLines().size();
+        mapWidth = getInputLines().get(0).length();
+
+        for (int y = 0; y < getInputLines().size(); y++) {
+            String line = getInputLines().get(y);
+
+            for (int x = 0; x < line.length(); x++) {
+                char frequency = line.charAt(x);
+                if (frequency == '.') {
+                    continue;
+                }
+                var antenna = new Antenna(x, y, frequency);
+                addAntenna(antenna);
+            }
+        }
+    }
+
+    private boolean addAntenna(Antenna antenna) {
+        char frequency = antenna.frequency;
+        return antennaByFrequency.computeIfAbsent(frequency, c -> new ArrayList<>()).add(antenna);
+    }
+
+
+    private void markPairedAntinodes(List<Antenna> antennas) {
+        for (int i = 0; i < antennas.size(); i++) {
+            Antenna firstAntenna = antennas.get(i);
+
+            for (int k = i + 1; k < antennas.size(); k++) {
+                Antenna secondAntenna = antennas.get(k);
+                markAntinodeFor(firstAntenna, secondAntenna);
+                markAntinodeFor(secondAntenna, firstAntenna);
+            }
+        }
+    }
+
+    private void markAntinodeFor(Antenna firstAntenna, Antenna secondAntenna) {
+        int dx = secondAntenna.x - firstAntenna.x;
+        int dy = secondAntenna.y - firstAntenna.y;
+        int x = firstAntenna.x + dx * 2;
+        int y = firstAntenna.y + dy * 2;
+        var antinode = new Antinode(x, y, firstAntenna.frequency);
+        if (isInsideMap(antinode)) {
+            antinodeMap.computeIfAbsent(x, i -> new HashMap<>()).put(y, antinode);
+        }
+    }
+
+    private boolean isInsideMap(Antinode antinode) {
+        return 0 <= antinode.x && antinode.x < mapWidth
+                && 0 <= antinode.y && antinode.y < mapHeight;
     }
 
 
@@ -28,5 +94,15 @@ public class AdventOfCodeDay08 extends AdventOfCodePuzzleSolver {
         // TODO solveSecondPart
         return 0;
     }
+
+
+    record Antenna(int x, int y, char frequency) {
+
+    }
+
+    record Antinode(int x, int y, char frequency) {
+
+    }
+
 
 }
--- 2024/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
+++ 2024/src/test/java/de/adventofcode/chrisgw/day08/AdventOfCodeDay08Test.java
@@ -1,25 +1,39 @@
 package de.adventofcode.chrisgw.day08;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdventOfCodeDay08Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of(
+            "...........",
+            "........0...",
+            ".....0......",
+            ".......0....",
+            "....0.......",
+            "......A.....",
+            "...........",
+            "...........",
+            ".......A...",
+            "........A..",
+            "...........",
+            "...........");
 
     @Test
     void solveAocPuzzle_firstPart_example() {
         int result = new AdventOfCodeDay08(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        assertEquals(14, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay08.txt");
         int result = new AdventOfCodeDay08(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        assertEquals(323, result, "firstPart myPuzzleInput");
     }
 
 
