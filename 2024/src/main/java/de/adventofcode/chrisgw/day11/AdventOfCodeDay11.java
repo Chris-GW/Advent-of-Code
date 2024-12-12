e4d9edcfc7f03df2c724a72ecb89ec31d97283e1
Chris-GW <chris-gw@users.noreply.github.com>
2024-12-12 15:50:48 +0100

    2024 Day 11: Plutonian Pebbles - start
    Took 2 minutes

---
+++ 2024/src/main/java/de/adventofcode/chrisgw/day11/AdventOfCodeDay11.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day11;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2024/day/11">Advent of Code - day 11</a>
+ */
+public class AdventOfCodeDay11 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay11(List<String> inputLines) {
+        super(Year.of(2024), 11, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
