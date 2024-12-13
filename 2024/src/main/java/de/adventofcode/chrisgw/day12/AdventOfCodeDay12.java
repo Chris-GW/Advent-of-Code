7448320bca3472b164fddf1167e61580b069272a
Chris-GW <chris-gw@users.noreply.github.com>
2024-12-13 12:48:10 +0100

    2024 Day 12: Garden Groups - start

---
+++ 2024/src/main/java/de/adventofcode/chrisgw/day12/AdventOfCodeDay12.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day12;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2024/day/12">Advent of Code - day 12</a>
+ */
+public class AdventOfCodeDay12 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay12(List<String> inputLines) {
+        super(Year.of(2024), 12, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
