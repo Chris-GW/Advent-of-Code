9bf69ed053e19388ce5456a0495511c47681fbe6
Chris-GW <chris-gw@users.noreply.github.com>
2025-12-01 11:16:03 +0100

    2025 Day 01: Secret Entrance - start
    Took 7 minutes

---
+++ 2025/src/main/java/de/adventofcode/chrisgw/day01/AdventOfCodeDay01.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day01;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2025/day/1">Advent of Code - day 1</a>
+ */
+public class AdventOfCodeDay01 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay01(List<String> inputLines) {
+        super(Year.of(2025), 1, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
---
+++ 2025/src/main/resources/AdventOfCodeTemplate.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day${day0};
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/${year}/day/${day}">Advent of Code - day ${day}</a>
+ */
+public class AdventOfCodeDay${day0} extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay${day0}(List<String> inputLines) {
+        super(Year.of(${year}), ${day}, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
