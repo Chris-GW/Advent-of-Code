2953efb4f325e97e2352cd69eb663c55b57477c3
Chris-GW <Chris-GW@users.noreply.github.com>
2025-12-03 09:36:07 +0100

    2025 Day 3: Lobby - start

---
+++ 2025/src/main/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day03;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2025/day/3">Advent of Code - day 3</a>
+ */
+public class AdventOfCodeDay03 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay03(List<String> inputLines) {
+        super(Year.of(2025), 3, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
