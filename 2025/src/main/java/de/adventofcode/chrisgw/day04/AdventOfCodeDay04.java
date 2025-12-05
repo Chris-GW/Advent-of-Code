7b8bad15b70b2855788d8435f08540fc7c336565
Chris-GW <chris-gw@users.noreply.github.com>
2025-12-05 10:19:16 +0100

    2025 Day 4: Printing Department - part 1
    Took 34 minutes

--- 2025/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
+++ 2025/src/main/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04.java
@@ -3,7 +3,10 @@ package de.adventofcode.chrisgw.day04;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
 
 
 /**
@@ -11,15 +14,31 @@ import java.util.List;
  */
 public class AdventOfCodeDay04 extends AdventOfCodePuzzleSolver {
 
+    private GridPosition[][] grid;
+
     public AdventOfCodeDay04(List<String> inputLines) {
         super(Year.of(2025), 4, inputLines);
     }
 
+    private GridPosition[][] parseGrid() {
+        List<String> inputLines = getInputLines();
+        GridPosition[][] newGrid = new GridPosition[inputLines.size()][];
+        for (int y = 0; y < inputLines.size(); y++) {
+            String line = inputLines.get(y);
+            newGrid[y] = new GridPosition[line.length()];
+            for (int x = 0; x < line.length(); x++) {
+                boolean withPaperRoll = line.charAt(x) == '@';
+                newGrid[y][x] = new GridPosition(x, y, withPaperRoll);
+            }
+        }
+        return newGrid;
+    }
+
 
     @Override
-    public Integer solveFirstPart() {
-        // TODO solveFirstPart
-        return 0;
+    public Long solveFirstPart() {
+        grid = parseGrid();
+        return gridPositions().filter(GridPosition::isAccessiblePaperRoll).count();
     }
 
 
@@ -27,6 +46,78 @@ public class AdventOfCodeDay04 extends AdventOfCodePuzzleSolver {
     public Integer solveSecondPart() {
         // TODO solveSecondPart
         return 0;
+    }
+
+
+    private Stream<GridPosition> gridPositions() {
+        return Arrays.stream(grid).flatMap(Arrays::stream);
+    }
+
+    private boolean containsPosition(int x, int y) {
+        return 0 <= y && y < grid.length &&
+                0 <= x && x < grid[y].length;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (GridPosition[] gridPositions : grid) {
+            for (GridPosition gridPoisition : gridPositions) {
+                if (gridPoisition.isAccessiblePaperRoll()) {
+                    sb.append('x');
+                } else if (gridPoisition.isPaperRoll()) {
+                    sb.append('@');
+                } else {
+                    sb.append('.');
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+    private class GridPosition {
+
+        final int x;
+        final int y;
+        final boolean isPaperRoll;
+
+
+        private GridPosition(int x, int y, boolean isPaperRoll) {
+            this.x = x;
+            this.y = y;
+            this.isPaperRoll = isPaperRoll;
+        }
+
+
+        public boolean isAccessiblePaperRoll() {
+            return isPaperRoll() && adjacentPositions().filter(GridPosition::isPaperRoll).count() < 4;
+        }
+
+
+        public Stream<GridPosition> adjacentPositions() {
+            Builder<GridPosition> adjacentPositions = Stream.builder();
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dx = -1; dx <= 1; dx++) {
+                    int x = this.x + dx;
+                    int y = this.y + dy;
+
+                    if ((dy != 0 || dx != 0) && containsPosition(x, y)) {
+                        var gridPoisition = grid[y][x];
+                        adjacentPositions.add(gridPoisition);
+                    }
+                }
+            }
+            return adjacentPositions.build();
+        }
+
+
+        public boolean isPaperRoll() {
+            return isPaperRoll;
+        }
+
     }
 
 }
--- 2025/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
+++ 2025/src/test/java/de/adventofcode/chrisgw/day04/AdventOfCodeDay04Test.java
@@ -1,25 +1,39 @@
 package de.adventofcode.chrisgw.day04;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static de.adventofcode.chrisgw.TestUtils.readAllLinesOfClassPathResource;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class AdventOfCodeDay04Test {
 
-    private static final List<String> inputLinesExample = List.of("");
+    private static final List<String> inputLinesExample = List.of(
+            "..@@.@@@@.",
+            "@@@.@.@.@@",
+            "@@@@@.@.@@",
+            "@.@@@@..@.",
+            "@@.@@@@.@@",
+            ".@@@@@@@.@",
+            ".@.@.@.@@@",
+            "@.@@@.@@@@",
+            ".@@@@@@@@.",
+            "@.@.@@@.@.");
+
 
     @Test
     void solveAocPuzzle_firstPart_example() {
-        int result = new AdventOfCodeDay04(inputLinesExample).solveFirstPart();
-        assertEquals(1, result, "firstPart example");
+        long result = new AdventOfCodeDay04(inputLinesExample).solveFirstPart();
+        assertEquals(13L, result, "firstPart example");
     }
 
     @Test
     void solveAocPuzzle_firstPart_myPuzzleInput() {
         List<String> inputLines = readAllLinesOfClassPathResource("/puzzleInputDay04.txt");
-        int result = new AdventOfCodeDay04(inputLines).solveFirstPart();
-        assertEquals(1, result, "firstPart myPuzzleInput");
+        long result = new AdventOfCodeDay04(inputLines).solveFirstPart();
+        assertEquals(1411L, result, "firstPart myPuzzleInput");
     }
 
 
