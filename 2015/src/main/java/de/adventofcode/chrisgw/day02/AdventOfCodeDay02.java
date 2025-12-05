850e79f2ce09cc4c8f0d62f0397d2b81e7974a4a
Chris-GW <chris-gw@users.noreply.github.com>
2025-12-05 15:24:12 +0100

    2015 Day 02: I Was Told There Would Be No Math - start
    Took 43 seconds

---
+++ 2015/src/main/java/de/adventofcode/chrisgw/day02/AdventOfCodeDay02.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day02;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2015/day/2">Advent of Code - day 2</a>
+ */
+public class AdventOfCodeDay02 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay02(List<String> inputLines) {
+        super(Year.of(2015), 2, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
