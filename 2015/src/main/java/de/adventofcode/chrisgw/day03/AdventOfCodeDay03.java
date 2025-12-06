29a88629e6950d68a1f71521163982d15b69078c
Chris-GW <chris-gw@users.noreply.github.com>
2025-12-06 22:42:50 +0100

    2015 Day 3: Perfectly Spherical Houses in a Vacuum - refactor
    Took 33 minutes

--- 2015/src/main/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03.java
+++ 2015/src/main/java/de/adventofcode/chrisgw/day03/AdventOfCodeDay03.java
@@ -3,13 +3,25 @@ package de.adventofcode.chrisgw.day03;
 import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 /**
  * <a href="https://adventofcode.com/2015/day/3">Advent of Code - day 3</a>
  */
 public class AdventOfCodeDay03 extends AdventOfCodePuzzleSolver {
+
+    private int santaIndex = 0;
+    private List<DirectedSanta> santaList = new ArrayList<>();
+    private Map<Integer, Set<Integer>> visitedPositions = new HashMap<>();
 
 
     public AdventOfCodeDay03(List<String> inputLines) {
@@ -19,35 +31,47 @@ public class AdventOfCodeDay03 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Integer solveFirstPart() {
-        var santa = new DirectedSanta();
-        String inputLine = getInputLines().getFirst();
-        for (int i = 0; i < inputLine.length(); i++) {
-            char directionSign = inputLine.charAt(i);
-            var direction = Direction.fromSign(directionSign);
-            santa.walkDirection(direction);
-        }
-        return santa.countVisitedHouses();
+        return countVisitedHouses(1);
     }
-
 
     @Override
     public Integer solveSecondPart() {
-        var santa = new DirectedSanta();
-        var robotSanta = new DirectedSanta();
-        boolean robotSantaTurn = false;
+        return countVisitedHouses(2);
+    }
 
+    private int countVisitedHouses(int santaCount) {
+        visitedPositions = new HashMap<>();
+        santaList = Stream.generate(DirectedSanta::new).limit(santaCount).toList();
+        saveVisitedPosition(0, 0);
+        inputDirections().forEachOrdered(this::processDirection);
+        return countVisitedPositions();
+    }
+
+
+    private Stream<Direction> inputDirections() {
         String inputLine = getInputLines().getFirst();
-        for (int i = 0; i < inputLine.length(); i++) {
-            char directionSign = inputLine.charAt(i);
-            var direction = Direction.fromSign(directionSign);
-            if (robotSantaTurn) {
-                robotSanta.walkDirection(direction);
-            } else {
-                santa.walkDirection(direction);
-            }
-            robotSantaTurn = !robotSantaTurn;
-        }
-        return santa.countVisitedHouses(robotSanta);
+        return IntStream.range(0, inputLine.length())
+                .mapToObj(i -> Direction.fromSign(inputLine.charAt(i)));
+    }
+
+
+    private void processDirection(Direction direction) {
+        DirectedSanta santa = santaList.get(santaIndex++);
+        santaIndex %= santaList.size();
+        santa.walk(direction);
+        saveVisitedPosition(santa.x(), santa.y());
+    }
+
+    private void saveVisitedPosition(int x, int y) {
+        visitedPositions.computeIfAbsent(x, _ -> new HashSet<>()).add(y);
+    }
+
+
+    private int countVisitedPositions() {
+        return visitedPositions.values()
+                .stream()
+                .mapToInt(Collection::size)
+                .sum();
     }
 
 }
