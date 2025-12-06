37358de8660c2d617dc3630d432eb5001919fd61
Chris-GW <chris-gw@users.noreply.github.com>
2025-12-06 18:29:14 +0100

    2025 Day 06:Trash Compactor - refactor
    Took 2 hour 63 minutes

--- 2025/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
+++ 2025/src/main/java/de/adventofcode/chrisgw/day06/AdventOfCodeDay06.java
@@ -5,10 +5,6 @@ import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
 import java.time.Year;
 import java.util.List;
 import java.util.function.LongBinaryOperator;
-import java.util.function.Predicate;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -18,8 +14,6 @@ import java.util.stream.Stream;
  */
 public class AdventOfCodeDay06 extends AdventOfCodePuzzleSolver {
 
-    public static final Pattern OPERATOR_COLUMN_PATTERN = Pattern.compile("[+*]\\s+");
-
     public AdventOfCodeDay06(List<String> inputLines) {
         super(Year.of(2025), 6, inputLines);
     }
@@ -27,59 +21,95 @@ public class AdventOfCodeDay06 extends AdventOfCodePuzzleSolver {
 
     @Override
     public Long solveFirstPart() {
-        String operationRow = getInputLines().getLast();
-        Matcher matcher = OPERATOR_COLUMN_PATTERN.matcher(operationRow);
-        return matcher.results().mapToLong(this::solveColumn).sum();
+        return IntStream.iterate(0, start -> start < rowLength(), start -> columnEnd(start) + 1)
+                .mapToObj(WorksheetColumn::new)
+                .mapToLong(WorksheetColumn::solveFirstPart)
+                .sum();
     }
-
-    private long solveColumn(MatchResult matchResult) {
-        int beginIndex = matchResult.start();
-        int endIndex = matchResult.end();
-        LongBinaryOperator operator = parseOperator(matchResult.group());
-        return columnValues(beginIndex, endIndex)
-                .map(String::trim)
-                .mapToLong(Long::parseLong)
-                .reduce(operator)
-                .orElse(0);
-    }
-
 
     @Override
     public Long solveSecondPart() {
-        String operationRow = getInputLines().getLast();
-        Matcher matcher = OPERATOR_COLUMN_PATTERN.matcher(operationRow);
-        return matcher.results().mapToLong(this::solveColumnSecondPart).sum();
-    }
-
-    private long solveColumnSecondPart(MatchResult matchResult) {
-        int beginIndex = matchResult.start();
-        int endIndex = matchResult.end();
-        LongBinaryOperator operator = parseOperator(matchResult.group());
-        return IntStream.range(0, endIndex - beginIndex)
-                .mapToObj(index -> columnValues(beginIndex, endIndex)
-                        .reduce("", (left, right) -> left + right.charAt(index)))
-                .map(String::trim)
-                .filter(Predicate.not(String::isEmpty))
-                .mapToLong(Long::parseLong)
-                .reduce(operator)
-                .orElse(0);
+        return IntStream.iterate(0, start -> start < rowLength(), start -> columnEnd(start) + 1)
+                .mapToObj(WorksheetColumn::new)
+                .mapToLong(WorksheetColumn::solveSecondPart)
+                .sum();
     }
 
 
-    private Stream<String> columnValues(int beginIndex, int endIndex) {
-        return inputLines()
-                .limit(getInputLines().size() - 1L)
-                .map(s -> s.substring(beginIndex, endIndex));
+    private int columnEnd(int fromIndex) {
+        return IntStream.range(fromIndex, rowLength())
+                .filter(this::isSpacerColumnAt)
+                .findFirst()
+                .orElse(rowLength());
+    }
+
+    private boolean isSpacerColumnAt(int index) {
+        return inputLines().allMatch(row -> row.charAt(index) == ' ');
     }
 
 
-    private LongBinaryOperator parseOperator(String operationStr) {
-        operationStr = operationStr.trim();
-        return switch (operationStr) {
-            case "+" -> (left, right) -> left + right;
-            case "*" -> (left, right) -> left * right;
-            default -> throw new IllegalStateException("Unexpected value: " + operationStr);
-        };
+    private int rowLength() {
+        return getInputLines().getFirst().length();
+    }
+
+
+    private class WorksheetColumn {
+
+        final int start;
+        final int end;
+
+        public WorksheetColumn(int start) {
+            this.start = start;
+            this.end = columnEnd(start);
+        }
+
+
+        public long solveFirstPart() {
+            return solveColumnValues(values());
+        }
+
+        public long solveSecondPart() {
+            Stream<String> verticallyNumbers = IntStream.range(start, end)
+                    .map(index -> index - start)
+                    .mapToObj(this::parseNumberVertically);
+            return solveColumnValues(verticallyNumbers);
+        }
+
+
+        private Stream<String> values() {
+            return inputLines()
+                    .limit(getInputLines().size() - 1L)
+                    .map(s -> s.substring(start, end));
+        }
+
+
+        private long solveColumnValues(Stream<String> values) {
+            return values
+                    .map(String::trim)
+                    .mapToLong(Long::parseLong)
+                    .reduce(columnOperator())
+                    .orElse(0);
+        }
+
+        private LongBinaryOperator columnOperator() {
+            String operationStr = getInputLines().getLast().substring(start, end);
+            return switch (operationStr.trim()) {
+                case "+" -> (left, right) -> left + right;
+                case "*" -> (left, right) -> left * right;
+                default -> throw new IllegalStateException("Unexpected operation: " + operationStr);
+            };
+        }
+
+
+        private String parseNumberVertically(int index) {
+            return values().reduce("", (left, right) -> left + right.charAt(index));
+        }
+
+
+        @Override
+        public String toString() {
+            return "[%d-%d)".formatted(start, end);
+        }
     }
 
 }
