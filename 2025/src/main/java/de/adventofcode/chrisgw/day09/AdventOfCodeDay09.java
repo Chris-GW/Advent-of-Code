9c38718f4e1f380031f211295442b557f7a3185c
Chris-GW <chris-gw@users.noreply.github.com>
2025-12-09 09:36:14 +0100

    2025 Day 9: Movie Theater - start
    Took 8 minutes

---
+++ 2025/src/main/java/de/adventofcode/chrisgw/day09/AdventOfCodeDay09.java
@@ -0,0 +1,32 @@
+package de.adventofcode.chrisgw.day09;
+
+import de.adventofcode.chrisgw.AdventOfCodePuzzleSolver;
+
+import java.time.Year;
+import java.util.List;
+
+
+/**
+ * <a href="https://adventofcode.com/2025/day/9">Advent of Code - day 9</a>
+ */
+public class AdventOfCodeDay09 extends AdventOfCodePuzzleSolver {
+
+    public AdventOfCodeDay09(List<String> inputLines) {
+        super(Year.of(2025), 9, inputLines);
+    }
+
+
+    @Override
+    public Integer solveFirstPart() {
+        // TODO solveFirstPart
+        return 0;
+    }
+
+
+    @Override
+    public Integer solveSecondPart() {
+        // TODO solveSecondPart
+        return 0;
+    }
+
+}
